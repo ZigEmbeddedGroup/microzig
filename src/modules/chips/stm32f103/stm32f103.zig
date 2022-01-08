f1ea8abe3d955735f06c28d4b697e58913a14e4a
Matt Knight <mattnite@protonmail.com>
2022-01-08 12:29:50 -0800

    works as a package

--- src/modules/chips/stm32f103/stm32f103.zig
+++ src/modules/chips/stm32f103/stm32f103.zig
@@ -1,11 +1,4 @@
 const std = @import("std");
-const micro = @import("microzig");
-const micro_linker = @import("microzig-linker");
 
 pub const cpu = @import("cpu");
 pub const registers = @import("registers.zig");
-
-pub const memory_regions = [_]micro_linker.MemoryRegion{
-    micro_linker.MemoryRegion{ .offset = 0x00000000, .length = 64 * 1024, .kind = .flash },
-    micro_linker.MemoryRegion{ .offset = 0x10000000, .length = 20 * 1024, .kind = .ram },
-};
