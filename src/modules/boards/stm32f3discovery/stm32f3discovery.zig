552f4fec9f831a0f03514885b46ef551a6aec6c7
Marnix Klooster <marnix.klooster+github.com@gmail.com>
2022-01-14 18:39:52 +0100

    STM32F3DISCOVERY: map all LED pins

--- src/modules/boards/stm32f3discovery/stm32f3discovery.zig
+++ src/modules/boards/stm32f3discovery/stm32f3discovery.zig
@@ -3,6 +3,22 @@ pub const chip = @import("chip");
 pub const cpu_frequency = 8_000_000;
 
 pub const pin_map = .{
-    // LEDs
-    .@"LD3" = "PE9", // GPIOE bit 9
+    // circle of LEDs, connected to GPIOE bits 8..15
+
+    // NW blue
+    .@"LD4" = "PE8",
+    // N red
+    .@"LD3" = "PE9",
+    // NE orange
+    .@"LD5" = "PE10",
+    // E green
+    .@"LD7" = "PE11",
+    // SE blue
+    .@"LD9" = "PE12",
+    // S red
+    .@"LD10" = "PE13",
+    // SW orange
+    .@"LD8" = "PE14",
+    // W green
+    .@"LD6" = "PE15",
 };
