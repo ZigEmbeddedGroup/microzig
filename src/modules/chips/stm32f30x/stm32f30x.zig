dcbad98be252e09839461fb638cb72c9e01f002c
Marnix Klooster <marnix.klooster+github.com@gmail.com>
2022-01-14 18:39:19 +0100

    stm32f30x: Allow all 16 pins, all GPIOx registers

--- src/modules/chips/stm32f30x/stm32f30x.zig
+++ src/modules/chips/stm32f30x/stm32f30x.zig
@@ -5,37 +5,51 @@ pub const cpu = @import("cpu");
 pub const registers = @import("registers.zig");
 
 pub fn parsePin(comptime spec: []const u8) type {
-    const invalid_format_msg = "The given pin '" ++ spec ++ "' has an invalid format.";
+    const invalid_format_msg = "The given pin '" ++ spec ++ "' has an invalid format. Pins must follow the format \"P{Port}{Pin}\" scheme.";
 
-    if (!std.mem.eql(u8, spec, "PE9")) {
+    if (spec[0] != 'P')
         @compileError(invalid_format_msg);
-    }
-    return struct {};
+    if (spec[1] < 'A' or spec[1] > 'H')
+        @compileError(invalid_format_msg);
+
+    const pin_number: comptime_int = std.fmt.parseInt(u4, spec[2..], 10) catch @compileError(invalid_format_msg);
+
+    return struct {
+        /// 'A'...'H'
+        const gpio_port_name = spec[1..2];
+        const gpio_port = @field(registers, "GPIO" ++ gpio_port_name);
+        const suffix = std.fmt.comptimePrint("{d}", .{pin_number});
+    };
+}
+
+fn setRegField(reg: anytype, comptime field_name: anytype, value: anytype) void {
+    var temp = reg.read();
+    @field(temp, field_name) = value;
+    reg.write(temp);
 }
 
 pub const gpio = struct {
     pub fn setOutput(comptime pin: type) void {
-        _ = pin;
-        registers.RCC.AHBENR.modify(.{ .IOPEEN = 1 });
-        registers.GPIOE.MODER.modify(.{ .MODER9 = 0b01 });
+        setRegField(registers.RCC.AHBENR, "IOP" ++ pin.gpio_port_name ++ "EN", 1);
+        setRegField(@field(pin.gpio_port, "MODER"), "MODER" ++ pin.suffix, 0b01);
     }
 
     pub fn setInput(comptime pin: type) void {
-        _ = pin;
-        registers.RCC.AHBENR.modify(.{ .IOPEEN = 1 });
-        registers.GPIOE.MODER.modify(.{ .MODER9 = 0b00 });
+        setRegField(registers.RCC.AHBENR, "IOP" ++ pin.gpio_port_name ++ "EN", 1);
+        setRegField(@field(pin.gpio_port, "MODER"), "MODER" ++ pin.suffix, 0b00);
     }
 
     pub fn read(comptime pin: type) micro.gpio.State {
-        _ = pin;
-        return @intToEnum(micro.gpio.State, registers.GPIOE.IDR.read().IDR9);
+        const idr_reg = pin.gpio_port.IDR;
+        const reg_value = @field(idr_reg.read(), "IDR" ++ pin.suffix); // TODO extract to getRegField()?
+        return @intToEnum(micro.gpio.State, reg_value);
     }
 
     pub fn write(comptime pin: type, state: micro.gpio.State) void {
         _ = pin;
         switch (state) {
-            .low => registers.GPIOE.BRR.modify(.{ .BR9 = 1 }),
-            .high => registers.GPIOE.BSRR.modify(.{ .BS9 = 1 }),
+            .low => setRegField(pin.gpio_port.BRR, "BR" ++ pin.suffix, 1),
+            .high => setRegField(pin.gpio_port.BSRR, "BS" ++ pin.suffix, 1),
         }
     }
 };
