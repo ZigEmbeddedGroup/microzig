7e64003421903e1a6a3c892821e75d53c1e5cea4
Felix "xq" Queißner <git@masterq32.de>
2022-03-14 17:15:55 +0100

    Greatly simplifies the package logic by using a proxy-to-root package.

--- src/core/microzig.zig
+++ src/core/microzig.zig
@@ -1,6 +1,14 @@
+//! This is the entry point and root file of microzig.
+//! If you do a @import("microzig"), you'll *basically* get this file.
+//!
+//! But microzig employs a proxy tactic
+
 const std = @import("std");
 const root = @import("root");
 const builtin = @import("builtin");
+
+/// The app that is currently built.
+pub const app = @import("app");
 
 /// Contains build-time generated configuration options for microzig.
 /// Contains a CPU target description, chip, board and cpu information
@@ -14,7 +22,7 @@ pub const chip = @import("chip");
 pub const board = if (config.has_board) @import("board") else void;
 
 /// Provides access to the low level features of the CPU.
-pub const cpu = chip.cpu;
+pub const cpu = @import("cpu");
 
 /// Module that helps with interrupt handling.
 pub const interrupts = @import("interrupts.zig");
@@ -32,6 +40,8 @@ pub const uart = @import("uart.zig");
 pub const Uart = uart.Uart;
 
 pub const debug = @import("debug.zig");
+
+pub const mmio = @import("mmio.zig");
 
 /// The microzig panic handler. Will disable interrupts and loop endlessly.
 /// Export this symbol from your main file to enable microzig:
@@ -75,4 +85,45 @@ pub fn hang() noreturn {
 
 comptime {
     _ = cpu.startup_logic;
+}
+
+/// This is the logical entry point for microzig.
+/// It will invoke the main function from the root source file
+/// and provides error return handling as well as a event loop if requested.
+///
+/// Why is this function exported?
+/// This is due to the modular design of microzig to allow the "chip" dependency of microzig
+/// to call into our main function here. If we would use a normal function call, we'd have a
+/// circular dependency between the `microzig` and `chip` package. This function is also likely
+/// to be invoked from assembly, so it's also convenient in that regard.
+export fn microzig_main() noreturn {
+    if (!@hasDecl(app, "main"))
+        @compileError("The root source file must provide a public function main!");
+
+    const main = @field(app, "main");
+    const info: std.builtin.TypeInfo = @typeInfo(@TypeOf(main));
+
+    const invalid_main_msg = "main must be either 'pub fn main() void' or 'pub fn main() !void'.";
+    if (info != .Fn or info.Fn.args.len > 0)
+        @compileError(invalid_main_msg);
+
+    const return_type = info.Fn.return_type orelse @compileError(invalid_main_msg);
+
+    if (info.Fn.calling_convention == .Async)
+        @compileError("TODO: Embedded event loop not supported yet. Please try again later.");
+
+    if (@typeInfo(return_type) == .ErrorUnion) {
+        main() catch |err| {
+            // TODO:
+            // - Compute maximum size on the type of "err"
+            // - Do not emit error names when std.builtin.strip is set.
+            var msg: [64]u8 = undefined;
+            @panic(std.fmt.bufPrint(&msg, "main() returned error {s}", .{@errorName(err)}) catch @panic("main() returned error."));
+        };
+    } else {
+        main();
+    }
+
+    // main returned, just hang around here a bit
+    hang();
 }
