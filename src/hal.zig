e541f966d42004acbcba9a939c0acfb604a3b4d5
Maciej 'vesim' Kuliński <vesim809@pm.me>
2022-07-06 21:29:36 +0200

    multicore: add initial support

--- src/hal.zig
+++ src/hal.zig
@@ -2,6 +2,7 @@ const microzig = @import("microzig");
 const regs = microzig.chip.regsisters;
 pub const gpio = @import("hal/gpio.zig");
 pub const clocks = @import("hal/clocks.zig");
+pub const multicore = @import("hal/multicore.zig");
 
 pub const default_clock_config = clocks.GlobalConfiguration.init(.{
     //.ref = .{ .source = .src_xosc },
