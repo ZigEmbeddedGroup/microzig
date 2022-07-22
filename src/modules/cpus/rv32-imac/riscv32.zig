c21f79189c35085ab7a09a2169d66c1a6d0a83e4
Matheus Catarino <matheus-catarino@hotmail.com>
2022-07-22 14:35:29 -0300

    minimal interrupt

--- src/modules/cpus/rv32-imac/riscv32.zig
+++ src/modules/cpus/rv32-imac/riscv32.zig
@@ -902,12 +902,6 @@ pub const CAUSE_SUPERVISOR_ECALL = 0x9;
 pub const CAUSE_HYPERVISOR_ECALL = 0xa;
 pub const CAUSE_MACHINE_ECALL = 0xb;
 
-fn isValidField(field_name: []const u8) bool {
-    return !std.mem.startsWith(u8, field_name, "reserved") and
-        !std.mem.eql(u8, field_name, "initial_stack_pointer") and
-        !std.mem.eql(u8, field_name, "reset");
-}
-
 const VectorTable = microzig.chip.VectorTable;
 
 // will be imported by microzig.zig to allow system startup.
@@ -916,30 +910,6 @@ pub var vector_table: VectorTable = blk: {
         .initial_stack_pointer = microzig.config.end_of_stack,
         .Reset = .{ .C = microzig.cpu.startup_logic._start },
     };
-    if (@hasDecl(app, "interrupts")) {
-        if (@typeInfo(app.interrupts) != .Struct)
-            @compileLog("root.interrupts must be a struct");
-
-        inline for (@typeInfo(app.interrupts).Struct.decls) |decl| {
-            const function = @field(app.interrupts, decl.name);
-
-            if (!@hasField(VectorTable, decl.name)) {
-                var msg: []const u8 = "There is no such interrupt as '" ++ decl.name ++ "'. Declarations in 'interrupts' must be one of:\n";
-                inline for (std.meta.fields(VectorTable)) |field| {
-                    if (isValidField(field.name)) {
-                        msg = msg ++ "    " ++ field.name ++ "\n";
-                    }
-                }
-
-                @compileError(msg);
-            }
-
-            if (!isValidField(decl.name))
-                @compileError("You are not allowed to specify '" ++ decl.name ++ "' in the vector table, for your sins you must now pay a $5 fine to the ZSF: https://github.com/sponsors/ziglang");
-
-            @field(tmp, decl.name) = createInterruptVector(function);
-        }
-    }
     break :blk tmp;
 };
 
@@ -947,26 +917,5 @@ fn createInterruptVector(
     comptime function: anytype,
 ) microzig.chip.InterruptVector {
     const calling_convention = @typeInfo(@TypeOf(function)).Fn.calling_convention;
-    return switch (calling_convention) {
-        .C => .{ .C = function },
-        .Naked => .{ .Naked = function },
-        // for unspecified calling convention we are going to generate small wrapper
-        .Unspecified => .{
-            .C = struct {
-                fn wrapper() callconv(.C) void {
-                    if (calling_convention == .Unspecified) // TODO: workaround for some weird stage1 bug
-                        @call(.{ .modifier = .always_inline }, function, .{});
-                }
-            }.wrapper,
-        },
-
-        else => |val| {
-            const conv_name = inline for (std.meta.fields(std.builtin.CallingConvention)) |field| {
-                if (val == @field(std.builtin.CallingConvention, field.name))
-                    break field.name;
-            } else unreachable;
-
-            @compileError("unsupported calling convention for interrupt vector: " ++ conv_name);
-        },
-    };
+    return calling_convention;
 }
