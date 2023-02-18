56e3d88bc0a0ebda41588f906a25437592d7f3a9
Matt Knight <mattnite@protonmail.com>
2023-02-18 18:06:45 -0500

    add chips and board (#1)
    * add chips and board
    
    * fix up hal

--- src/boards.zig
+++ src/boards.zig
@@ -1,6 +1,13 @@
 const std = @import("std");
-const microzig = @import("../deps/microzig/src/main.zig");
+const micro = @import("../deps/microzig/src/main.zig");
+const chips = @import("chips.zig");
 
 fn root_dir() []const u8 {
     return std.fs.path.dirname(@src().file) orelse ".";
 }
+
+pub const longan_nano = micro.Board{
+    .name = "Longan Nano",
+    .source = .{ .path = root_dir() ++ "/boards/longan_nano.zig" },
+    .chip = chips.gd32vf103xb,
+};
