f85276722419141b55a5af9ef6c6ee0a889ab6eb
Matt Knight <mattnite@protonmail.com>
2023-02-28 01:10:41 -0800

    keep it to one arch, bump flash for debug

--- test/backings.zig
+++ test/backings.zig
@@ -13,7 +13,12 @@ const cpu = microzig.Cpu{
     },
 
     // this will actually make it a native target, this is still fine
-    .target = std.zig.CrossTarget{},
+    .target = std.zig.CrossTarget{
+        .cpu_arch = .thumb,
+        .cpu_model = .{ .explicit = &std.Target.arm.cpu.cortex_m0 },
+        .os_tag = .freestanding,
+        .abi = .none,
+    },
 };
 
 const minimal_chip = microzig.Chip{
@@ -23,8 +28,8 @@ const minimal_chip = microzig.Chip{
         .path = root_dir() ++ "/chip.zig",
     },
     .memory_regions = &.{
-        .{ .offset = 0x1000, .length = 0x1000, .kind = .flash },
-        .{ .offset = 0x2000, .length = 0x1000, .kind = .ram },
+        .{ .offset = 0x00000, .length = 0x10000, .kind = .flash },
+        .{ .offset = 0x10000, .length = 0x10000, .kind = .ram },
     },
 };
 
@@ -38,8 +43,8 @@ const chip_with_hal = microzig.Chip{
         .path = root_dir() ++ "/hal.zig",
     },
     .memory_regions = &.{
-        .{ .offset = 0x1000, .length = 0x1000, .kind = .flash },
-        .{ .offset = 0x2000, .length = 0x1000, .kind = .ram },
+        .{ .offset = 0x00000, .length = 0x10000, .kind = .flash },
+        .{ .offset = 0x10000, .length = 0x10000, .kind = .ram },
     },
 };
 
