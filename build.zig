31613f43a145b46be4ed140e0a538b5da447320c
Matt Knight <mattnite@protonmail.com>
2023-02-28 01:26:10 -0800

    Update microzig (#4)
    * update microzig
    
    * update to new api
    
    ---------
    
    Co-authored-by: mattnite <mattnite@users.noreply.github.com>

--- build.zig
+++ build.zig
@@ -1,7 +1,8 @@
 const std = @import("std");
 const microzig = @import("deps/microzig/build.zig");
-const boards = @import("src/boards.zig");
-const chips = @import("src/chips.zig");
+
+pub const boards = @import("src/boards.zig");
+pub const chips = @import("src/chips.zig");
 
 pub fn build(b: *std.build.Builder) void {
     const optimize = b.standardOptimizeOption(.{});
@@ -9,13 +10,14 @@ pub fn build(b: *std.build.Builder) void {
         if (!decl.is_pub)
             continue;
 
-        const exe = microzig.addEmbeddedExecutable(
-            b,
-            @field(boards, decl.name).name ++ ".minimal",
-            "test/programs/minimal.zig",
-            .{ .board = @field(boards, decl.name) },
-            .{ .optimize = optimize },
-        );
+        const exe = microzig.addEmbeddedExecutable(b, .{
+            .name = @field(boards, decl.name).name ++ ".minimal",
+            .source_file = .{
+                .path = "test/programs/minimal.zig",
+            },
+            .backing = .{ .board = @field(boards, decl.name) },
+            .optimize = optimize,
+        });
         exe.install();
     }
 
@@ -23,13 +25,14 @@ pub fn build(b: *std.build.Builder) void {
         if (!decl.is_pub)
             continue;
 
-        const exe = microzig.addEmbeddedExecutable(
-            b,
-            @field(chips, decl.name).name ++ ".minimal",
-            "test/programs/minimal.zig",
-            .{ .chip = @field(chips, decl.name) },
-            .{ .optimize = optimize },
-        );
+        const exe = microzig.addEmbeddedExecutable(b, .{
+            .name = @field(chips, decl.name).name ++ ".minimal",
+            .source_file = .{
+                .path = "test/programs/minimal.zig",
+            },
+            .backing = .{ .chip = @field(chips, decl.name) },
+            .optimize = optimize,
+        });
         exe.install();
     }
 }
