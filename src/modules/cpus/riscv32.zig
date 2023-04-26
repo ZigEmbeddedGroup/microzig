b5edf6da6b540215f03689c3cc07d00478255f7d
Matt Knight <mattnite@proton.me>
2023-04-26 00:25:08 -0700

    interrupt enable/disable for riscv and air (#127)
    * rename disable/enable interrupt functions
    
    * enable/disable interrupt functions for avr and riscv
    
    * add todo panic

--- src/modules/cpus/riscv32.zig
+++ src/modules/cpus/riscv32.zig
@@ -2,11 +2,13 @@ const std = @import("std");
 const root = @import("root");
 const microzig = @import("microzig");
 
-pub fn sei() void {
+pub fn enable_interrupts() void {
+    @panic("TODO");
     // asm volatile ("sei");
 }
 
-pub fn cli() void {
+pub fn disable_interrupts() void {
+    @panic("TODO");
     // asm volatile ("cli");
 }
 
