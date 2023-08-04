d5c93ba33d0344bfb12415fd632577c639369b75
Matt Knight <mattnite@proton.me>
2023-08-03 21:12:49 -0700

    anyopaque to u8

--- src/modules/cpus/riscv32.zig
+++ src/modules/cpus/riscv32.zig
@@ -47,11 +47,14 @@ pub const REGBYTES = (1 << LOG_REGBYTES);
 pub const startup_logic = struct {
     extern fn microzig_main() noreturn;
 
-    extern var microzig_data_start: anyopaque;
-    extern var microzig_data_end: anyopaque;
-    extern var microzig_bss_start: anyopaque;
-    extern var microzig_bss_end: anyopaque;
-    extern const microzig_data_load_start: anyopaque;
+    // it looks odd to just use a u8 here, but in C it's common to use a
+    // char when linking these values from the linkerscript. What's
+    // important is the addresses of these values.
+    extern var microzig_data_start: u8;
+    extern var microzig_data_end: u8;
+    extern var microzig_bss_start: u8;
+    extern var microzig_bss_end: u8;
+    extern const microzig_data_load_start: u8;
 
     pub fn _start() callconv(.C) noreturn {
 
