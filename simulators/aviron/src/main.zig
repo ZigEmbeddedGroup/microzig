761c2dc86649d4a33facbc54ccb128eedf238ee7
Felix "xq" Queißner <git@random-projects.net>
2023-08-27 23:35:32 +0200

    Adds CI, improves build, fixes elf loading

--- simulators/aviron/src/main.zig
+++ simulators/aviron/src/main.zig
@@ -5,44 +5,31 @@ pub const Cpu = @import("Cpu.zig");
 pub fn main() !void {
     const allocator = std.heap.page_allocator;
 
-    var thing = try std.fs.cwd().openFile("zig-out/bin/aviron-sample-math", .{});
-    defer thing.close();
+    const argv = try std.process.argsAlloc(allocator);
+    defer std.process.argsFree(allocator, argv);
 
-    var source = std.io.StreamSource{ .file = thing };
-    var header = try std.elf.Header.read(&source);
-
-    var strtab: ?struct { usize, usize } = null;
-    var section_it = header.section_header_iterator(&source);
-    while (try section_it.next()) |section| {
-        if (section.sh_type == 0x3) {
-            strtab = .{ section.sh_offset, section.sh_size };
-        }
-    }
-
-    try source.seekTo(strtab.?[0]);
-    var strtab_data = try allocator.alloc(u8, strtab.?[1]);
-    try source.reader().readNoEof(strtab_data);
-
-    var text: ?struct { usize, usize } = null;
-    section_it = header.section_header_iterator(&source);
-    while (try section_it.next()) |section| {
-        const name = std.mem.span(@as([*:0]const u8, @ptrCast(strtab_data[section.sh_name..strtab_data.len].ptr)));
-
-        if (std.mem.eql(u8, name, ".text")) {
-            text = .{ section.sh_offset, section.sh_size };
-        }
-    }
-
-    try source.seekTo(text.?[0]);
-    var text_data = try allocator.alloc(u8, text.?[1]);
-    try source.reader().readNoEof(text_data);
-    std.log.info("{}", .{std.fmt.fmtSliceHexLower(text_data)});
+    if (argv.len != 2) @panic("usage: aviron <elf>");
 
     var flash_storage = Cpu.Flash.Static(32768){};
-    @memcpy(@as([*]u8, @ptrCast((&flash_storage.data).ptr))[0..text_data.len], text_data);
-
     var sram = Cpu.RAM.Static(2048){};
     var eeprom = Cpu.RAM.Static(1024){};
+
+    {
+        var elf_file = try std.fs.cwd().openFile(argv[1], .{});
+        defer elf_file.close();
+
+        var source = std.io.StreamSource{ .file = elf_file };
+        var header = try std.elf.Header.read(&source);
+
+        var pheaders = header.program_header_iterator(&source);
+        while (try pheaders.next()) |phdr| {
+            if ((phdr.p_flags & std.elf.PT_LOAD) == 0)
+                continue; // Header isn't lodead
+
+            try source.seekTo(phdr.p_offset);
+            try source.reader().readNoEof(flash_storage.data[phdr.p_paddr..][0..phdr.p_filesz]);
+        }
+    }
 
     var cpu = Cpu{
         .flash = flash_storage.memory(),
