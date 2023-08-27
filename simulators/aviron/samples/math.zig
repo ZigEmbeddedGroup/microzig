93bad745c18633d51e9b9bbb86c49b50ec675588
Felix "xq" Queißner <git@random-projects.net>
2023-08-27 22:50:34 +0200

    Implements a pluggable memory interface

--- simulators/aviron/samples/math.zig
+++ simulators/aviron/samples/math.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub export fn _start() callconv(.Naked) noreturn {
+pub export fn _start() callconv(.C) noreturn {
     var a: usize = 1 + 2;
 
     for (0..10) |p| {
--- simulators/aviron/src/Cpu.zig
+++ simulators/aviron/src/Cpu.zig
@@ -3,47 +3,30 @@ const isa = @import("isa.zig");
 
 const Cpu = @This();
 
-pub const StatusRegisters = enum(u8) {
-    const Map = std.enums.EnumSet(@This());
-
-    /// Carry Flag
-    c,
-    /// Zero Flag
-    z,
-    /// Negative Flag
-    n,
-    /// Two's Compliment Overflow Flag
-    v,
-    /// Sign Flag, S = N xor V
-    s,
-    /// Half Carry Flag
-    h,
-    /// Copy Storage
-    t,
-    /// Global Interrupt Enable
-    i,
-};
-
-pc: u32 = 0,
+pc: u24 = 0,
 gp_registers: [32]u8 = [1]u8{0} ** 32,
 st_registers: StatusRegisters.Map = StatusRegisters.Map.initEmpty(),
-io_registers: [64]u8 = [1]u8{0} ** 64,
 
-memory: [1024 * 4]u16 = undefined,
+flash: Flash,
+sram: RAM,
+eeprom: EEPROM,
+io: IO,
 
 pub fn getIndirectAddressRegister(cpu: *Cpu, which: enum { x, y, z }) *u16 {
     return @ptrCast(cpu.gp_registers[26 + @intFromEnum(which) * 2][0..2]);
 }
 
-pub fn shiftProgramCounter(cpu: *Cpu, by: i32) void {
-    cpu.pc = @intCast(@as(i33, @intCast(cpu.pc)) + by);
+pub fn shiftProgramCounter(cpu: *Cpu, by: i12) void {
+    cpu.pc = @intCast(@as(i32, @intCast(cpu.pc)) + by);
 }
 
 pub fn run(cpu: *Cpu) !void {
     while (true) : (cpu.pc += 1) {
-        const inst = isa.decode(cpu.memory[cpu.pc]) catch break;
+        const inst = isa.decode(cpu.flash.read(cpu.pc)) catch break;
 
-        std.log.info(" {any}", .{inst});
+        switch (inst) {
+            inline else => |value| std.log.info(" {s} {}", .{ @tagName(inst), value }),
+        }
 
         switch (inst) {
             .movw,
@@ -212,3 +195,187 @@ pub fn run(cpu: *Cpu) !void {
         }
     }
 }
+
+pub const Flash = struct {
+    ctx: ?*anyopaque,
+    vtable: *const VTable,
+    size: usize,
+
+    pub fn read(mem: Flash, addr: u24) u16 {
+        std.debug.assert(addr < mem.size);
+        return mem.vtable.readFn(mem.ctx, addr);
+    }
+
+    pub const VTable = struct {
+        readFn: *const fn (ctx: ?*anyopaque, addr: u24) u16,
+    };
+
+    pub const empty = Flash{
+        .ctx = null,
+        .size = 0,
+        .vtable = &VTable{ .readFn = emptyRead },
+    };
+
+    fn emptyRead(ctx: ?*anyopaque, addr: u24) u16 {
+        _ = addr;
+        _ = ctx;
+        return 0;
+    }
+
+    pub fn Static(comptime size: comptime_int) type {
+        if ((size & 1) != 0)
+            @compileError("size must be a multiple of two!");
+        return struct {
+            const Self = @This();
+
+            data: [size]u8 align(2) = .{0} ** size,
+
+            pub fn memory(self: *Self) Flash {
+                return Flash{
+                    .ctx = self,
+                    .vtable = &vtable,
+                    .size = @divExact(size, 2),
+                };
+            }
+
+            pub const vtable = VTable{ .readFn = memRead };
+
+            fn memRead(ctx: ?*anyopaque, addr: u24) u16 {
+                const mem: *Self = @ptrCast(@alignCast(ctx.?));
+                return std.mem.bytesAsSlice(u16, &mem.data)[addr];
+            }
+        };
+    }
+};
+
+pub const RAM = struct {
+    ctx: ?*anyopaque,
+    vtable: *const VTable,
+    size: usize,
+
+    pub fn read(mem: RAM, addr: u16) u8 {
+        std.debug.assert(addr < mem.size);
+        return mem.vtable.readFn(mem.ctx, addr);
+    }
+
+    pub fn write(mem: RAM, addr: u16, value: u8) void {
+        std.debug.assert(addr < mem.size);
+        return mem.vtable.writeFn(mem.ctx, addr, value);
+    }
+
+    pub const VTable = struct {
+        readFn: *const fn (ctx: ?*anyopaque, addr: u16) u8,
+        writeFn: *const fn (ctx: ?*anyopaque, addr: u16, value: u8) void,
+    };
+
+    pub const empty = RAM{
+        .ctx = null,
+        .size = 0,
+        .vtable = &VTable{ .readFn = emptyRead, .writeFn = emptyWrite },
+    };
+
+    fn emptyRead(ctx: ?*anyopaque, addr: u16) u8 {
+        _ = addr;
+        _ = ctx;
+        return 0;
+    }
+
+    fn emptyWrite(ctx: ?*anyopaque, addr: u16, value: u8) void {
+        _ = value;
+        _ = addr;
+        _ = ctx;
+    }
+
+    pub fn Static(comptime size: comptime_int) type {
+        return struct {
+            const Self = @This();
+
+            data: [size]u8 align(2) = .{0} ** size,
+
+            pub fn memory(self: *Self) RAM {
+                return RAM{
+                    .ctx = self,
+                    .vtable = &vtable,
+                    .size = @divExact(size, 2),
+                };
+            }
+
+            pub const vtable = VTable{
+                .readFn = memRead,
+                .writeFn = memWrite,
+            };
+
+            fn memRead(ctx: ?*anyopaque, addr: u16) u8 {
+                const mem: *Self = @ptrCast(@alignCast(ctx.?));
+                return mem.data[addr];
+            }
+
+            fn memWrite(ctx: ?*anyopaque, addr: u16, value: u8) void {
+                const mem: *Self = @ptrCast(@alignCast(ctx.?));
+                mem.data[addr] = value;
+            }
+        };
+    }
+};
+
+pub const EEPROM = RAM; // actually the same interface *shrug*
+
+pub const IO = struct {
+    ctx: ?*anyopaque,
+    vtable: *const VTable,
+
+    pub fn read(mem: IO, addr: u5) u8 {
+        std.debug.assert(addr < mem.size);
+        return mem.vtable.readFn(mem.ctx, addr);
+    }
+
+    /// `mask` determines which bits of `value` are written. To write everything, use `0xFF` for `mask`.
+    pub fn write(mem: IO, addr: u5, mask: u8, value: u8) void {
+        std.debug.assert(addr < mem.size);
+        return mem.vtable.writeFn(mem.ctx, addr, mask, value);
+    }
+
+    pub const VTable = struct {
+        readFn: *const fn (ctx: ?*anyopaque, addr: u5) u8,
+        writeFn: *const fn (ctx: ?*anyopaque, addr: u5, mask: u8, value: u8) void,
+    };
+
+    pub const empty = IO{
+        .ctx = null,
+        .vtable = &VTable{ .readFn = emptyRead, .writeFn = emptyWrite },
+    };
+
+    fn emptyRead(ctx: ?*anyopaque, addr: u5) u8 {
+        _ = addr;
+        _ = ctx;
+        return 0;
+    }
+
+    fn emptyWrite(ctx: ?*anyopaque, addr: u5, mask: u8, value: u8) void {
+        _ = mask;
+        _ = value;
+        _ = addr;
+        _ = ctx;
+    }
+};
+
+pub const StatusRegisters = enum(u8) {
+    const Map = std.enums.EnumSet(@This());
+
+    /// Carry Flag
+    c,
+    /// Zero Flag
+    z,
+    /// Negative Flag
+    n,
+    /// Two's Compliment Overflow Flag
+    v,
+    /// Sign Flag, S = N xor V
+    s,
+    /// Half Carry Flag
+    h,
+    /// Copy Storage
+    t,
+    /// Global Interrupt Enable
+    i,
+};
