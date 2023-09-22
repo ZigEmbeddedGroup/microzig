dbbbb1392f43eea0047aa1a6d91ae2c87d75ec9b
Felix Queißner <felix@ib-queissner.de>
2023-09-22 09:02:23 +0200

    Microzig Generation 2 Build Interface  (#23)
    * Rework for MicroZig Gen 2 Build
    * Drops CI
    
    ---------
    
    Co-authored-by: Felix "xq" Queißner <git@random-projects.net>

--- build.zig
+++ build.zig
@@ -1,19 +1,65 @@
 const std = @import("std");
-const microzig = @import("microzig");
 
-pub const chips = @import("src/chips.zig");
+fn path(comptime suffix: []const u8) std.Build.LazyPath {
+    return .{
+        .cwd_relative = comptime ((std.fs.path.dirname(@src().file) orelse ".") ++ suffix),
+    };
+}
+
+pub const chips = struct {
+    pub const nrf52840 = .{
+        .preferred_format = .elf,
+        .chip = .{
+            .name = "nrf52840",
+            .url = "https://www.nordicsemi.com/products/nrf52840",
+            .cpu = .cortex_m4,
+            .register_definition = .{
+                .json = path("/src/chips/nrf52840.json"),
+            },
+            .memory_regions = &.{
+                .{ .offset = 0x00000000, .length = 0x100000, .kind = .flash },
+                .{ .offset = 0x20000000, .length = 0x40000, .kind = .ram },
+
+                // EXTFLASH
+                .{ .offset = 0x12000000, .length = 0x8000000, .kind = .flash },
+
+                // CODE_RAM
+                .{ .offset = 0x800000, .length = 0x40000, .kind = .ram },
+            },
+        },
+    };
+
+    pub const nrf52832 = .{
+        .preferred_format = .elf,
+        .chip = .{
+            .name = "nrf52",
+            .url = "https://www.nordicsemi.com/products/nrf52832",
+            .cpu = .cortex_m4,
+            .register_definition = .{
+                .json = path("/src/chips/nrf52.json"),
+            },
+            .memory_regions = &.{
+                .{ .offset = 0x00000000, .length = 0x80000, .kind = .flash },
+                .{ .offset = 0x20000000, .length = 0x10000, .kind = .ram },
+            },
+        },
+    };
+};
+
+pub const boards = struct {
+    pub const nordic = struct {
+        pub const nRF52840_Dongle = .{
+            .preferred_format = .elf,
+            .chip = chips.nrf52840.chip,
+            .board = .{
+                .name = "nRF52840 Dongle",
+                .url = "https://www.nordicsemi.com/Products/Development-hardware/nrf52840-dongle",
+                .source_file = path("/src/boards/nrf52840-dongle.zig"),
+            },
+        };
+    };
+};
 
 pub fn build(b: *std.build.Builder) void {
-    const optimize = b.standardOptimizeOption(.{});
-    inline for (@typeInfo(chips).Struct.decls) |decl| {
-        const exe = microzig.addEmbeddedExecutable(b, .{
-            .name = decl.name ++ ".minimal",
-            .source_file = .{
-                .path = "test/programs/minimal.zig",
-            },
-            .backing = .{ .chip = @field(chips, decl.name) },
-            .optimize = optimize,
-        });
-        exe.installArtifact(b);
-    }
+    _ = b;
 }
