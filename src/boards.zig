597843c4c1759dd62baf0cbec9e4d0a391c4586c
Felix Queißner <felix@ib-queissner.de>
2023-09-22 09:02:37 +0200

    Microzig Generation 2 Build Interface  (#26)
    * Drops microzig dependency
    * Starts to port to MicroZig Build Gen 2
    * Drops CI
    
    ---------
    
    Co-authored-by: Felix "xq" Queißner <git@random-projects.net>

--- src/boards.zig
+++ src/boards.zig
@@ -5,15 +5,3 @@ const chips = @import("chips.zig");
 fn root_dir() []const u8 {
     return std.fs.path.dirname(@src().file) orelse unreachable;
 }
-
-pub const arduino_nano = micro.Board{
-    .name = "Arduino Nano",
-    .source = .{ .path = root_dir() ++ "/boards/arduino_nano.zig" },
-    .chip = chips.atmega328p,
-};
-
-pub const arduino_uno = micro.Board{
-    .name = "Arduino Uno",
-    .source = .{ .path = root_dir() ++ "/boards/arduino_uno.zig" },
-    .chip = chips.atmega328p,
-};
--- src/chips.zig
+++ src/chips.zig
@@ -6,12 +6,3 @@ const MemoryRegion = micro.MemoryRegion;
 fn root_dir() []const u8 {
     return std.fs.path.dirname(@src().file) orelse ".";
 }
-
-pub const atmega328p = Chip.from_standard_paths(root_dir(), .{
-    .name = "ATmega328P",
-    .cpu = micro.cpus.avr5,
-    .memory_regions = &.{
-        MemoryRegion{ .offset = 0x000000, .length = 32 * 1024, .kind = .flash },
-        MemoryRegion{ .offset = 0x800100, .length = 2048, .kind = .ram },
-    },
-});
