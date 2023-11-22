93ddffbcbe7a0b849cca610ee933210280d731b4
Matt Knight <mattnite@proton.me>
2023-11-21 20:20:21 -0800

    fix path

--- build.zig
+++ build.zig
@@ -17,7 +17,7 @@ pub const chips = struct {
         .url = "https://www.microchip.com/en-us/product/ATSAMD51J19A",
         .cpu = .cortex_m4,
         .register_definition = .{
-            .atdf = .{ .path = build_root ++ "src/chips/ATSAMD51J19A.atdf" },
+            .atdf = .{ .path = build_root ++ "/src/chips/ATSAMD51J19A.atdf" },
         },
         .memory_regions = &.{
             .{ .kind = .flash, .offset = 0x00004000, .length = 512 * 1024 }, // Embedded Flash
