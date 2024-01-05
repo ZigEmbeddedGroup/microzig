78def9dc24ba54337d68d6efb46d009e854b4e26
Felix "xq" Queißner <git@random-projects.net>
2024-01-05 09:37:43 +0100

    Adds a nix flake for tooling, clang-format file and formats string.h, stdlib.h

--- include/string.h
+++ include/string.h
@@ -3,28 +3,28 @@
 
 #include <stddef.h>
 
-void * memchr(const void *s, int c, size_t n);
-int    memcmp(const void *s1, const void *s2, size_t n);
-void * memcpy(void * restrict s1, const void * restrict s2, size_t n);
-void * memmove(void *s1, const void *s2, size_t n);
-void * memset(void *s, int c, size_t n);
-char * strcat(char * restrict s1, const char * restrict s2);
-char * strchr( const char *str, int ch );
-int    strcmp(const char *s1, const char *s2);
-char * strcpy(char * restrict s1, const char * restrict s2);
-size_t strcspn(const char *s1, const char *s2);
+void * memchr(void const * s, int c, size_t n);
+int    memcmp(void const * s1, void const * s2, size_t n);
+void * memcpy(void * restrict s1, void const * restrict s2, size_t n);
+void * memmove(void * s1, void const * s2, size_t n);
+void * memset(void * s, int c, size_t n);
+char * strcat(char * restrict s1, char const * restrict s2);
+char * strchr(char const * str, int ch);
+int    strcmp(char const * s1, char const * s2);
+char * strcpy(char * restrict s1, char const * restrict s2);
+size_t strcspn(char const * s1, char const * s2);
 char * strerror(int errnum);
-size_t strlen( const char *str );
-char * strncat(char * restrict s1, const char * restrict s2, size_t n);
-int    strncmp( const char* lhs, const char* rhs, size_t count );
-char * strncpy(char * restrict s1, const char * restrict s2, size_t n);
-char * strpbrk(const char *s1, const char *s2);
-char * strrchr(const char *s, int c);
-size_t strspn(const char *s1, const char *s2);
-char * strstr(const char *s1, const char *s2);
-char * strtok(char * restrict s1, const char * restrict s2);
+size_t strlen(char const * str);
+char * strncat(char * restrict s1, char const * restrict s2, size_t n);
+int    strncmp(char const * lhs, char const * rhs, size_t count);
+char * strncpy(char * restrict s1, char const * restrict s2, size_t n);
+char * strpbrk(char const * s1, char const * s2);
+char * strrchr(char const * s, int c);
+size_t strspn(char const * s1, char const * s2);
+char * strstr(char const * s1, char const * s2);
+char * strtok(char * restrict s1, char const * restrict s2);
 
-// locale dependent: size_t strxfrm(char * restrict s1, const char * restrict s2, size_t n);
-// locale dependent: int strcoll(const char *s1, const char *s2);
+// locale dependent: size_t strxfrm(char * restrict s1, const char * restrict
+// s2, size_t n); locale dependent: int strcoll(const char *s1, const char *s2);
 
 #endif
