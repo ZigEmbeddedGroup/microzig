243c263150e6e0edb8b6dc0617e253ae459f422b
Felix "xq" Queißner <git@random-projects.net>
2024-02-03 10:23:44 +0100

    Ports raspberrypi-rp2040 to microzig-package.json style, reorders fields to have nicer file.

--- examples/raspberrypi-rp2040/build.zig
+++ examples/raspberrypi-rp2040/build.zig
@@ -1,30 +1,37 @@
 const std = @import("std");
-const rp2040 = @import("microzig-bsp-rp2040");
-const MicroZig = @import("microzig");
+const MicroZig = @import("microzig-build");
 
 const available_examples = [_]Example{
-    .{ .name = "pico_adc", .target = "board:raspberry_pi/pico", .file = "src/adc.zig" },
-    .{ .name = "pico_blinky", .target = "board:raspberry_pi/pico", .file = "src/blinky.zig" },
-    // TODO: Fix multicore hal! .{ .name = "pico", .target = "board:raspberry_pi/pico", .file = "src/blinky_core1.zig" },
-    .{ .name = "pico_flash-program", .target = "board:raspberry_pi/pico", .file = "src/flash_program.zig" },
-    .{ .name = "pico_gpio-clk", .target = "board:raspberry_pi/pico", .file = "src/gpio_clk.zig" },
-    .{ .name = "pico_i2c-bus-scan", .target = "board:raspberry_pi/pico", .file = "src/i2c_bus_scan.zig" },
-    .{ .name = "pico_pwm", .target = "board:raspberry_pi/pico", .file = "src/pwm.zig" },
-    .{ .name = "pico_random", .target = "board:raspberry_pi/pico", .file = "src/random.zig" },
-    .{ .name = "pico_spi-master", .target = "board:raspberry_pi/pico", .file = "src/spi_master.zig" },
-    .{ .name = "pico_squarewave", .target = "board:raspberry_pi/pico", .file = "src/squarewave.zig" },
-    .{ .name = "pico_uart", .target = "board:raspberry_pi/pico", .file = "src/uart.zig" },
-    .{ .name = "pico_usb-device", .target = "board:raspberry_pi/pico", .file = "src/usb_device.zig" },
-    .{ .name = "pico_usb-hid", .target = "board:raspberry_pi/pico", .file = "src/usb_hid.zig" },
-    .{ .name = "pico_ws2812", .target = "board:raspberry_pi/pico", .file = "src/ws2812.zig" },
+    // RaspberryPi Boards:
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_adc", .file = "src/adc.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_blinky", .file = "src/blinky.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_flash-program", .file = "src/flash_program.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_gpio-clk", .file = "src/gpio_clk.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_i2c-bus-scan", .file = "src/i2c_bus_scan.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_pwm", .file = "src/pwm.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_random", .file = "src/random.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_spi-master", .file = "src/spi_master.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_squarewave", .file = "src/squarewave.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_uart", .file = "src/uart.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_usb-device", .file = "src/usb_device.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_usb-hid", .file = "src/usb_hid.zig" },
+    .{ .target = "board:raspberry_pi/pico", .name = "pico_ws2812", .file = "src/ws2812.zig" },
+    // TODO: Fix multicore hal! .{ .name = "pico", .target = "board:raspberry_pi/pico" , .file = "src/blinky_core1.zig" },
 
-    .{ .name = "rp2040-matrix_tiles", .target = "board:waveshare/rp2040_matrix", .file = "src/tiles.zig" },
+    // WaveShare Boards:
+    .{ .target = "board:waveshare/rp2040_matrix", .name = "rp2040-matrix_tiles", .file = "src/tiles.zig" },
+    // .{ .target = "board:waveshare/rp2040_eth", .name = "rp2040-eth" },
+    // .{ .target = "board:waveshare/rp2040_plus_4m", .name = "rp2040-plus-4m" },
+    // .{ .target = "board:waveshare/rp2040_plus_16m", .name = "rp2040-plus-16m" },
 };
 
 pub fn build(b: *std.Build) void {
     const microzig = MicroZig.createBuildEnvironment(b, .{});
 
     const optimize = b.standardOptimizeOption(.{});
+
+    const show_targets_step = b.step("show-targets", "Shows all available MicroZig targets");
+    show_targets_step.dependOn(microzig.getShowTargetsStep());
 
     for (available_examples) |example| {
         const target = microzig.findTarget(example.target).?;
