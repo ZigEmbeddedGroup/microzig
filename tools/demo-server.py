2ef68047de25e308ccd43264a81113ed6839e3a8
Felix "xq" Queißner <git@random-projects.net>
2024-02-06 09:20:43 +0100

    Adds up navigation in dev server

--- tools/demo-server.py
+++ tools/demo-server.py
@@ -59,12 +59,18 @@ class Handler(SimpleHTTPRequestHandler):
         r.append('<title>%s</title>\n</head>' % title)
         r.append('<body>\n<h1>%s</h1>' % title)
         r.append('<hr>\n<ul>')
+
+        suffix = path.removeprefix(self.directory)
+        
+        if suffix != "/":
+            r.append('<li><a href="..">..</a></li>')
+
         for name in list:
             fullname = os.path.join(path, name)
             displayname = linkname = name
             if name.startswith("."):
                 # ignore "hidden" directories
-                continue 
+                continue
             # Append / for directories or @ for symbolic links
             if os.path.isdir(fullname):
                 displayname = name + "/"
