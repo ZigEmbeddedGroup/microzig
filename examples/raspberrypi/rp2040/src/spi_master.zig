86503b4184f34e01929c097e1052ee1699e1827f
Hayden Riddiford <hayden@terrakaffe.com>
2024-07-17 12:38:20 -0700

    - Update example with SPI

--- examples/raspberrypi/rp2040/src/spi_master.zig
+++ examples/raspberrypi/rp2040/src/spi_master.zig
@@ -8,19 +8,19 @@ const clocks = rp2040.clocks;
 const peripherals = microzig.chip.peripherals;
 
 const BUF_LEN = 0x100;
-const spi = rp2040.spi.num(0);
+const spi = rp2040.spi.from_instance_number(0);
 
 // Communicate with another RP2040 over spi
 // Slave implementation: https://github.com/raspberrypi/pico-examples/blob/master/spi/spi_master_slave/spi_slave/spi_slave.c
 pub fn main() !void {
-    spi.apply(.{
+    try spi.apply(.{
         .clock_config = rp2040.clock_config,
     });
     var out_buf: [BUF_LEN]u8 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
     var in_buf: [BUF_LEN]u8 = undefined;
 
     while (true) {
-        _ = spi.transceive(&out_buf, &in_buf);
+        _ = spi.transceive(u8, &out_buf, &in_buf);
         time.sleep_ms(1 * 1000);
     }
 }
