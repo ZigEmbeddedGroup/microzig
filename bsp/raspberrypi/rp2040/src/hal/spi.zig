7eb2db0857ee129d0bdd59a6e620673565f4f2b3
Hayden Riddiford <hayden@terrakaffe.com>
2024-07-17 11:04:52 -0700

    - Changed num() to from_instance_number() for clarity - Added support for all SPI modes + bit widths - Implemented TX FIFO "pre-loading" for improved performance

--- bsp/raspberrypi/rp2040/src/hal/spi.zig
+++ bsp/raspberrypi/rp2040/src/hal/spi.zig
@@ -12,19 +12,57 @@ const hw = @import("hw.zig");
 
 const SpiRegs = microzig.chip.types.peripherals.SPI0;
 
-pub const Config = struct {
-    clock_config: clocks.GlobalConfiguration,
-    tx_pin: ?gpio.Pin = gpio.num(19),
-    rx_pin: ?gpio.Pin = gpio.num(16),
-    sck_pin: ?gpio.Pin = gpio.num(18),
-    csn_pin: ?gpio.Pin = gpio.num(17),
-    baud_rate: u32 = 1000 * 1000,
+pub const Motorola = struct {
+    pub const Polarity = enum(u1) {
+        low = 0,
+        high = 1,
+    };
+
+    pub const Phase = enum(u1) {
+        first_edge = 0,
+        second_edge = 1,
+    };
+
+    /// Low means clock is steady state low, high means steady state high
+    clock_polarity: Polarity = .low,
+
+    /// Controls whether data is captured on the first or second clock edge transition
+    clock_phase: Phase = .first_edge,
 };
 
-pub fn num(n: u1) SPI {
-    return @as(SPI, @enumFromInt(n));
+pub const FrameFormat = union(enum) {
+    motorola: Motorola,
+    texas_instruments,
+    ns_microwire,
+};
+
+pub const Config = struct {
+    clock_config: clocks.GlobalConfiguration,
+    /// Number of bits in a frame can't be less than 4 or more than 16
+    data_width: u4 = 8,
+    frame_format: FrameFormat = .{ .motorola = .{} },
+    baud_rate: u32 = 1_000_000,
+};
+
+pub fn from_instance_number(instance_number: u1) SPI {
+    return @as(SPI, @enumFromInt(instance_number));
 }
 
+pub const ConfigError = error{
+    InvalidDataWidth,
+    InputFreqTooLow,
+    InputFreqTooHigh,
+    UnsupportedBaudRate,
+};
+
+/// An API for interacting with the RP2040's SPI driver.
+///
+/// Note: Assumes proper GPIO configuration, does NOT configure GPIO pins.
+///
+/// Features of the peripheral that are explicitly NOT supported by this API are:
+/// - Target mode
+/// - Interrupt Driven/Asynchronous writes/reads
+/// - DMA based writes/reads
 pub const SPI = enum(u1) {
     _,
 
@@ -35,34 +73,76 @@ pub const SPI = enum(u1) {
         };
     }
 
-    pub fn apply(spi: SPI, comptime config: Config) void {
+    /// Initializes the SPI HW block per the Config provided
+    /// Per the API limitations discussed above, the following settings are fixed and not configurable:
+    /// - Controller Mode only
+    /// - DREQ signalling is always enabled, harmless if DMA isn't configured to listen for this
+    pub fn apply(spi: SPI, comptime config: Config) ConfigError!void {
+        if (config.data_width < 4) return ConfigError.InvalidDataWidth;
+
         const peri_freq = config.clock_config.peri.?.output_freq;
-        _ = spi.set_baudrate(config.baud_rate, peri_freq);
+        try spi.set_baudrate(config.baud_rate, peri_freq);
 
         const spi_regs = spi.get_regs();
 
-        // set fromat
-        spi_regs.SSPCR0.modify(.{
-            .DSS = 0b0111, // 8 bits
-            .SPO = 0,
-            .SPH = 0,
-        });
+        switch (config.frame_format) {
+            .motorola => |ff| {
+                spi_regs.SSPCR0.modify(.{
+                    .FRF = 0b00,
+                    .DSS = config.data_width - 1,
+                    .SPO = @intFromEnum(ff.clock_polarity),
+                    .SPH = @intFromEnum(ff.clock_phase),
+                });
+            },
 
-        // Always enable DREQ signals -- harmless if DMA is not listening
+            .texas_instruments => {
+                spi_regs.SSPCR0.modify(.{
+                    .FRF = 0b01,
+                    .DSS = config.data_width - 1,
+                    .SPO = 0,
+                    .SPH = 0,
+                });
+            },
+
+            .ns_microwire => {
+                spi_regs.SSPCR0.modify(.{
+                    .FRF = 0b10,
+                    .DSS = config.data_width - 1,
+                    .SPO = 0,
+                    .SPH = 0,
+                });
+            },
+        }
+
         spi_regs.SSPDMACR.modify(.{
             .TXDMAE = 1,
             .RXDMAE = 1,
         });
 
-        // Finally enable the SPI
+        // Enable SPI
         spi_regs.SSPCR1.modify(.{
             .SSE = 1,
         });
+    }
 
-        if (config.tx_pin) |pin| pin.set_function(.spi);
-        if (config.rx_pin) |pin| pin.set_function(.spi);
-        if (config.sck_pin) |pin| pin.set_function(.spi);
-        if (config.csn_pin) |pin| pin.set_function(.spi);
+    /// Disable SPI and return peripheral registers to reset values
+    pub fn reset(spi: SPI) void {
+        const spi_regs = spi.get_regs();
+        spi_regs.SSPCR1.modify(.{
+            .SSE = 0,
+        });
+        spi_regs.SSPDMACR.modify(.{
+            .TXDMAE = 0,
+            .RXDMAE = 0,
+        });
+        spi_regs.SSPCR0.modify(.{
+            .FRF = 0,
+            .DSS = 0,
+            .SPO = 0,
+            .SPH = 0,
+            .SCR = 0,
+        });
+        spi_regs.SSPCPSR.modify(.{ .CPSDVSR = 0 });
     }
 
     pub inline fn is_writable(spi: SPI) bool {
@@ -72,12 +152,66 @@ pub const SPI = enum(u1) {
     pub inline fn is_readable(spi: SPI) bool {
         return spi.get_regs().SSPSR.read().RNE == 1;
     }
-    pub fn transceive(spi: SPI, src: []const u8, dst: []u8) usize {
+
+    fn validate_bitwidth(PacketType: type) void {
+        const acceptable_types = .{ u4, u5, u6, u7, u8, u9, u10, u11, u12, u13, u14, u15, u16 };
+        for (acceptable_types) |acceptable_type| {
+            if (std.meta.eql(PacketType, acceptable_type)) return;
+        }
+        @compileError("PacketType must be one of u4, u5, ..., u16");
+    }
+
+    const fifo_depth = 8;
+
+    /// Disable SPI, pre-fill the TX FIFO as much as possible, and then re-enable to start transmission.
+    /// Leads to performance gains in thoroughput. Returns how many bytes were consumed from src.
+    fn prime_tx_fifo(spi: SPI, PacketType: type, src: []const PacketType) usize {
+        const spi_regs = spi.get_regs();
+        spi_regs.SSPCR1.modify(.{
+            .SSE = 0,
+        });
+        var tx_remaining = src.len;
+        while (tx_remaining > 0 and spi.is_writable()) {
+            spi_regs.SSPDR.write_raw(src[src.len - tx_remaining]);
+            tx_remaining -= 1;
+        }
+        spi_regs.SSPCR1.modify(.{
+            .SSE = 1,
+        });
+        return src.len - tx_remaining;
+    }
+
+    /// Same as prime_tx_fifo but for a repeated byte.
+    fn prime_tx_fifo_repeated(spi: SPI, PacketType: type, repeated_byte: PacketType, repeat_count: usize) usize {
+        const spi_regs = spi.get_regs();
+        spi_regs.SSPCR1.modify(.{
+            .SSE = 0,
+        });
+        var tx_remaining = repeat_count;
+        while (tx_remaining > 0 and spi.is_writable()) {
+            spi_regs.SSPDR.write_raw(repeated_byte);
+            tx_remaining -= 1;
+        }
+        spi_regs.SSPCR1.modify(.{
+            .SSE = 1,
+        });
+        return repeat_count - tx_remaining;
+    }
+
+    /// Write and read a number of packets. src and dst must be the same length.
+    ///
+    /// PacketType specifies the bit width of each packet using any of
+    /// the types u4, u5, ..., u16. Data truncation is possible if this
+    /// doesn't match the peripheral's configured bit width.
+    pub fn transceive(spi: SPI, PacketType: type, src: []const PacketType, dst: []PacketType) void {
+        comptime validate_bitwidth(PacketType);
+
         const spi_regs = spi.get_regs();
         std.debug.assert(src.len == dst.len);
-        const fifo_depth = 8;
         var rx_remaining = dst.len;
         var tx_remaining = src.len;
+
+        tx_remaining -= spi.prime_tx_fifo(PacketType, src);
 
         while (rx_remaining > 0 or tx_remaining > 0) {
             if (tx_remaining > 0 and spi.is_writable() and rx_remaining < tx_remaining + fifo_depth) {
@@ -85,26 +219,37 @@ pub const SPI = enum(u1) {
                 tx_remaining -= 1;
             }
             if (rx_remaining > 0 and spi.is_readable()) {
-                const bytes = std.mem.asBytes(&spi_regs.SSPDR.read().DATA);
-                dst[dst.len - rx_remaining] = bytes[0];
+                const value: u16 = spi_regs.SSPDR.read().DATA;
+                dst[dst.len - rx_remaining] = @truncate(value);
                 rx_remaining -= 1;
             }
         }
-
-        return src.len;
     }
-    /// Write len bytes directly from src to the SPI, and discard any data received back
-    pub fn write(spi: SPI, src: []const u8) usize {
+
+    /// Write a number of packets and discard any data received back.
+    ///
+    /// PacketType specifies the bit width of each packet using any of
+    /// the types u4, u5, ..., u16. Data truncation is possible if this
+    /// doesn't match the peripheral's configured bit width.
+    pub fn write(spi: SPI, PacketType: type, src: []const PacketType) void {
+        comptime validate_bitwidth(PacketType);
+
+        var tx_remaining = src.len;
+        tx_remaining -= spi.prime_tx_fifo(PacketType, src);
+
         const spi_regs = spi.get_regs();
+
         // Write to TX FIFO whilst ignoring RX, then clean up afterward. When RX
         // is full, PL022 inhibits RX pushes, and sets a sticky flag on
         // push-on-full, but continues shifting. Safe if SSPIMSC_RORIM is not set.
-        for (src) |s| {
+        while (tx_remaining > 0) {
             while (!spi.is_writable()) {
                 hw.tight_loop_contents();
             }
-            spi_regs.SSPDR.write_raw(s);
+            spi_regs.SSPDR.write_raw(src[src.len - tx_remaining]);
+            tx_remaining -= 1;
         }
+
         // Drain RX FIFO, then wait for shifting to finish (which may be *after*
         // TX FIFO drains), then drain RX FIFO again
         while (spi.is_readable()) {
@@ -118,42 +263,52 @@ pub const SPI = enum(u1) {
         }
         // Don't leave overrun flag set
         peripherals.SPI0.SSPICR.modify(.{ .RORIC = 1 });
-        return src.len;
     }
 
-    /// Read len bytes directly from the SPI to dst.
-    /// repeated_tx_data is output repeatedly on SO as data is read in from SI.
-    /// Generally this can be 0, but some devices require a specific value here,
+    /// Read a number of packets while repeatedly sending the data
+    /// packet specified by "repeated_tx_data". Generally this can
+    /// be 0, but some devices require a specific value here,
     /// e.g. SD cards expect 0xff
-    pub fn read(spi: SPI, repeated_tx_data: u8, dst: []u8) usize {
+    ///
+    /// PacketType specifies the bit width of each packet using any of
+    /// the types u4, u5, ..., u16. Data truncation is possible if this
+    /// doesn't match the peripheral's configured bit width.
+    pub fn read(spi: SPI, PacketType: type, repeated_tx_data: PacketType, dst: []PacketType) void {
+        comptime validate_bitwidth(PacketType);
+
         const spi_regs = spi.get_regs();
-        const fifo_depth = 8;
         var rx_remaining = dst.len;
         var tx_remaining = dst.len;
 
+        tx_remaining -= spi.prime_tx_fifo_repeated(PacketType, repeated_tx_data, tx_remaining);
         while (rx_remaining > 0 or tx_remaining > 0) {
             if (tx_remaining > 0 and spi.is_writable() and rx_remaining < tx_remaining + fifo_depth) {
                 spi_regs.SSPDR.write_raw(repeated_tx_data);
                 tx_remaining -= 1;
             }
             if (rx_remaining > 0 and spi.is_readable()) {
-                const bytes = std.mem.asBytes(&spi_regs.SSPDR.read().DATA);
-                dst[dst.len - rx_remaining] = bytes[0];
+                const value: u16 = spi_regs.SSPDR.read().DATA;
+                dst[dst.len - rx_remaining] = @truncate(value);
                 rx_remaining -= 1;
             }
         }
-        return dst.len;
     }
 
-    fn set_baudrate(spi: SPI, baudrate: u32, freq_in: u32) u64 {
+    fn set_baudrate(spi: SPI, baudrate: u32, freq_in: u32) ConfigError!void {
         const spi_regs = spi.get_regs();
+
+        // Clock constraints per the datasheet
+        if (baudrate > 62_500_000) return ConfigError.UnsupportedBaudRate;
+        if (freq_in < 2 * baudrate) return ConfigError.InputFreqTooLow;
+        if ((freq_in / (254 * 256)) > baudrate) return ConfigError.InputFreqTooHigh;
+
         // Find smallest prescale value which puts output frequency in range of
         // post-divide. Prescale is an even number from 2 to 254 inclusive.
         var prescale: u64 = 2;
         while (prescale <= 254) : (prescale += 2) {
             if (freq_in < (prescale + 2) * 256 * baudrate) break;
         }
-        std.debug.assert(prescale <= 254); //Freq too low
+        if (prescale > 254) return ConfigError.InputFreqTooLow;
 
         // Find largest post-divide which makes output <= baudrate. Post-divide is
         // an integer in the range 1 to 256 inclusive.
@@ -163,8 +318,5 @@ pub const SPI = enum(u1) {
         }
         spi_regs.SSPCPSR.modify(.{ .CPSDVSR = @as(u8, @intCast(prescale)) });
         spi_regs.SSPCR0.modify(.{ .SCR = @as(u8, @intCast(postdiv - 1)) });
-
-        // Return the frequency we were able to achieve
-        return freq_in / (prescale * postdiv);
     }
 };
