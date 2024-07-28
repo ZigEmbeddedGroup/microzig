42a23f751fb8edd00e81ff6389f7e6b8967980f1
Hayden Riddiford <hayden@terrakaffe.com>
2024-07-28 14:15:27 -0700

    - Added example of using data width other than 8

--- examples/raspberrypi/rp2040/src/spi_master.zig
+++ examples/raspberrypi/rp2040/src/spi_master.zig
@@ -13,14 +13,32 @@ const spi = rp2040.spi.instance.SPI0;
 // Communicate with another RP2040 over spi
 // Slave implementation: https://github.com/raspberrypi/pico-examples/blob/master/spi/spi_master_slave/spi_slave/spi_slave.c
 pub fn main() !void {
+
+    // 8 bit data words
     try spi.apply(.{
         .clock_config = rp2040.clock_config,
+        .data_width = .eight,
     });
-    var out_buf: [BUF_LEN]u8 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
-    var in_buf: [BUF_LEN]u8 = undefined;
+    var out_buf_eight: [BUF_LEN]u8 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
+    var in_buf_eight: [BUF_LEN]u8 = undefined;
+    _ = spi.transceive_blocking(u8, &out_buf_eight, &in_buf_eight);
 
+    // 12 bit data words
+    try spi.apply(.{
+        .clock_config = rp2040.clock_config,
+        .data_width = .twelve,
+    });
+    var out_buf_twelve: [BUF_LEN]u12 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
+    var in_buf_twelve: [BUF_LEN]u12 = undefined;
+    _ = spi.transceive_blocking(u12, &out_buf_twelve, &in_buf_twelve);
+
+    // Back to 8 bit mode
+    try spi.apply(.{
+        .clock_config = rp2040.clock_config,
+        .data_width = .eight,
+    });
     while (true) {
-        _ = spi.transceive_blocking(u8, &out_buf, &in_buf);
+        _ = spi.transceive_blocking(u8, &out_buf_eight, &in_buf_eight);
         time.sleep_ms(1 * 1000);
     }
 }
