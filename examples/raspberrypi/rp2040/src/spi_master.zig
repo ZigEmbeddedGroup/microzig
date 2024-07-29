c7803810c9ffee4e2a265ab8254cb314181a1632
haydenridd <haydenridd@gmail.com>
2024-07-29 10:07:34 -0700

    RP2040 SPI HAL Improvements (#222)
    - Changed num() to from_instance_number() for clarity
    - Added support for all SPI modes + bit widths
    - Implemented TX FIFO "pre-loading" for improved performance
    - Update example with SPI
    - Renamed functions to _blocking for consistency among HALs
    - Changed instance access style
    - Changed data_width to DataWidthBits to reduce confusion + need for checks
    - Other minor odds and ends per PR review
    - Added example of using data width other than 8

--- examples/raspberrypi/rp2040/src/spi_master.zig
+++ examples/raspberrypi/rp2040/src/spi_master.zig
@@ -8,19 +8,37 @@ const clocks = rp2040.clocks;
 const peripherals = microzig.chip.peripherals;
 
 const BUF_LEN = 0x100;
-const spi = rp2040.spi.num(0);
+const spi = rp2040.spi.instance.SPI0;
 
 // Communicate with another RP2040 over spi
 // Slave implementation: https://github.com/raspberrypi/pico-examples/blob/master/spi/spi_master_slave/spi_slave/spi_slave.c
 pub fn main() !void {
-    spi.apply(.{
-        .clock_config = rp2040.clock_config,
-    });
-    var out_buf: [BUF_LEN]u8 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
-    var in_buf: [BUF_LEN]u8 = undefined;
 
+    // 8 bit data words
+    try spi.apply(.{
+        .clock_config = rp2040.clock_config,
+        .data_width = .eight,
+    });
+    var out_buf_eight: [BUF_LEN]u8 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
+    var in_buf_eight: [BUF_LEN]u8 = undefined;
+    _ = spi.transceive_blocking(u8, &out_buf_eight, &in_buf_eight);
+
+    // 12 bit data words
+    try spi.apply(.{
+        .clock_config = rp2040.clock_config,
+        .data_width = .twelve,
+    });
+    var out_buf_twelve: [BUF_LEN]u12 = .{ 0xAA, 0xBB, 0xCC, 0xDD } ** (BUF_LEN / 4);
+    var in_buf_twelve: [BUF_LEN]u12 = undefined;
+    _ = spi.transceive_blocking(u12, &out_buf_twelve, &in_buf_twelve);
+
+    // Back to 8 bit mode
+    try spi.apply(.{
+        .clock_config = rp2040.clock_config,
+        .data_width = .eight,
+    });
     while (true) {
-        _ = spi.transceive(&out_buf, &in_buf);
+        _ = spi.transceive_blocking(u8, &out_buf_eight, &in_buf_eight);
         time.sleep_ms(1 * 1000);
     }
 }
