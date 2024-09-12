24c2e2ab27333aa1db467f602a65924ded8943cf
Tim Savage <tim@savage.company>
2024-09-13 00:58:17 +1000

    Flesh out the RP2350 bootrom

---
+++ bsp/raspberrypi/rp2040/src/bootroms/RP2350/shared/regs.h
@@ -0,0 +1,10 @@
+#ifndef RP2350_STAGE2_REGS_H
+#define RP2350_STAGE2_REGS_H
+
+#define _u(x) x ## u
+
+#include "pads_qspi.h"
+#include "addressmap.h"
+#include "m33.h"
+
+#endif // RP2350_STAGE2_REGS_H
