a4d5d9294864b70b0c302460f3bafcbdb10436b3
Matt Knight <mattnite@proton.me>
2024-11-01 14:14:59 -0700

    try building website

--- website/build.zig
+++ website/build.zig
@@ -2,12 +2,12 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) void {
-    // zine.scriptyReferenceDocs(b, "content/documentation/scripty/index.md");
     zine.website(b, .{
-        .layouts_dir_path = "layouts",
-        .content_dir_path = "content",
-        .static_dir_path = "static",
-        .host_url = "https://microzig.tech",
         .title = "Zig Embedded Group",
+        .host_url = "https://ZigEmbeddedGroup/github.io/microzig",
+        .content_dir_path = "content",
+        .layouts_dir_path = "layouts",
+        .assets_dir_path = "assets",
+        .output_path_prefix = "www",
     });
 }
