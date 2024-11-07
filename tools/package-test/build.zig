87c250c99675262259bd8afe230c386d0810b79f
Matt Knight <mattnite@proton.me>
2024-11-06 19:30:03 -0800

    fix package test

--- tools/package-test/build.zig
+++ tools/package-test/build.zig
@@ -1,12 +1,13 @@
 const std = @import("std");
 
-pub fn build(b: *std.Build) void {
-    const optimize = b.standardOptimizeOption(.{});
-    const microzig_dep = b.dependency("microzig", .{
-        .optimize = optimize,
-    });
-    b.getInstallStep().dependOn(microzig_dep.builder.getInstallStep());
+const ports = &.{
+    "microzig/port/raspberrypi/rp2xxx",
+};
 
+pub fn build(b: *std.Build) void {
     const test_ports_step = b.step("run-port-tests", "Run all platform agnostic tests for Ports");
-    test_ports_step.dependOn(&microzig_dep.builder.top_level_steps.get("run-port-tests").?.step);
+    for (ports) |port| {
+        const dep = b.dependency(port, .{});
+        test_ports_step.dependOn(&dep.builder.top_level_steps.get("test").?.step);
+    }
 }
