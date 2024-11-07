4101955801ff8ad80af68699974ecb092be93677
Matt Knight <mattnite@proton.me>
2024-11-06 22:11:34 -0800

    update package test

--- tools/package-test/build.zig
+++ tools/package-test/build.zig
@@ -2,11 +2,8 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
-    const microzig_dep = b.dependency("microzig", .{
-        .optimize = optimize,
-    });
-    b.getInstallStep().dependOn(microzig_dep.builder.getInstallStep());
+    _ = optimize;
 
     const test_ports_step = b.step("run-port-tests", "Run all platform agnostic tests for Ports");
-    test_ports_step.dependOn(&microzig_dep.builder.top_level_steps.get("run-port-tests").?.step);
+    _ = test_ports_step;
 }
