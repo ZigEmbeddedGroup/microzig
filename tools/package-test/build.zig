c84a24e24ffc65688e00ce96c9118f38e39a6a3c
Matt Knight <mattnite@proton.me>
2024-11-06 22:28:21 -0800

    add dependency to package test

--- tools/package-test/build.zig
+++ tools/package-test/build.zig
@@ -1,9 +1,15 @@
 const std = @import("std");
 
+const ports: []const []const u8 = &.{
+    "microzig/port/raspberrypi/rp2xxx",
+};
+
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
-    _ = optimize;
-
     const test_ports_step = b.step("run-port-tests", "Run all platform agnostic tests for Ports");
-    _ = test_ports_step;
+
+    for (ports) |port| {
+        const dep = b.dependency(port, .{ .optimize = optimize });
+        test_ports_step.dependOn(&dep.builder.top_level_steps.get("test").?.step);
+    }
 }
