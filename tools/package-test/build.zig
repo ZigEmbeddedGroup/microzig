e58d12c10db68f9a4ee793796922655ec2290b0f
Matt Knight <mattnite@proton.me>
2024-11-06 20:28:29 -0800

    add optimize option

--- tools/package-test/build.zig
+++ tools/package-test/build.zig
@@ -5,10 +5,11 @@ const ports: []const []const u8 = &.{
 };
 
 pub fn build(b: *std.Build) void {
+    const optimize = b.standardOptimizeOption(.{});
     const test_ports_step = b.step("run-port-tests", "Run all platform agnostic tests for Ports");
 
     for (ports) |port| {
-        const dep = b.dependency(port, .{});
+        const dep = b.dependency(port, .{ .optimize = optimize });
         test_ports_step.dependOn(&dep.builder.top_level_steps.get("test").?.step);
     }
 }
