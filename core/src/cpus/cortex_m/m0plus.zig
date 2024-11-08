d6a97c9476c3377877946873cad040f769e42698
Ethan Frei <ethan.frei@gmail.com>
2024-11-08 02:06:41 -0600

    adding nvic for cortex m0+, fixing little bug with stringToEnum (#281)

--- core/src/cpus/cortex_m/m0plus.zig
+++ core/src/cpus/cortex_m/m0plus.zig
@@ -1,3 +1,20 @@
 pub const SystemControlBlock = @compileError("TODO");
-pub const NestedVectorInterruptController = @compileError("TODO");
+
+pub const NestedVectorInterruptController = extern struct {
+    /// Interrupt set registers.
+    ISER: u32,
+    _reserved0: [31]u32,
+    /// Interrupt clear enable registers.
+    ICER: u32,
+    _reserved1: [31]u32,
+    /// Interrupt set pending registers.
+    ISPR: u32,
+    _reserved2: [31]u32,
+    /// Interrupt clear pending registers.
+    ICPR: u32,
+    _reserved3: [31]u32,
+    /// Interrupt priority registers.
+    IPR: [28]u8,
+};
+
 pub const MemoryProtectionUnit = @compileError("TODO");
