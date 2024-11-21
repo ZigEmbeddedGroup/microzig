fbdaff5f9f27a38646ec0381e0522bceacd5b3bb
nosuz <norisuzu@gmail.com>
2024-11-22 08:55:08 +0900

    Added wch/ch32 to build.zig files in the top and examples directory.

--- build.zig
+++ build.zig
@@ -22,6 +22,7 @@ const port_list: []const struct {
     .{ .name = "lpc", .dep_name = "port/nxp/lpc" },
     .{ .name = "rp2xxx", .dep_name = "port/raspberrypi/rp2xxx" },
     .{ .name = "stm32", .dep_name = "port/stmicro/stm32" },
+    .{ .name = "ch32", .dep_name = "port/wch/ch32" },
 };
 
 pub fn build(b: *Build) void {
