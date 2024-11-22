2abb99cf085bf0407a824136bcb674908a33fcb5
Matt Knight <mattnite@proton.me>
2024-11-21 18:19:07 -0800

    fix package test for rp2xxx

--- tools/package-test/build.zig
+++ tools/package-test/build.zig
@@ -1,15 +1,40 @@
 const std = @import("std");
+const microzig = @import("microzig");
 
-const ports: []const []const u8 = &.{
-    "microzig/port/raspberrypi/rp2xxx",
-};
+const MicroBuild = microzig.MicroBuild(.{
+    .rp2xxx = true,
+    .gd32 = true,
+    .atsam = true,
+    .avr = true,
+    .nrf5x = true,
+    .lpc = true,
+    .stm32 = true,
+    .ch32 = true,
+});
 
 pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
-    const test_ports_step = b.step("run-port-tests", "Run all platform agnostic tests for Ports");
 
-    for (ports) |port| {
-        const dep = b.dependency(port, .{ .optimize = optimize });
-        test_ports_step.dependOn(&dep.builder.top_level_steps.get("test").?.step);
+    const mz_dep = b.dependency("microzig", .{});
+    const mb = MicroBuild.init(b, mz_dep);
+
+    const examples: []const Example = &.{
+        .{ .target = mb.ports.rp2xxx.boards.raspberrypi.pico, .name = "rpi_pico" },
+    };
+
+    for (examples) |example| {
+        const firmware = mb.add_firmware(.{
+            .name = example.name,
+            .target = example.target,
+            .optimize = optimize,
+            .root_source_file = b.path("src/empty.zig"),
+        });
+
+        mb.install_firmware(firmware, .{});
     }
 }
+
+const Example = struct {
+    target: *microzig.Target,
+    name: []const u8,
+};
