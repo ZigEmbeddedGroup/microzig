21a087635bc8c492242713b7dc4a3c375dabc21b
RecursiveError <guilhermeSschultz.contato@gmail.com>
2024-12-03 21:54:08 -0300

    fix style

--- drivers/display/hd44780.zig
+++ drivers/display/hd44780.zig
@@ -1,276 +1,244 @@
 const mdf = @import("../framework.zig");
-pub const lcd_delayus_callback = fn (delay: u32) void;
+pub const delayus_callback = fn (delay: u32) void;
 
-pub const Lcd_commands = enum(u8) {
-    lcd_clear = 0x01,
-    lcd_reset = 0x02,
-    lcd_shift_cursor_left = 0x10,
-    lcd_shift_cursor_right = 0x14,
-    lcd_shift_display_left = 0x18,
-    lcd_shift_display_right = 0x1C,
+pub const LcdCommands = enum(u8) {
+    clear = 0x01,
+    reset = 0x02,
+    shift_cursor_left = 0x10,
+    shift_cursor_right = 0x14,
+    shift_display_left = 0x18,
+    shift_display_right = 0x1C,
 };
 
 //lcd "function set" enums
-const Function_set = enum(u8) {
-    lcd_line = 0x8,
-    lcd_bus = 0x10,
-    lcd_char = 0x4,
+const FunctionSet = enum(u8) {
+    line = 0x8,
+    bus = 0x10,
+    char = 0x4,
 };
 
-pub const Lines = enum(u8) { one, two };
-pub const Bussize = enum(u8) { bus4bits, bus8bits };
-pub const Charsize = enum(u8) { char5x8, char5x10 };
+pub const Lines = enum { one, two };
+pub const BusSize = enum { four, eight };
+pub const CharSize = enum { @"5x8", @"5x10" };
 
 //lcd "entry mode" enums
-const Entry_mode = enum(u8) { lcd_shift_mode = 0x1, lcd_shift_dir = 0x2 };
-pub const State = enum(u8) {
+const EntryMode = enum(u8) { shift_mode = 0x1, shift_dir = 0x2 };
+pub const State = enum {
     off,
     on,
 };
 
-pub const ShiftDirection = enum(u8) {
+pub const ShiftDirection = enum {
     dec,
     inc,
 };
 
 //lcd "display control" enums
-const Display_control = enum(u8) { lcd_blink = 0x01, lcd_cursor = 0x02, lcd_display = 0x04 };
+const DisplayControl = enum(u8) {
+    blink = 0x01,
+    cursor = 0x02,
+    display = 0x04,
+};
 
-pub const Config = struct {
+pub const DeviceConfig = struct {
     lines: Lines = .two,
-    bus: Bussize = .bus4bits,
-    char_size: Charsize = .char5x8,
+    bus: BusSize = .four,
+    char_size: CharSize = .@"5x8",
     shift_direction: ShiftDirection = .inc,
     display_shift: State = .off,
     cursor: State = .off,
     cursor_blink: State = .off,
     display: State = .on,
+    skip_begin_delay: bool = false,
 };
-pub const HD44780 = struct {
-    //internal config vars
-    function_set: u8,
-    entry_mode: u8,
-    display_control: u8,
-    enable_set: u8,
-    full_bus: bool,
 
-    //internal_callbacks
-    internal_delay: *const lcd_delayus_callback,
-    io_interface: mdf.base.Datagram_Device,
-
-    //LCD functions
-
-    //create a new LCD without set configs
-    //TODO:  8bits - 11 pin parallel interface
-    fn interface(Self: *HD44780, config: u8, data: u8) void {
-        const pkg: [1]u8 = .{(config & 0b00000111) | (data & 0xF0) | 0x08};
-        Self.io_interface.write(&pkg) catch return; //TODO: return Error
-    }
-
-    fn send8bits(Self: *HD44780, data: u8, rs_state: u1) void {
-        Self.interface(rs_state, data);
-        Self.interface(rs_state | Self.enable_set, data);
-        Self.internal_delay(1);
-        Self.interface(rs_state, data);
-    }
-
-    fn send4bits(Self: *HD44780, data: u8, rs_state: u1) void {
-        const high_nibble: u8 = data & 0xF0;
-        const low_nibble: u8 = data << 4;
-        Self.send8bits(high_nibble, rs_state);
-        Self.internal_delay(1);
-        Self.send8bits(low_nibble, rs_state);
-    }
-
-    //Low level sendfunction
-    pub fn send(Self: *HD44780, data: u8, rs_state: u1) *HD44780 {
-        if (Self.full_bus) {
-            Self.send8bits(data, rs_state);
-        } else {
-            Self.send4bits(data, rs_state);
-        }
-
-        if (rs_state == 0) {
-            Self.internal_delay(40);
-        } else {
-            Self.internal_delay(2);
-        }
-        return Self;
-    }
-    //config functions
-    pub fn set_bus_size(Self: *HD44780, size: Bussize) *HD44780 {
-        if (size == Bussize.bus4bits) {
-            Self.function_set &= ~@intFromEnum(Function_set.lcd_bus);
-            Self.full_bus = false;
-        } else {
-            Self.function_set |= @intFromEnum(Function_set.lcd_bus);
-            Self.full_bus = true;
-        }
-        return Self;
-    }
-
-    pub fn set_char_size(Self: *HD44780, charsize: Charsize) *HD44780 {
-        if (charsize == Charsize.char5x8) {
-            Self.function_set &= ~@intFromEnum(Function_set.lcd_char);
-        } else {
-            Self.function_set |= @intFromEnum(Function_set.lcd_char);
-        }
-        return Self;
-    }
-
-    //========== commands functions ==========
-
-    //TODO: change left/right to dec/inc
-
-    //low level command function
-    pub inline fn command(Self: *HD44780, cmd: Lcd_commands) void {
-        _ = Self.send(@intFromEnum(cmd), 0);
-    }
-
-    pub fn screen_clear(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_clear);
-        Self.internal_delay(1600); //clear and reset need to delay >1.6ms
-        return Self;
-    }
-
-    pub fn reset_cursor(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_reset);
-        Self.internal_delay(1600); //clear and reset need to delay >1.6ms
-        return Self;
-    }
-
-    pub fn shift_cursor_left(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_shift_cursor_left);
-        return Self;
-    }
-
-    pub fn shift_cursor_right(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_shift_cursor_right);
-        return Self;
-    }
-
-    pub fn shift_display_left(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_shift_display_left);
-        return Self;
-    }
-
-    pub fn shift_display_right(Self: *HD44780) *HD44780 {
-        Self.command(Lcd_commands.lcd_shift_display_right);
-        return Self;
-    }
-
-    //control functions
-
-    pub fn shift_control(Self: *HD44780, st: State) *HD44780 {
-        switch (st) {
-            .on => Self.entry_mode |= Entry_mode.lcd_shift_mode,
-            .off => Self.entry_mode &= ~Entry_mode.lcd_shift_mode,
-        }
-        _ = Self.send(Self.entry_mode, 0);
-        return Self;
-    }
-
-    pub fn shift_mode(Self: *HD44780, mode: ShiftDirection) *HD44780 {
-        switch (mode) {
-            .inc => Self.entry_mode |= Entry_mode.lcd_shift_dir,
-            .dec => Self.entry_mode &= ~Entry_mode.lcd_shift_dir,
-        }
-        _ = Self.send(Self.entry_mode, 0);
-        return Self;
-    }
-
-    pub fn display_set(Self: *HD44780, st: State) *HD44780 {
-        switch (st) {
-            .on => Self.display_control |= Display_control.lcd_display,
-            .off => Self.display_control &= ~Display_control.lcd_display,
-        }
-        _ = Self.send(Self.display_control, 0);
-        return Self;
-    }
-
-    pub fn cursor_control(Self: *HD44780, st: State) *HD44780 {
-        switch (st) {
-            .on => Self.display_control |= Display_control.lcd_cursor,
-            .off => Self.display_control &= ~Display_control.lcd_cursor,
-        }
-        _ = Self.send(Self.display_control, 0);
-        return Self;
-    }
-
-    pub fn cursor_blink_control(Self: *HD44780, st: State) *HD44780 {
-        switch (st) {
-            .on => Self.display_control |= Display_control.lcd_blink,
-            .off => Self.display_control &= ~Display_control.lcd_blink,
-        }
-        _ = Self.send(Self.display_control, 0);
-        return Self;
-    }
-
-    pub fn select_all(Self: *HD44780) *HD44780 {
-        Self.enable_set = 0b1100;
-        return Self;
-    }
-    pub inline fn select_lcd(Self: *HD44780, en: u1) *HD44780 {
-        Self.enable_set = 1 << en;
-        return Self;
-    }
-
-    pub fn set_cursor(Self: *HD44780, line: u8, col: u8) *HD44780 {
-        const addrs = [_]u8{ 0x80, 0xC0 };
-        if ((line < 2) and (col < 40)) {
-            _ = Self.send(addrs[line] | col, 0);
-        }
-    }
-
-    pub fn create_custom_char(Self: *HD44780, new_char: [8]u8, mem_addr: u8) *HD44780 {
-        const mem_aux = ((mem_addr & 0b111) << 3) | 0x40;
-        _ = Self.send(mem_aux, 0);
-        for (new_char) |line| {
-            _ = Self.send(line, 1);
-        }
-        return Self;
-    }
-
-    pub fn write(Self: *HD44780, text: []const u8) *HD44780 {
-        for (text) |char| {
-            _ = Self.send(char, 1);
-        }
-        return Self;
-    }
-
-    pub fn init_device(Self: *HD44780, skip_init_time: bool) *HD44780 {
-        if (!skip_init_time) {
-            Self.internal_delay(55000); //wait time = power on time + init time (datasheet: power up time = >40ms | begin time = >15ms)
-        }
-        _ = Self.select_all();
-        Self.send8bits(0x30, 0);
-        Self.internal_delay(4100);
-        Self.send8bits(0x30, 0);
-        Self.internal_delay(100);
-        Self.send8bits(0x30, 0);
-        Self.internal_delay(100);
-        Self.send8bits(0x20, 0);
-        _ = Self.send(Self.function_set, 0);
-        _ = Self.screen_clear().reset_cursor();
-        _ = Self.send(Self.entry_mode, 0);
-        _ = Self.send(Self.display_control, 0);
-        return Self;
-    }
-
-    pub fn init(io_device: mdf.base.Datagram_Device, delay_callback: *const lcd_delayus_callback, config: Config) HD44780 {
-        const function_set = (1 << 5) + (@intFromEnum(config.bus) << 4) + (@intFromEnum(config.lines) << 3) + @intFromEnum(config.char_size) << 2;
-        const entry_mode = (1 << 2) + (@intFromEnum(config.shift_direction) << 1) + @intFromEnum(config.display_shift);
-        const display_control = (1 << 3) + (@intFromEnum(config.display) << 2) + (@intFromEnum(config.cursor) << 1) + @intFromEnum(config.cursor_blink);
-        const bus = !(config.bus == .bus4bits);
-        const lcd = HD44780{
-            .function_set = function_set,
-            .entry_mode = entry_mode,
-            .display_control = display_control,
-            .enable_set = 0b1100,
-            .full_bus = bus,
-            .internal_delay = delay_callback,
-            .io_interface = io_device,
-        };
-        return lcd;
-    }
+pub const HD44780_Config = struct {
+    Datagram_Device: type = mdf.base.Datagram_Device,
 };
+
+pub fn HD44780(comptime config: HD44780_Config) type {
+    return struct {
+        const Self = @This();
+        //internal_callbacks
+        internal_delay: *const delayus_callback,
+        io_interface: config.Datagram_Device,
+
+        //internal Deviceconfig vars
+        function_set: u6 = 0,
+        entry_mode: u3 = 0,
+        display_control: u4 = 0,
+        enable_set: u8 = 0b1100,
+        full_bus: bool = false,
+
+        //LCD functions
+        //TODO:  remove this, implement a driver for PCF8574 i2C I/O expander
+        fn interface(lcd: *Self, lcd_config: u8, data: u8) !void {
+            const pkg: [1]u8 = .{(lcd_config & 0b00000111) | (data & 0xF0) | 0x08};
+            try lcd.io_interface.write(&pkg);
+        }
+
+        fn send8bits(lcd: *Self, data: u8, rs_state: u1) !void {
+            try lcd.interface(rs_state, data);
+            try lcd.interface(rs_state | lcd.enable_set, data);
+            lcd.internal_delay(1);
+            try lcd.interface(rs_state, data);
+        }
+
+        fn send4bits(lcd: *Self, data: u8, rs_state: u1) !void {
+            const high_nibble: u8 = data & 0xF0;
+            const low_nibble: u8 = data << 4;
+            try lcd.send8bits(high_nibble, rs_state);
+            lcd.internal_delay(1);
+            try lcd.send8bits(low_nibble, rs_state);
+        }
+
+        //Low level sendfunction
+        pub fn send(lcd: *Self, data: u8, rs_state: u1) !void {
+            if (lcd.full_bus) {
+                try lcd.send8bits(data, rs_state);
+            } else {
+                try lcd.send4bits(data, rs_state);
+            }
+
+            if (rs_state == 0) {
+                lcd.internal_delay(40);
+            } else {
+                lcd.internal_delay(2);
+            }
+        }
+
+        //========== commands functions ==========
+
+        //TODO: change left/right to dec/inc
+
+        //low level command function
+        pub inline fn command(lcd: *Self, cmd: LcdCommands) !void {
+            try lcd.send(@intFromEnum(cmd), 0);
+        }
+
+        pub fn screen_clear(lcd: *Self) !void {
+            try lcd.command(LcdCommands.clear);
+            lcd.internal_delay(1600); //clear and reset need to delay >1.6ms
+        }
+
+        pub fn reset_cursor(lcd: *Self) !void {
+            try lcd.command(LcdCommands.reset);
+            lcd.internal_delay(1600); //clear and reset need to delay >1.6ms
+        }
+
+        pub fn shift_cursor_left(lcd: *Self) !void {
+            try lcd.command(LcdCommands.shift_cursor_left);
+        }
+
+        pub fn shift_cursor_right(lcd: *Self) !void {
+            try lcd.command(LcdCommands.shift_cursor_right);
+        }
+
+        pub fn shift_display_left(lcd: *Self) !void {
+            try lcd.command(LcdCommands.shift_display_left);
+        }
+
+        pub fn shift_display_right(lcd: *Self) !void {
+            try lcd.command(LcdCommands.shift_display_right);
+        }
+
+        //control functions
+
+        pub fn shift_control(lcd: *Self, st: State) !void {
+            switch (st) {
+                .on => lcd.entryMode |= EntryMode.shift_mode,
+                .off => lcd.entryMode &= ~EntryMode.shift_mode,
+            }
+            try lcd.send(lcd.entryMode, 0);
+        }
+
+        pub fn shift_mode(lcd: *Self, mode: ShiftDirection) !void {
+            switch (mode) {
+                .inc => lcd.entryMode |= EntryMode.shift_dir,
+                .dec => lcd.entryMode &= ~EntryMode.shift_dir,
+            }
+            try lcd.send(lcd.entryMode, 0);
+        }
+
+        pub fn display_set(lcd: *Self, st: State) !void {
+            switch (st) {
+                .on => lcd.displayControl |= DisplayControl.display,
+                .off => lcd.displayControl &= ~DisplayControl.display,
+            }
+            try lcd.send(lcd.displayControl, 0);
+        }
+
+        pub fn cursor_control(lcd: *Self, st: State) !void {
+            switch (st) {
+                .on => lcd.displayControl |= DisplayControl.cursor,
+                .off => lcd.displayControl &= ~DisplayControl.cursor,
+            }
+            try lcd.send(lcd.displayControl, 0);
+        }
+
+        pub fn cursor_blink_control(lcd: *Self, st: State) !void {
+            switch (st) {
+                .on => lcd.displayControl |= DisplayControl.blink,
+                .off => lcd.displayControl &= ~DisplayControl.blink,
+            }
+            try lcd.send(lcd.displayControl, 0);
+        }
+
+        pub fn select_all(lcd: *Self) void {
+            lcd.enable_set = 0b1100;
+        }
+        pub inline fn select_lcd(lcd: *Self, en: u1) !void {
+            lcd.enable_set = 1 << en;
+        }
+
+        pub fn set_cursor(lcd: *Self, line: u8, col: u8) !void {
+            const addrs = [_]u8{ 0x80, 0xC0 };
+            if ((line < 2) and (col < 40)) {
+                try lcd.send(addrs[line] | col, 0);
+            }
+        }
+
+        pub fn create_custom_char(lcd: *Self, new_char: [8]u8, mem_addr: u3) !void {
+            const mem_aux = (@as(u8, mem_addr) << 3) | 0x40;
+            try lcd.send(mem_aux, 0);
+            for (new_char) |line| {
+                try lcd.send(line, 1);
+            }
+        }
+
+        pub fn write(lcd: *Self, text: []const u8) !void {
+            for (text) |char| {
+                try lcd.send(char, 1);
+            }
+        }
+
+        pub fn init_device(lcd: *Self, device_config: DeviceConfig) !void {
+            lcd.function_set = (1 << 5) + (@as(u6, @intFromEnum(device_config.bus)) << 4) + (@as(u6, @intFromEnum(device_config.lines)) << 3) + (@as(u6, @intFromEnum(device_config.char_size)) << 2);
+            lcd.entry_mode = (1 << 2) + (@as(u3, @intFromEnum(device_config.shift_direction)) << 1) + @intFromEnum(device_config.display_shift);
+            lcd.display_control = (1 << 3) + (@as(u4, @intFromEnum(device_config.display)) << 2) + (@as(u4, @intFromEnum(device_config.cursor)) << 1) + @intFromEnum(device_config.cursor_blink);
+            lcd.full_bus = !(device_config.bus == .four);
+            if (!device_config.skip_begin_delay) {
+                lcd.internal_delay(55000); //wait time = power on time + init time (datasheet: power up time = >40ms | begin time = >15ms)
+            }
+            try lcd.send8bits(0x30, 0);
+            lcd.internal_delay(4100);
+            try lcd.send8bits(0x30, 0);
+            lcd.internal_delay(100);
+            try lcd.send8bits(0x30, 0);
+            lcd.internal_delay(100);
+            try lcd.send8bits(0x20, 0);
+            try lcd.send(lcd.function_set, 0);
+            try lcd.screen_clear();
+            try lcd.reset_cursor();
+            try lcd.send(lcd.entry_mode, 0);
+            try lcd.send(lcd.display_control, 0);
+        }
+
+        pub fn init(io_device: config.Datagram_Device, delay_callback: *const delayus_callback) Self {
+            return .{
+                .internal_delay = delay_callback,
+                .io_interface = io_device,
+            };
+        }
+    };
+}
--- examples/raspberrypi/rp2xxx/src/rp2040_only/hd44780.zig
+++ examples/raspberrypi/rp2xxx/src/rp2040_only/hd44780.zig
@@ -1,48 +1,19 @@
 const std = @import("std");
 const microzig = @import("microzig");
 const drivers = microzig.drivers;
+const lcd_driver = drivers.display.hd44780;
 const lcd = drivers.display.HD44780;
-const lcd_config = drivers.display.hd44780.Config;
-const io_device = drivers.base.Datagram_Device;
-const WriteError = io_device.WriteError;
 
 const rp2040 = microzig.hal;
 const i2c = rp2040.i2c;
+const I2C_Device = rp2040.drivers.I2C_Device;
 const gpio = rp2040.gpio;
 const peripherals = microzig.chip.peripherals;
 const timer = rp2040.time;
 
 const i2c0 = i2c.instance.num(0);
 
-pub const i2c_device = struct {
-    i2c_bus: i2c.I2C,
-    addr: i2c.Address,
-    pub fn init(bus: i2c.I2C, addr: i2c.Address) i2c_device {
-        return .{
-            .i2c_bus = bus,
-            .addr = addr,
-        };
-    }
-
-    fn writev(ctx: *anyopaque, datagrams: []const []const u8) WriteError!void {
-        const device: *i2c_device = @ptrCast(@alignCast(ctx));
-        device.i2c_bus.writev_blocking(device.addr, datagrams, null) catch return WriteError.IoError;
-    }
-
-    const vtable = drivers.base.Datagram_Device.VTable{
-        .connect_fn = null,
-        .disconnect_fn = null,
-        .writev_fn = i2c_device.writev,
-        .readv_fn = null,
-    };
-
-    pub fn datagram_device(device: *i2c_device) io_device {
-        return io_device{
-            .object = device,
-            .vtable = &vtable,
-        };
-    }
-};
+const i2c_device = I2C_Device.init(i2c0, @enumFromInt(0x27));
 
 pub fn delay_us(time_delay: u32) void {
     timer.sleep_us(time_delay);
@@ -60,17 +31,18 @@ pub fn main() !void {
     try i2c0.apply(.{
         .clock_config = rp2040.clock_config,
     });
+
     const msg = "hello world - From Zig";
-    var bus = i2c_device.init(i2c0, @enumFromInt(0x27));
-    var my_lcd = lcd.init(
-        bus.datagram_device(),
+    var my_lcd = lcd(.{ .Datagram_Device = I2C_Device }).init(
+        i2c_device,
         delay_us,
-        .{},
     );
-    _ = my_lcd.init_device(false).write(msg);
+
+    try my_lcd.init_device(.{});
+    try my_lcd.write(msg);
 
     while (true) {
-        _ = my_lcd.shift_display_right();
+        try my_lcd.shift_display_right();
         timer.sleep_ms(350);
     }
 }
