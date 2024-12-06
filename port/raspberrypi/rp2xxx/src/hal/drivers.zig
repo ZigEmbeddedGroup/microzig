434ccf628471af536290910a48c13cc6ac45335e
RecursiveError <guilhermeSschultz.contato@gmail.com>
2024-12-06 18:27:28 -0300

    fix i2c read

--- port/raspberrypi/rp2xxx/src/hal/drivers.zig
+++ port/raspberrypi/rp2xxx/src/hal/drivers.zig
@@ -56,7 +56,7 @@ pub const I2C_Device = struct {
         try dev.bus.writev_blocking(dev.address, datagrams, null);
     }
 
-    pub fn read(dev: I2C_Device, datagram: []const u8) !usize {
+    pub fn read(dev: I2C_Device, datagram: []u8) !usize {
         try dev.bus.read_blocking(dev.address, datagram, null);
         return datagram.len;
     }
