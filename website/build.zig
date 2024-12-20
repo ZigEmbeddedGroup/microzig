809777311f7254a8183ed4b0ed7fd40c6a0251d7
Matt Knight <mattnite@proton.me>
2024-12-19 16:14:14 -0800

    revert deletion of url prefix (#322)

--- website/build.zig
+++ website/build.zig
@@ -8,5 +8,6 @@ pub fn build(b: *std.Build) void {
         .content_dir_path = "content",
         .layouts_dir_path = "layouts",
         .assets_dir_path = "assets",
+        .url_path_prefix = "www",
     });
 }
