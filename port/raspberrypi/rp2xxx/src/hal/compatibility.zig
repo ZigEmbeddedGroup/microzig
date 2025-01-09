71b0abf385e92174af755124e5c095a1031abfd9
tact1m4n3 <tudor@dicu.org>
2025-01-09 08:39:59 +0200

    fix ci errors

--- port/raspberrypi/rp2xxx/src/hal/compatibility.zig
+++ port/raspberrypi/rp2xxx/src/hal/compatibility.zig
@@ -1,24 +1,9 @@
 const std = @import("std");
 const microzig = @import("microzig");
 
-pub const Arch = enum {
-    arm,
-    riscv,
-};
-
 pub const Chip = enum {
     RP2040,
     RP2350,
-};
-
-pub const arch: Arch = blk: {
-    if (std.mem.eql(u8, microzig.config.cpu_name, "cortex_m33")) {
-        break :blk .arm;
-    } else if (std.mem.eql(u8, microzig.config.cpu_name, "generic_rv32")) {
-        break :blk .riscv;
-    } else {
-        @compileError(std.fmt.comptimePrint("Unsupported cpu for RP2XXX HAL: \"{s}\"", .{microzig.config.cpu_name}));
-    }
 };
 
 pub const chip: Chip = blk: {
@@ -29,4 +14,22 @@ pub const chip: Chip = blk: {
     } else {
         @compileError(std.fmt.comptimePrint("Unsupported chip for RP2XXX HAL: \"{s}\"", .{microzig.config.chip_name}));
     }
+};
+
+pub const Arch = enum {
+    arm,
+    riscv,
+};
+
+pub const arch: Arch = switch (chip) {
+    .RP2040 => if (std.mem.eql(u8, microzig.config.cpu_name, "cortex_m0plus"))
+        .arm
+    else
+        @compileError(std.fmt.comptimePrint("Unsupported cpu for RP2040: \"{s}\"", .{microzig.config.cpu_name})),
+    .RP2350 => if (std.mem.eql(u8, microzig.config.cpu_name, "cortex_m33"))
+        .arm
+    else if (std.mem.eql(u8, microzig.config.cpu_name, "generic_rv32"))
+        .riscv
+    else
+        @compileError(std.fmt.comptimePrint("Unsupported cpu for RP2350: \"{s}\"", .{microzig.config.cpu_name})),
 };
