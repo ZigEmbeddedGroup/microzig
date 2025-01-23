66b1f254c02866c56c96fdd29d6da66c09d126f0
tact1m4n3 <tudor@dicu.org>
2025-01-23 17:56:52 +0200

    default to secure image mode so it works to flash it

--- port/raspberrypi/rp2xxx/src/hal/image_def.zig
+++ port/raspberrypi/rp2xxx/src/hal/image_def.zig
@@ -13,7 +13,7 @@ const Cpu = enum(u3) {
     riscv = 1,
 };
 
-const security: Security = if (@hasDecl(app, "image_def_security")) app.image_def_security else .non_secure;
+const security: Security = if (@hasDecl(app, "image_def_security")) app.image_def_security else .secure;
 const cpu: Cpu = std.meta.stringToEnum(Cpu, @tagName(arch)).?;
 
 const image_def = init();
@@ -29,7 +29,7 @@ comptime {
 fn init() [5]u32 {
     return .{
         0xffffded3,
-        0x10010142 | (@as(u32, @intFromEnum(security)) << 21) | (@as(u32, @intFromEnum(cpu)) << 24),
+        0x10010142 | (@as(u32, @intFromEnum(security)) << 20) | (@as(u32, @intFromEnum(cpu)) << 24),
         0x000001ff,
         0x00000000,
         0xab123579,
