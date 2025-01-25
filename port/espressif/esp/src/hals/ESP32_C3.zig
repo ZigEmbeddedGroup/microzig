daaf421f95780ab5f963cbde71bd76cb2c6888c5
Grazfather <grazfather@gmail.com>
2025-01-25 14:58:52 -0500

    wip: esp32c3: Add time module

---
+++ port/espressif/esp/src/hals/ESP32_C3.zig
@@ -0,0 +1,6 @@
+const std = @import("std");
+const microzig = @import("microzig");
+
+pub const gpio = @import("esp32-c3/gpio.zig");
+pub const uart = @import("esp32-c3/uart.zig");
+pub const time = @import("esp32-c3/time.zig");
