a413e29de06010330abb0a9860467fc04dec2dbb
Marnix Klooster <marnix@users.noreply.github.com>
2025-02-05 07:05:31 +0100

    STM32F3DISCOVERY: Working blinky (#379)
    This enables the STM32F3DISCOVERY build of blinky,
    which blinks all 8 LEDs simulateously.

--- examples/stmicro/stm32/src/blinky.zig
+++ examples/stmicro/stm32/src/blinky.zig
@@ -20,6 +20,28 @@ pub fn main() !void {
                 pins.led,
             };
             break :res .{ pins, all_leds };
+        } else if (comptime microzig.config.board_name != null and std.mem.eql(u8, microzig.config.board_name.?, "STM32F3DISCOVERY")) {
+            const pins = (stm32.pins.GlobalConfiguration{ .GPIOE = .{
+                .PE8 = .{ .mode = .{ .output = .push_pull } },
+                .PE9 = .{ .mode = .{ .output = .push_pull } },
+                .PE10 = .{ .mode = .{ .output = .push_pull } },
+                .PE11 = .{ .mode = .{ .output = .push_pull } },
+                .PE12 = .{ .mode = .{ .output = .push_pull } },
+                .PE13 = .{ .mode = .{ .output = .push_pull } },
+                .PE14 = .{ .mode = .{ .output = .push_pull } },
+                .PE15 = .{ .mode = .{ .output = .push_pull } },
+            } }).apply();
+            const all_leds = .{
+                pins.PE8,
+                pins.PE9,
+                pins.PE10,
+                pins.PE11,
+                pins.PE12,
+                pins.PE13,
+                pins.PE14,
+                pins.PE15,
+            };
+            break :res .{ pins, all_leds };
         } else {
             @compileError("blinky is not (yet?) implemented for this target");
         }
