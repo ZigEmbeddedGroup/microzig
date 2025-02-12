70cdf78777d170163228b9cebd9cb53134c0666f
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-02-11 23:38:08 -0300

    Add remove and Rename

--- core/src/core/experimental/semihosting.zig
+++ core/src/core/experimental/semihosting.zig
@@ -1,9 +1,42 @@
 const std = @import("std");
 
+//this WriteC and Write0 write direct to the Debug terminal, no context need
+pub const Debug = struct {
+    const Writer = std.io.Writer(void, anyerror, writerfn);
+
+    //this is ssssssslow but WriteC is even more slow and Write0 requires '\0' sentinel
+    fn writerfn(_: void, data: []const u8) anyerror!usize {
+        var len = data.len;
+
+        if (len != 1) {
+            const tmp_c = data[len - 1]; //check if last char is a sentinel
+            if (tmp_c != 0) {
+                len -= 1; //last char is gonna be change to '\0'
+                var tmp_data: []u8 = @constCast(data);
+                tmp_data[len] = 0;
+                write0(@ptrCast(tmp_data.ptr));
+                tmp_data[len] = tmp_c;
+                return len;
+            }
+            write0(@ptrCast(data.ptr));
+            return len;
+        }
+        write_byte(data[0]);
+        return 1;
+    }
+
+    pub fn print(comptime fmt: []const u8, args: anytype) void {
+        const dbg_w = Writer{ .context = {} };
+        dbg_w.print(fmt, args) catch return;
+    }
+};
+
 pub const fs = struct {
     pub const FileError = error{
         OpenFail,
         CloseFail,
+        RenameFail,
+        RemoveFail,
     };
     pub const OpenMode = enum(usize) {
         R,
@@ -25,19 +58,25 @@ pub const fs = struct {
         path_len: usize,
     };
 
-    pub const WriteFile = extern struct {
+    pub const RWFile = extern struct {
         file: File,
-        buf: [*]const u8,
+        buf: [*]u8,
         buf_len: usize,
+    };
+
+    pub const Path = extern struct {
+        path: [*:0]const u8,
+        len: usize,
     };
 
     pub const File = enum(usize) {
         _,
         const Writer = std.io.Writer(File, anyerror, writefn);
+        const Reader = std.io.Reader(File, anyerror, readfn);
         fn writefn(ctx: File, data: []const u8) anyerror!usize {
-            const w_file = WriteFile{
+            const w_file = RWFile{
                 .file = ctx,
-                .buf = data.ptr,
+                .buf = @constCast(data.ptr),
                 .buf_len = data.len,
             };
 
@@ -45,17 +84,42 @@ pub const fs = struct {
             return data.len - ret;
         }
 
+        fn readfn(ctx: File, out: []u8) anyerror!usize {
+            const r_file = RWFile{
+                .file = ctx,
+                .buf = out.ptr,
+                .buf_len = out.len,
+            };
+
+            const ret: usize = @bitCast(sys_read(&r_file));
+            return out.len - ret;
+        }
+
+        pub fn get_writer(file: File) Writer {
+            return Writer{ .context = file };
+        }
+
+        pub fn get_reader(file: File) Reader {
+            return Reader{ .context = file };
+        }
+
+        //Write Functions
+
         pub fn print(file: File, comptime fmt: []const u8, args: anytype) void {
             const wrt = Writer{ .context = file };
             wrt.print(fmt, args) catch return;
         }
+
+        //Read Functions
+
+        //util Functions
 
         pub fn close(file: File) FileError!void {
             return if (sys_close(&file) < 0) FileError.CloseFail else {};
         }
     };
 
-    pub fn open_file(path: [:0]const u8, mode: OpenMode) FileError!File {
+    pub fn open(path: [:0]const u8, mode: OpenMode) FileError!File {
         const file = OpenFile{
             .path = path,
             .mode = mode,
@@ -63,6 +127,24 @@ pub const fs = struct {
         };
         const ret = sys_open(&file);
         return if (ret < 0) FileError.OpenFail else @enumFromInt(@as(usize, @bitCast(ret)));
+    }
+
+    pub fn remove(path: [:0]const u8) FileError!void {
+        const rm = Path{
+            .path = path.ptr,
+            .len = path.len,
+        };
+
+        return if (sys_remove(&rm) < 0) FileError.RemoveFail else {};
+    }
+
+    pub fn rename(old: [:0]const u8, new: [:0]const u8) FileError!void {
+        const re: [2]Path = .{
+            Path{ .path = old.ptr, .len = old.len },
+            Path{ .path = new.ptr, .len = new.len },
+        };
+
+        return if (sys_rename(&re) < 0) FileError.RenameFail else {};
     }
 };
 
@@ -74,6 +156,12 @@ pub const Syscalls = enum(usize) {
     WRITEC = 0x03,
     WRITE0 = 0x04,
     SYS_WRITE = 0x05,
+    SYS_READ = 0x06,
+    SYS_ISTTY = 0x09,
+    SYS_SEEK = 0x0A,
+    SYS_FLEN = 0x0C,
+    SYS_REMOVE = 0x0E,
+    SYS_RENAME = 0x0F,
 };
 
 fn call(number: Syscalls, param: *const anyopaque) isize {
@@ -107,37 +195,18 @@ pub inline fn sys_close(file: *const fs.File) isize {
     return call(.SYS_CLOSE, file);
 }
 
-pub inline fn sys_write(file: *const fs.WriteFile) isize {
+pub inline fn sys_write(file: *const fs.RWFile) isize {
     return call(.SYS_WRITE, file);
 }
 
-//======END BASE SYSCALLS=========
-
-//this WriteC and Write0 write direct to the Debug terminal, no context need
-const DebugWriter = std.io.Writer(void, anyerror, debug_writer);
-
-//this is ssssssslow but WriteC is even more slow and Write0 requires '\0' sentinel
-fn debug_writer(_: void, data: []const u8) anyerror!usize {
-    var len = data.len;
-
-    if (len != 1) {
-        const tmp_c = data[len - 1]; //check if last char is a sentinel
-        if (tmp_c != 0) {
-            len -= 1; //last char is gonna be change to '\0'
-            var tmp_data: []u8 = @constCast(data);
-            tmp_data[len] = 0;
-            write0(@ptrCast(tmp_data.ptr));
-            tmp_data[len] = tmp_c;
-            return len;
-        }
-        write0(@ptrCast(data.ptr));
-        return len;
-    }
-    write_byte(data[0]);
-    return 1;
+pub inline fn sys_read(file: *const fs.RWFile) isize {
+    return call(.SYS_READ, file);
 }
 
-pub fn debug_print(comptime fmt: []const u8, args: anytype) void {
-    const dbg_w = DebugWriter{ .context = {} };
-    dbg_w.print(fmt, args) catch return;
+pub inline fn sys_remove(file: *const fs.Path) isize {
+    return call(.SYS_REMOVE, file);
+}
+
+pub inline fn sys_rename(file: [*]const fs.Path) isize {
+    return call(.SYS_RENAME, file);
 }
--- examples/stmicro/stm32/src/semihosting.zig
+++ examples/stmicro/stm32/src/semihosting.zig
@@ -6,17 +6,22 @@ pub fn main() void {
     const msg1 = "hello";
     const msg2 = "semihosting";
     const file_name = "<path>/foo.txt";
+    const new_name = "<path>/bar.txt";
 
-    semihosting.debug_print("{s} {s}\n", .{ msg1, msg2 });
-    const file = semihosting.fs.open_file(file_name, .W) catch {
-        semihosting.debug_print("fail to open {s}!", .{file_name});
+    semihosting.Debug.print("{s} {s}\n", .{ msg1, msg2 });
+    const file = semihosting.fs.open(file_name, .W) catch {
+        semihosting.Debug.print("fail to open {s}!", .{file_name});
         return;
     };
 
     file.print("Hello File {s} id {d}", .{ file_name, @intFromEnum(file) });
     file.close() catch {
-        semihosting.debug_print("fail to open {s}!", .{file_name});
+        semihosting.Debug.print("fail to close {s}!", .{file_name});
         return;
+    };
+
+    semihosting.fs.rename(file_name, new_name) catch {
+        semihosting.Debug.print("fail to rename {s} to {s}", .{ file_name, new_name });
     };
 
     while (true) {}
