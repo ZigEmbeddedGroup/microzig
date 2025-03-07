aedfc47b5947bdead5ca6735cb86b17b6b5cb2cc
tact1m4n3 <tudor.andrei.dicu@gmail.com>
2025-03-07 12:34:58 +0200

    tweak regz tests

--- tools/regz/src/gen.zig
+++ tools/regz/src/gen.zig
@@ -1563,7 +1563,7 @@ test "gen.interrupts.avr" {
         \\
         \\pub const devices = struct {
         \\    pub const ATmega328P = struct {
-        \\        pub const interrupts: [2]Interrupt = .{
+        \\        pub const interrupts: []Interrupt = &.{
         \\            .{ .name = "TEST_VECTOR1", .index = 1, .description = null },
         \\            .{ .name = "TEST_VECTOR2", .index = 3, .description = null },
         \\        };
