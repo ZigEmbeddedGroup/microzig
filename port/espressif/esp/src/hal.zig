881672bac2d9825c416c5a3c45a0a6b1641d282e
tact1m4n3 <tudor.andrei.dicu@gmail.com>
2025-03-26 09:03:08 +0200

    Import compatibility.zig in hal.zig

--- port/espressif/esp/src/hal.zig
+++ port/espressif/esp/src/hal.zig
@@ -3,3 +3,4 @@ const microzig = @import("microzig");
 
 pub const gpio = @import("hal/gpio.zig");
 pub const uart = @import("hal/uart.zig");
+pub const compatibility = @import("hal/compatibility.zig");
