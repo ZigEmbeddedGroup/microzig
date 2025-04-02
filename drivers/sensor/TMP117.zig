2414e1c1298ba92a82b3a9c85f82fdad572a9912
Grazfather <grazfather@gmail.com>
2025-04-01 23:22:58 -0400

    mostly working

--- drivers/sensor/TMP117.zig
+++ drivers/sensor/TMP117.zig
@@ -9,6 +9,7 @@ const mdf = @import("../framework.zig");
 const Datagram_Device = mdf.base.Datagram_Device;
 
 // TODO: Support other similar temperature sensors. Make this a generic with an enum for the specific?
+// TMP116 is the same, just less accurate. Exact same interface
 pub const TMP117 = struct {
     const Self = @This();
     // TODO: Enum?
@@ -19,42 +20,108 @@ pub const TMP117 = struct {
     const EEPROM_UNLOCK = 0x04;
     const EEPROM1 = 0x05;
     const EEPROM2 = 0x06;
+    // Used to add/subtract from read temperature for calibration
     const TEMPERATURE_OFFSET = 0x07;
     const EEPROM3 = 0x08;
     const DEVICE_ID = 0x0F;
 
     channel: Datagram_Device,
 
+    pub const Configuration = packed struct {
+        reserved0: u1 = 0,
+        Soft_Reset: bool = false,
+        DR: u1 = 0,
+        POL: u1 = 0,
+        TNA: u1 = 0,
+        AVG: u2 = 0,
+        CONV: u3 = 0,
+        MOD: u2 = 0,
+        EEPROM_BUSY: bool = false,
+        Data_Ready: bool = false,
+        LOW_Alert: bool = false,
+        HIGH_Alert: bool = false,
+    };
+
     pub fn init(
         // TODO: Does channel make sense?
-        // SMBus?
         channel: mdf.base.Datagram_Device,
     ) !Self {
         return Self{ .channel = channel };
     }
 
+    pub fn read_raw(self: *const Self, register: u8) !u16 {
+        // TODO: Up to 7? Use an enum?
+        try self.channel.write(&[_]u8{register});
+        var buf: [2]u8 = undefined;
+        const size = try self.channel.read(&buf);
+        // TODO: Better error name
+        if (size != 2) return error.BadRead;
+        return std.mem.readInt(u16, &buf, .big);
+    }
+
+    pub fn write_raw(self: *const Self, register: u8, v: u16) !void {
+        // TODO: Up to 7? Use an enum?
+        // TODO endian-aware int to slice
+        var buf: [2]u8 = undefined;
+        std.mem.writeInt(u16, &buf, v, .big);
+        try self.channel.writev(&[_][]const u8{ &.{register}, &buf });
+    }
+
+    // TODO: Nuke?
     pub fn configure_raw(self: *const Self, config: u16) !void {
-        const bytes = [_]u8{ Self.CONFIGURATION, @truncate(config), @truncate(config >> 8) };
-        try self.channel.write(&bytes);
+        return self.write_raw(Self.CONFIGURATION, config);
+    }
+
+    pub fn write_configuration(self: *const Self, config: Configuration) !void {
+        try self.write_raw(Self.CONFIGURATION, @bitCast(config));
+    }
+
+    pub fn read_configuration(self: *const Self) !Configuration {
+        const v = try self.read_raw(Self.CONFIGURATION);
+        return @bitCast(v);
     }
 
     pub fn set_high_limit(self: *const Self, temp_c: f32) !void {
-        const limit = try get_temperature_units(temp_c);
-        const bytes = [_]u8{ Self.TEMPERATURE_HIGH, @truncate(limit), @truncate(limit >> 8) };
-        try self.channel.write(&bytes);
+        const limit = try from_temp(temp_c);
+        try self.write_raw(Self.TEMPERATURE_HIGH, limit);
     }
 
     pub fn set_low_limit(self: *const Self, temp_c: f32) !void {
-        const limit = try get_temperature_units(temp_c);
-        // var bytes = [3]u8{ Self.TEMPERATURE_LOW, 0, 0 };
-        const bytes = [_]u8{Self.TEMPERATURE_LOW} ++ std.mem.toBytes(limit);
-        std.mem.writeInt(u16, bytes[1..], limit, .little);
-        try self.channel.write(&bytes);
+        const limit = try from_temp(temp_c);
+        try self.write_raw(Self.TEMPERATURE_LOW, limit);
     }
 
-    pub fn get_temperature_units(temp_c: f32) !u16 {
+    pub fn read_temperature(self: *const Self) !f32 {
+        const units: i16 = @bitCast(try self.read_raw(Self.TEMPERATURE_RESULT));
+        return to_temp(units);
+    }
+
+    pub fn read_temperature_f(self: *const Self) !f32 {
+        const temp_c = try self.read_temperature();
+        return Self.c_to_f(temp_c);
+    }
+
+    pub const DeviceId = packed struct {
+        revision: u4,
+        device_id: u12,
+    };
+
+    pub fn read_device_id(self: *const Self) !DeviceId {
+        const did = try self.read_raw(Self.DEVICE_ID);
+        return @bitCast(did);
+    }
+
+    fn from_temp(temp_c: f32) !u16 {
         if (temp_c > 256 or temp_c < -256)
             return error.TemperatureOutOfRange;
-        return @intFromFloat(temp_c * 7.8125E-6);
+        return @intFromFloat(temp_c / 7.8125E-3);
+    }
+
+    pub fn to_temp(units: i16) f32 {
+        return @as(f32, @floatFromInt(units)) * 7.8125E-3;
+    }
+
+    pub fn c_to_f(temp_c: f32) f32 {
+        return (temp_c * 9 / 5) + 32;
     }
 };
