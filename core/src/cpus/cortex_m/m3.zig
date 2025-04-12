efccb04be04ae262633d52671664199d46367a82
curuvar <58759586+curuvar@users.noreply.github.com>
2025-04-12 18:07:32 -0400

    Set default for moving RP2xxx vectors to ram back to false (don't move).

--- core/src/cpus/cortex_m/m3.zig
+++ core/src/cpus/cortex_m/m3.zig
@@ -3,7 +3,7 @@ const mmio = microzig.mmio;
 
 pub const PlatformOptions = struct {
     /// When true, interrupt vectors are moved to RAM so handlers can be set at runtime.
-    ram_vectors: bool = true,
+    ram_vectors: bool = false,
     /// When true, the RAM vectors are placed in section `ram_vectors`.
     has_ram_vectors_section: bool = false,
 };
