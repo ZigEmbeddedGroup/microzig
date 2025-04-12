64c3a31bce98813009b61e60ad0bedd1d4b3d634
curuvar <58759586+curuvar@users.noreply.github.com>
2025-04-12 00:00:04 -0400

    Modified raspi interrupt example to work with new interrupt handling

--- examples/raspberrypi/rp2xxx/src/interrupts.zig
+++ examples/raspberrypi/rp2xxx/src/interrupts.zig
@@ -10,13 +10,29 @@ const uart = rp2xxx.uart.instance.num(0);
 const baud_rate = 115200;
 const uart_tx_pin = rp2xxx.gpio.num(0);
 
-pub const microzig_options: microzig.Options = .{
+const chip = rp2xxx.compatibility.chip;
+
+const timer = if (chip == .RP2040) peripherals.TIMER else peripherals.TIMER0;
+const timer_irq = if (chip == .RP2040) .TIMER_IRQ_0 else .TIMER0_IRQ_0;
+
+
+pub const rp2040_options: microzig.Options = .{
     .log_level = .debug,
     .logFn = rp2xxx.uart.logFn,
     .interrupts = .{
-        .TIMER_IRQ_0 = .{ .c = timer_interrupt },
-    },
+        .TIMER_IRQ_0 = .{ .c = timer_interrupt }
+     },
 };
+
+pub const rp2350_options: microzig.Options = .{
+    .log_level = .debug,
+    .logFn = rp2xxx.uart.logFn,
+    .interrupts = .{
+        .TIMER0_IRQ_0= .{ .c = timer_interrupt }
+     },
+};
+
+pub const microzig_options = if (chip == .RP2040) rp2040_options else rp2350_options;
 
 fn timer_interrupt() callconv(.c) void {
     const cs = microzig.interrupt.enter_critical_section();
@@ -25,11 +41,8 @@ fn timer_interrupt() callconv(.c) void {
     std.log.info("toggle led!", .{});
     led.toggle();
 
-    switch (rp2xxx.compatibility.chip) {
-        // These registers are write-to-clear
-        .RP2040 => peripherals.TIMER.INTR.modify(.{ .ALARM_0 = 1 }),
-        .RP2350 => peripherals.TIMER0.INTR.modify(.{ .ALARM_0 = 1 }),
-    }
+    timer.INTR.modify(.{ .ALARM_0 = 1 });
+
     set_alarm(1_000_000);
 }
 
@@ -37,10 +50,8 @@ pub fn set_alarm(us: u32) void {
     const Duration = microzig.drivers.time.Duration;
     const current = time.get_time_since_boot();
     const target = current.add_duration(Duration.from_us(us));
-    switch (rp2xxx.compatibility.chip) {
-        .RP2040 => peripherals.TIMER.ALARM0.write_raw(@intCast(@intFromEnum(target) & 0xffffffff)),
-        .RP2350 => peripherals.TIMER0.ALARM0.write_raw(@intCast(@intFromEnum(target) & 0xffffffff)),
-    }
+
+    timer.ALARM0.write_raw(@intCast(@intFromEnum(target) & 0xffffffff));
 }
 
 pub fn main() !void {
@@ -59,22 +70,9 @@ pub fn main() !void {
 
     set_alarm(1_000_000);
 
-    switch (rp2xxx.compatibility.chip) {
-        .RP2040 => {
-            peripherals.TIMER.INTE.toggle(.{ .ALARM_0 = 1 });
-            interrupt.enable(.TIMER_IRQ_0);
-        },
-        .RP2350 => {
-            peripherals.TIMER0.INTE.toggle(.{ .ALARM_0 = 1 });
-            switch (rp2xxx.compatibility.arch) {
-                .arm => interrupt.enable(.TIMER0_IRQ_0),
-                .riscv => {
-                    interrupt.enable(.MachineExternal);
-                    interrupt.external.enable(.TIMER0_IRQ_0);
-                },
-            }
-        },
-    }
+    timer.INTE.toggle(.{ .ALARM_0 = 1 });
+
+    interrupt.enable(timer_irq);
 
     microzig.cpu.interrupt.enable_interrupts();
 
