c9f031e37f298209843f8b2b990a08762927633b
Grazfather <grazfather@gmail.com>
2025-04-12 12:38:28 -0400

    cleanup

--- port/espressif/esp/src/hal/time.zig
+++ port/espressif/esp/src/hal/time.zig
@@ -3,6 +3,7 @@ const microzig = @import("microzig");
 const time = microzig.drivers.time;
 
 // NOTE: esp32c3 specific
+const SYSTEM = microzig.chip.peripherals.SYSTEM;
 const SYSTIMER = microzig.chip.peripherals.SYSTIMER;
 
 pub fn get_time_since_boot() time.Absolute {
@@ -20,8 +21,14 @@ pub fn sleep_us(time_us: u64) void {
 
 fn get_systimer_counter(counter_id: u32) u64 {
     // TODO: We need to put it somewhere else
+    // This is 1 on reset, but let's set it anyway
+    SYSTEM.PERIP_CLK_EN0.modify(.{
+        .SYSTIMER_CLK_EN = 1,
+    });
     SYSTIMER.CONF.modify(.{
+        // Resets as 1 anyway
         .TIMER_UNIT0_WORK_EN = 1,
+        // Needs explicit setting
         .CLK_EN = 1,
     });
 
@@ -31,24 +38,20 @@ fn get_systimer_counter(counter_id: u32) u64 {
     const LO = &SYSTIMER.UNIT0_VALUE_LO;
     const HI = &SYSTIMER.UNIT0_VALUE_HI;
 
-    var lo: u32 = 1; // Make it not match for now in case lo_start raeds as 0
-    var lo_start: u32 = 0;
-    var hi: u20 = 0;
     // Set the "update" bit and wait for acknowledgment
     OP.modify(.{ .TIMER_UNIT0_UPDATE = 1 });
     while (OP.read().TIMER_UNIT0_VALUE_VALID != 1) {}
-    // Read LO, HI, then LO again, check that LO returns the same value.
-    // This accounts for the case when an interrupt may happen between reading
-    // HI and LO values, and this function may get called from the ISR.
-    // In this case, the repeated read will return consistent values.
-    lo_start = LO.read().TIMER_UNIT0_VALUE_LO; // 32 bits
-    while (lo_start != lo) {
-        lo = lo_start;
-        hi = HI.read().TIMER_UNIT0_VALUE_HI; // 20 bits
+    // Read LO, HI, then LO again, check that LO returns the same value. This accounts for the case
+    // when an interrupt may happen between reading HI and LO values, and this function may get
+    // called from the ISR. In this case, the repeated read will return consistent values.
+    var lo_start = LO.read().TIMER_UNIT0_VALUE_LO; // 32 bits
+    while (true) {
+        const lo = lo_start;
+        const hi = HI.read().TIMER_UNIT0_VALUE_HI;
         lo_start = LO.read().TIMER_UNIT0_VALUE_LO;
-    }
 
-    return (@as(u64, hi) << 32) | lo;
+        if (lo_start == lo) return (@as(u64, hi) << 32) | lo;
+    }
 }
 
 inline fn ticks_to_us(ticks: u64) u64 {
