263691b42d1bb54cb833a310a0afb936d3a16023
Grazfather <grazfather@gmail.com>
2025-04-19 10:28:12 -0400

    logging in test

--- examples/raspberrypi/rp2xxx/src/blinky.zig
+++ examples/raspberrypi/rp2xxx/src/blinky.zig
@@ -2,6 +2,7 @@ const std = @import("std");
 const microzig = @import("microzig");
 const rp2xxx = microzig.hal;
 const time = rp2xxx.time;
+const gpio = rp2xxx.gpio;
 
 const pin_config = rp2xxx.pins.GlobalConfiguration{
     .GPIO25 = .{
@@ -11,12 +12,36 @@ const pin_config = rp2xxx.pins.GlobalConfiguration{
 };
 
 const pins = pin_config.pins();
+const uart = rp2xxx.uart.instance.num(0);
+const baud_rate = 115200;
+const uart_tx_pin = gpio.num(0);
+
+pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
+    std.log.err("panic: {s}", .{message});
+    @breakpoint();
+    while (true) {}
+}
+
+pub const microzig_options = microzig.Options{
+    .log_level = .debug,
+    .logFn = rp2xxx.uart.logFn,
+};
 
 pub fn main() !void {
+    uart_tx_pin.set_function(.uart);
+
+    uart.apply(.{
+        .baud_rate = baud_rate,
+        .clock_config = rp2xxx.clock_config,
+    });
+
+    rp2xxx.uart.init_logger(uart);
+
     pin_config.apply();
 
     while (true) {
         pins.led.toggle();
-        time.sleep_ms(250);
+        time.sleep_ms(500);
+        std.log.info("hello", .{});
     }
 }
