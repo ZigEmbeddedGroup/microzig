2da9dceb6d55445f62b20e3ae550464a05c003c2
Matt Knight <mattnite@proton.me>
2025-04-20 10:03:44 -0700

    Make github action for running driver unit tests (#467)

--- drivers/build.zig
+++ drivers/build.zig
@@ -1,17 +1,21 @@
 const std = @import("std");
 
 pub fn build(b: *std.Build) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
     const drivers_mod = b.addModule("drivers", .{
         .root_source_file = b.path("framework.zig"),
+        .target = target,
+        .optimize = optimize,
     });
-
-    _ = drivers_mod;
 
     const test_suite = b.addTest(.{
-        .root_source_file = b.path("framework.zig"),
+        .root_module = drivers_mod,
         .target = b.graph.host,
-        .optimize = .Debug,
     });
 
-    b.getInstallStep().dependOn(&b.addRunArtifact(test_suite).step);
+    const run_tests = b.addRunArtifact(test_suite);
+    const test_step = b.step("test", "Run unit tests");
+    test_step.dependOn(&run_tests.step);
 }
