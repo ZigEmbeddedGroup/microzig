5f0212b77486b232874e78b66476ce85f74e5dab
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-04-23 16:58:55 -0300

    Add I2C_Device to drivers

--- port/stmicro/stm32/src/hals/STM32F103/drivers.zig
+++ port/stmicro/stm32/src/hals/STM32F103/drivers.zig
@@ -1,3 +1,13 @@
+const std = @import("std");
+const microzig = @import("microzig");
+const I2c = @import("i2c.zig");
+const drivers = microzig.drivers.base;
+
+const Datagram_Device = drivers.Datagram_Device;
+const Stream_Device = drivers.Stream_Device;
+const Digital_IO = drivers.Digital_IO;
+const Clock_Device = drivers.Clock_Device;
+
 pub const CounterDevice = struct {
     us_psc: u32,
     ms_psc: u32,
@@ -35,5 +45,101 @@ pub const Timeout = struct {
 
     pub fn check_timeout(self: *const Timeout) bool {
         return self.check_event(self.ctx);
+    }
+};
+
+pub const I2C_Device = struct {
+    pub const ConnectError = Datagram_Device.ConnectError;
+    pub const WriteError = Datagram_Device.WriteError;
+    pub const ReadError = Datagram_Device.ReadError;
+
+    /// Selects I²C bus should be used.
+    bus: I2c.I2C,
+
+    /// The address of our I²C device.
+    address: I2c.Address,
+
+    //for the Datagram device
+    config: I2c.Config,
+    counter_device: ?*const CounterDevice,
+    timeout: u16,
+    pub fn init(bus: I2c.I2C, address: I2c.Address, config: I2c.Config, counter: ?*CounterDevice, timeout: ?u16) I2C_Device {
+        return .{
+            .bus = bus,
+            .address = address,
+            .config = config,
+            .counter_device = counter,
+            .timeout = timeout orelse 1000,
+        };
+    }
+
+    pub fn datagram_device(dev: *const I2C_Device) Datagram_Device {
+        dev.bus.runtime_apply(dev.config) catch {};
+        return .{
+            .ptr = @constCast(dev),
+            .vtable = &vtable,
+        };
+    }
+
+    pub fn write(dev: I2C_Device, datagram: []const u8, timeout: ?Timeout) !void {
+        try dev.bus.write_blocking(dev.address, datagram, timeout);
+    }
+
+    pub fn writev(dev: I2C_Device, datagrams: []const []const u8, timeout: ?Timeout) !void {
+        try dev.bus.writev_blocking(dev.address, datagrams, timeout);
+    }
+
+    pub fn read(dev: I2C_Device, datagram: []u8, timeout: ?Timeout) !usize {
+        try dev.bus.read_blocking(dev.address, datagram, timeout);
+        return datagram.len;
+    }
+
+    pub fn readv(dev: I2C_Device, datagrams: []const []u8, timeout: ?Timeout) !usize {
+        try dev.bus.readv_blocking(dev.address, datagrams, timeout);
+        return microzig.utilities.Slice_Vector([]u8).init(datagrams).size();
+    }
+
+    const vtable = Datagram_Device.VTable{
+        .connect_fn = null,
+        .disconnect_fn = null,
+        .writev_fn = writev_fn,
+        .readv_fn = readv_fn,
+    };
+
+    fn writev_fn(dd: *anyopaque, chunks: []const []const u8) WriteError!void {
+        const dev: *I2C_Device = @ptrCast(@alignCast(dd));
+        const timeout = if (dev.counter_device) |counter| counter.make_ms_timeout(dev.timeout) else null;
+        return dev.writev(chunks, timeout) catch |err| switch (err) {
+            I2c.IOError.AckFailure,
+            I2c.IOError.BusError,
+            I2c.IOError.BusTimeout,
+            I2c.IOError.ArbitrationLoss,
+            => return error.IoError,
+
+            I2c.IOError.Timeout => return error.Timeout,
+            I2c.IOError.UnrecoverableError => {
+                dev.bus.runtime_apply(dev.config) catch {};
+                return error.IoError;
+            },
+        };
+    }
+
+    fn readv_fn(dd: *anyopaque, chunks: []const []u8) ReadError!usize {
+        const dev: *I2C_Device = @ptrCast(@alignCast(dd));
+        const timeout = if (dev.counter_device) |counter| counter.make_ms_timeout(dev.timeout) else null;
+
+        return dev.readv(chunks, timeout) catch |err| switch (err) {
+            I2c.IOError.AckFailure,
+            I2c.IOError.BusError,
+            I2c.IOError.BusTimeout,
+            I2c.IOError.ArbitrationLoss,
+            => return error.IoError,
+
+            I2c.IOError.Timeout => return error.Timeout,
+            I2c.IOError.UnrecoverableError => {
+                dev.bus.runtime_apply(dev.config) catch {};
+                return error.IoError;
+            },
+        };
     }
 };
--- port/stmicro/stm32/src/hals/STM32F103/hal.zig
+++ port/stmicro/stm32/src/hals/STM32F103/hal.zig
@@ -2,6 +2,6 @@ pub const pins = @import("pins.zig");
 pub const gpio = @import("gpio.zig");
 pub const uart = @import("uart.zig");
 pub const i2c = @import("i2c.zig");
-pub const driver = @import("drivers.zig");
+pub const drivers = @import("drivers.zig");
 pub const timer = @import("timer.zig");
 pub fn init() void {}
