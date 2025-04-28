207dbc295d9a584e4000b2319a1e6df0e498222f
curuvar <58759586+curuvar@users.noreply.github.com>
2025-04-28 07:32:11 -0400

    at mmio to match m0+

--- core/src/cpus/cortex_m/m33.zig
+++ core/src/cpus/cortex_m/m33.zig
@@ -14,13 +14,132 @@ pub const SystemControlBlock = extern struct {
     /// CPUID Base Register.
     CPUID: u32,
     /// Interrupt Control and State Register.
-    ICSR: u32,
+    ICSR: mmio.Mmio(packed struct(u32) {
+        /// Contains the active exception number:
+        /// 0 = Thread mode
+        /// Nonzero = The exception number[a] of the currently active exception.
+        VECTACTIVE: u9,
+        reserved0: u3,
+        /// Indicates the exception number of the highest priority pending enabled exception:
+        /// 0 = no pending exceptions
+        /// Nonzero = the exception number of the highest priority pending enabled exception.
+        VECTPENDING: u9,
+        reserved1: u1,
+        /// Interrupt pending flag, excluding NMI and Faults:
+        /// 0 = interrupt not pending
+        /// 1 = interrupt pending.
+        ISRPENDING: u1,
+        /// Indicates whether a pending exception will be serviced on exit from debug
+        /// halt state:
+        /// 0 = will not be serviced
+        /// 1 = will be serviced.
+        ISRPREEMPT: u1,
+        reserved2: u1,
+        /// SysTick exception clear-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect
+        /// 1 = removes the pending state from the SysTick exception.
+        ///
+        /// This bit is WO. On a register read its value is Unknown.
+        /// If your device does not implement the SysTick timer, this bit is Reserved.
+        PENDSTCLR: u1,
+        /// SysTick exception set-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect1 = changes SysTick exception state to pending.
+        /// Read:
+        /// 0 = SysTick exception is not pending
+        /// 1 = SysTick exception is pending.
+        ///
+        /// If your device does not implement the SysTick timer, this bit is Reserved.
+        PENDSTSET: u1,
+        /// PendSV clear-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect
+        /// 1 = removes the pending state from the PendSV exception.
+        PENDSVCLR: u1,
+        /// PendSV set-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect
+        /// 1 = changes PendSV exception state to pending.
+        /// Read:
+        /// 0 = PendSV exception is not pending
+        /// 1 = PendSV exception is pending.
+        ///
+        /// Writing 1 to this bit is the only way to set the PendSV exception state to pending.
+        PENDSVSET: u1,
+        reserved3: u2,
+        /// NMI set-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect
+        /// 1 = changes NMI exception state to pending.
+        /// Read:
+        /// 0 = NMI exception is not pending
+        /// 1 = NMI exception is pending.
+        ///
+        /// Because NMI is the highest-priority exception, normally the processor enters the NMI
+        /// exception handler as soon as it detects a write of 1 to this bit. Entering the handler
+        /// then clears this bit to 0. This means a read of this bit by the NMI exception handler
+        /// returns 1 only if the NMI signal is reasserted while the processor is executing that handler.
+        NMIPENDSET: u1,
+    }),
     /// Vector Table Offset Register.
     VTOR: u32,
     /// Application Interrupt and Reset Control Register.
-    AIRCR: u32,
+    AIRCR: mmio.Mmio(packed struct {
+        reserved0: u1,
+        /// Reserved for debug use. This bit reads as 0. When writing to the register you must
+        /// write 0 to this bit, otherwise behavior is Unpredictable.
+        VECTCLRACTIVE: u1,
+        /// System reset request:
+        /// 0 = no effect
+        /// 1 = requests a system level reset.
+        ///
+        /// This bit reads as 0.
+        SYSRESETREQ: u1,
+        reserved1: u12,
+        /// Data endianness implemented:
+        /// 0 = Little-endian
+        /// 1 = Big-endian.
+        ENDIANESS: u1,
+        /// Register key:
+        /// Reads as Unknown
+        /// On writes, write 0x05FA to VECTKEY, otherwise the write is ignored.
+        VECTKEY: u16,
+    }),
     /// System Control Register.
-    SCR: u32,
+    SCR: mmio.Mmio(packed struct(u32) {
+        reserved0: u1,
+        /// Indicates sleep-on-exit when returning from Handler mode to Thread mode:
+        /// 0 = do not sleep when returning to Thread mode.
+        /// 1 = enter sleep, or deep sleep, on return from an ISR to Thread mode.
+        ///
+        /// Setting this bit to 1 enables an interrupt driven application to avoid returning
+        /// to an empty main application.
+        SLEEPONEXIT: u1,
+        /// Controls whether the processor uses sleep or deep sleep as its low power mode:
+        /// 0 = sleep
+        /// 1 = deep sleep.
+        ///
+        /// If your device does not support two sleep modes, the effect of changing the value
+        /// of this bit is implementation-defined.
+        SLEEPDEEP: u1,
+        reserved1: u1,
+        /// Send Event on Pending bit:
+        /// 0 = only enabled interrupts or events can wakeup the processor, disabled interrupts are excluded
+        /// 1 = enabled events and all interrupts, including disabled interrupts, can wakeup the processor.
+        ///
+        /// When an event or interrupt enters pending state, the event signal wakes up the processor from WFE.
+        /// If the processor is not waiting for an event, the event is registered and affects the next WFE.
+        ///
+        /// The processor also wakes up on execution of an SEV instruction or an external event.
+        SEVONPEND: u1,
+        reserved2: u27,
+    }),
     /// Configuration and Control Register.
     CCR: mmio.Mmio(packed struct(u32) {
         reserved0: u1,
@@ -99,6 +218,31 @@ pub const NestedVectorInterruptController = extern struct {
     reserved6: [584]u32,
     /// Software Trigger Interrupt Register.
     STIR: u32,
+
+    pub fn is_enabled(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        const reg = nvic.ISER[num / 32];
+        return reg & (1 << (num % 32)) != 0;
+    }
+
+    pub fn enable(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        nvic.ISER[num / 32] |= 1 << (num % 32);
+    }
+
+    pub fn disable(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        nvic.ICER[num / 32] |= 1 << (num % 32);
+    }
+
+    pub fn is_pending(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        return nvic.ISPR[num / 32] & (1 << (num % 32)) != 0;
+    }
+
+    pub fn set_pending(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        nvic.ISPR[num / 32] |= 1 << (num % 32);
+    }
+
+    pub fn clear_pending(nvic: *volatile NestedVectorInterruptController, num: comptime_int) void {
+        nvic.ICPR[num / 32] |= 1 << (num % 32);
+    }
 };
 
 pub const SecurityAttributionUnit = extern struct {
