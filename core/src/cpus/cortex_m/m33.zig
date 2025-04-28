5d3a7ba871a6307d3e4fb00590f36175597a298a
curuvar <58759586+curuvar@users.noreply.github.com>
2025-04-28 09:43:29 -0400

    align m33 and m55 with ARMv8-M Arch Ref Man.  make scb_base_offset architecture dependent.

--- core/src/cpus/cortex_m/m33.zig
+++ core/src/cpus/cortex_m/m33.zig
@@ -10,7 +10,11 @@ pub const CPUOptions = struct {
     has_ram_vectors_section: bool = false,
 };
 
+pub const scb_base_offset = 0x0cfc;
+
 pub const SystemControlBlock = extern struct {
+    /// REVIDR Register.
+    REVIDR: u32,
     /// CPUID Base Register.
     CPUID: u32,
     /// Interrupt Control and State Register.
@@ -19,7 +23,11 @@ pub const SystemControlBlock = extern struct {
         /// 0 = Thread mode
         /// Nonzero = The exception number[a] of the currently active exception.
         VECTACTIVE: u9,
-        reserved0: u3,
+        reserved0: u2,
+        /// Return to base. In Handler mode, indicates whether there is more than one active exception.
+        /// 0 = more than one active exception
+        /// 1 = only one active exception.
+        RETTOBASE: u1,
         /// Indicates the exception number of the highest priority pending enabled exception:
         /// 0 = no pending exceptions
         /// Nonzero = the exception number of the highest priority pending enabled exception.
@@ -34,7 +42,10 @@ pub const SystemControlBlock = extern struct {
         /// 0 = will not be serviced
         /// 1 = will be serviced.
         ISRPREEMPT: u1,
-        reserved2: u1,
+        /// SysTick Targets Non-secure. Controls whether in a single SysTick implementation, the SysTick is Secure or Non-secure.
+        /// 0 = Secure
+        /// 1 = Non-secure.
+        STTNS: u1,
         /// SysTick exception clear-pending bit.
         ///
         /// Write:
@@ -71,7 +82,15 @@ pub const SystemControlBlock = extern struct {
         ///
         /// Writing 1 to this bit is the only way to set the PendSV exception state to pending.
         PENDSVSET: u1,
-        reserved3: u2,
+        reserved2: u1,
+        /// NMI clear-pending bit.
+        ///
+        /// Write:
+        /// 0 = no effect
+        /// 1 = removes the pending state from the NMI exception.
+        ///
+        /// This bit is WO. On a register read its value is Unknown.
+        PENDNMICLR: u1,
         /// NMI set-pending bit.
         ///
         /// Write:
@@ -101,7 +120,36 @@ pub const SystemControlBlock = extern struct {
         ///
         /// This bit reads as 0.
         SYSRESETREQ: u1,
-        reserved1: u12,
+        ///System reset request Secure only. The value of this bit defines whether the SYSRESETREQ bit is functional
+        /// in Secure state.
+        /// 0 = SYSRESETREQ functionality is available to both Security states.
+        /// 1 = SYSRESETREQ functionality is available only in Secure state.
+        SYSRESETREQS: u1,
+        /// Data Independent Timing. This bit indicates and allows modification of whether for the selected Security
+        /// state data independent timing operations are guaranteed to be timing invariant with respect to the data values
+        /// being operated on.
+        /// 0 = timing is not guaranteed
+        /// 1 = timing is guaranteed
+        DIT: u1,
+        /// Implicit ESB Enable. This bit indicates and allows modification of whether an implicit Error Synchronization
+        /// Bit is inserted into the instruction stream.
+        /// 0 = No implicit Error Synchronization Bit
+        /// 1 = Insert an implicit Error Synchronization Bit
+        IESB: u1,
+        reserved1: u2,
+        /// Priority grouping. The value of this field defines the exception priority binary point position for the selected
+        /// value of PRIS.
+        PRIGROUP: u3,
+        reserved2: u2,
+        /// BusFault, HardFault, and NMI Non-secure enable. The value of this bit defines whether BusFault and NMI
+        /// exceptions are Non-secure, and whether exceptions target the Non-secure HardFault exception.
+        /// 0 = BusFault, HardFault, and NMI are Secure
+        /// 1 = BusFault and NMI are Non-secure and exceptions can target Non-secure HardFault.
+        BFHFNMINS: u1,
+        /// Prioritize Secure exceptions. The value of this bit defines whether Secure exception priority boosting is enabled.
+        /// 0 = Priority ranges of Secure and Non-secure exceptions are identical
+        /// 1 = Secure exceptions have a higher priority than Non-secure exceptions
+        PRIS: u1,
         /// Data endianness implemented:
         /// 0 = Little-endian
         /// 1 = Big-endian.
@@ -128,7 +176,10 @@ pub const SystemControlBlock = extern struct {
         /// If your device does not support two sleep modes, the effect of changing the value
         /// of this bit is implementation-defined.
         SLEEPDEEP: u1,
-        reserved1: u1,
+        /// Sleep deep secure. This field controls whether the SLEEPDEEP bit is only accessible from the Secure state.
+        /// 0 = The SLEEPDEEP bit accessible from both Security states.
+        /// 1 = The SLEEPDEEP bit behaves as RAZ/WI when accessed from the Non-secure state..
+        SLEEPDEEPS: u1,
         /// Send Event on Pending bit:
         /// 0 = only enabled interrupts or events can wakeup the processor, disabled interrupts are excluded
         /// 1 = enabled events and all interrupts, including disabled interrupts, can wakeup the processor.
@@ -138,7 +189,7 @@ pub const SystemControlBlock = extern struct {
         ///
         /// The processor also wakes up on execution of an SEV instruction or an external event.
         SEVONPEND: u1,
-        reserved2: u27,
+        reserved1: u27,
     }),
     /// Configuration and Control Register.
     CCR: mmio.Mmio(packed struct(u32) {
