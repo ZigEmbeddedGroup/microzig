6803cf0c71e281e52f243847b2ebaea02cbc8c63
Grazfather <grazfather@gmail.com>
2025-04-29 08:20:51 -0400

    cleanup example

--- examples/espressif/esp/src/i2c_bus_scan.zig
+++ examples/espressif/esp/src/i2c_bus_scan.zig
@@ -12,19 +12,12 @@ const i2c0 = i2c.instance.num(0);
 const usb_serial_jtag = esp.usb_serial_jtag;
 
 pub const microzig_options = microzig.Options{
-    // .log_level = .info,
-    // .log_level = .debug,
     .logFn = usb_serial_jtag.logger.logFn,
 };
 
 pub fn main() !void {
-    const sda_pin = gpio.num(5);
-    const scl_pin = gpio.num(6);
-    // TODO: Probably not needed?
-    inline for (&.{ scl_pin, sda_pin }) |pin| {
-        // Give the pin a sane default config
-        pin.apply(.{});
-    }
+    const sda_pin = gpio.num(6);
+    const scl_pin = gpio.num(5);
 
     // TODO: Should this be done in apply?
     // Enable I2C peripheral clock
@@ -34,22 +27,15 @@ pub fn main() !void {
 
     try i2c0.apply(
         .{ .sda = sda_pin, .scl = scl_pin },
-        // TODO: Take hal.clock_config?
         100_000,
     );
 
     // for (0x20..0x21) |addr| {
     for (0..std.math.maxInt(u7)) |addr| {
         const a: i2c.Address = @enumFromInt(addr);
-        std.log.info("Trying to read from address 0x{x:0>2}", .{addr}); // DELETEME
-        var rx_data: [1]u8 = undefined;
 
-        // _ = i2c0.read_blocking(a, &rx_data, null) catch |e| {
-        _ = i2c0.read_blocking(a, &rx_data, time.Duration.from_ms(250)) catch |e| {
-            // Usually timeout
-            std.log.info("Error {any}", .{e}); // DELETEME
-            continue;
-        };
+        var rx_data: [1]u8 = undefined;
+        _ = i2c0.read_blocking(a, &rx_data, time.Duration.from_ms(250)) catch continue;
 
         std.log.info("I2C device found at address {X}.", .{addr});
     }
