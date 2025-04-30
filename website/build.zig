27dd76702bb84daca370c2ade8b8e1be919bf510
Matt Knight <mattnite@proton.me>
2025-04-29 18:53:43 -0700

    Update website (#510)

--- website/build.zig
+++ website/build.zig
@@ -1,12 +1,10 @@
 const std = @import("std");
 const zine = @import("zine");
 
-pub fn build(b: *std.Build) void {
-    zine.website(b, .{
-        .title = "Zig Embedded Group",
-        .host_url = "https://ZigEmbeddedGroup/github.io/microzig",
-        .content_dir_path = "content",
-        .layouts_dir_path = "layouts",
-        .assets_dir_path = "assets",
-    });
+pub fn build(b: *std.Build) !void {
+    b.getInstallStep().dependOn(&zine.website(b, .{}).step);
+
+    const serve = b.step("serve", "Start the Zine dev server");
+    const run_zine = zine.serve(b, .{});
+    serve.dependOn(&run_zine.step);
 }
