cce5971e71082854d78a8e448fa97890c1600b4f
Vladislav Fursov <github@ghost.im>
2025-05-03 04:24:37 +0400

    WCH CH32V: build: add support for all CH32V series chip models and refactoring.

--- port/wch/ch32v/build.zig
+++ port/wch/ch32v/build.zig
@@ -5,14 +5,54 @@ const Self = @This();
 
 const KiB = 1024;
 
+const BaseChip = struct {
+    name: []const u8,
+    cpu_features: std.Target.Cpu.Feature.Set,
+    cpu: microzig.Cpu,
+    hal: microzig.HardwareAbstractionLayer,
+    svd: std.Build.LazyPath,
+
+    fn create(self: BaseChip, dep: *std.Build.Dependency, mem: []const microzig.MemoryRegion) *microzig.Target {
+        const ret = dep.builder.allocator.create(microzig.Target) catch @panic("out of memory");
+        ret.* = .{
+            .dep = dep,
+            .preferred_binary_format = .bin,
+            .zig_target = std.Target.Query{
+                .cpu_arch = .riscv32,
+                .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic },
+                .cpu_features_add = self.cpu_features,
+                .os_tag = .freestanding,
+                .abi = .eabi,
+            },
+            .cpu = self.cpu,
+            .chip = .{
+                .name = self.name,
+                .memory_regions = mem,
+                .register_definition = .{
+                    .svd = self.svd,
+                },
+            },
+            .hal = self.hal,
+        };
+
+        return ret;
+    }
+};
+
+// Generated with:
+// curl 'https://www.wch-ic.com/api/product_tables/47?page=1&limit=100' | jq -r '.data[] | select(.["Part NO."] | startswith("CH32V")) | [ "    ", (.["Part NO."] | ascii_downcase), ": *const microzig.Target, // ", .Flash, " / ", .SRAM, " / ", .Freq ] | join("")' | awk 'match($0, /    ch32v/) { $0 = substr($0, 1, RSTART+11) "x" substr($0, RSTART+13, 1) substr($0, RSTART+16) }; {print}' | sort -n | uniq
 chips: struct {
-    ch32v003x4: *const microzig.Target,
-    ch32v103x6: *const microzig.Target,
-    ch32v103x8: *const microzig.Target,
-    ch32v203x6: *const microzig.Target,
-    ch32v203x8: *const microzig.Target,
-    ch32v305xb: *const microzig.Target,
-    ch32v307xc: *const microzig.Target,
+    ch32v003x4: *const microzig.Target, // 16K / 2K / 48MHz
+    ch32v103x6: *const microzig.Target, // 32K / 10K / 80MHz
+    ch32v103x8: *const microzig.Target, // 64K / 20K / 80MHz
+    ch32v203x6: *const microzig.Target, // 32K / 10K / 144MHz
+    ch32v203x8: *const microzig.Target, // 64K / 20K / 144MHz
+    ch32v203xb: *const microzig.Target, // 128K / 64K / 144MHz
+    ch32v208xb: *const microzig.Target, // 128K / 64K / 144MHz
+    ch32v303xb: *const microzig.Target, // 128K / 32K / 144MHz
+    ch32v303xc: *const microzig.Target, // 256K / 64K / 144MHz
+    ch32v305xb: *const microzig.Target, // 128K / 32K / 144MHz
+    ch32v307xc: *const microzig.Target, // 256K / 64K / 144MHz
 },
 
 boards: struct {
@@ -36,202 +76,96 @@ boards: struct {
 pub fn init(dep: *std.Build.Dependency) Self {
     const b = dep.builder;
 
-    const hal_ch32v003: microzig.HardwareAbstractionLayer = .{
-        .root_source_file = b.path("src/hals/hal_ch32v003.zig"),
+    const mem_16k_2k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 16 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 2 * KiB, .kind = .ram },
     };
-    const hal_ch32v103: microzig.HardwareAbstractionLayer = .{
-        .root_source_file = b.path("src/hals/hal_ch32v103.zig"),
+    const mem_32k_10k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 32 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 10 * KiB, .kind = .ram },
     };
-    const hal_ch32v203: microzig.HardwareAbstractionLayer = .{
-        .root_source_file = b.path("src/hals/hal_ch32v203.zig"),
+    const mem_64k_20k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 64 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 20 * KiB, .kind = .ram },
     };
-    const hal_ch32v307: microzig.HardwareAbstractionLayer = .{
-        .root_source_file = b.path("src/hals/hal_ch32v307.zig"),
+    const mem_128k_32k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 128 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 32 * KiB, .kind = .ram },
     };
-
-    const qingkev2a_target: std.Target.Query = .{
-        // QingKe V2C is RV32EC
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic },
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .@"32bit", .e, .c, .xwchc }),
-        .os_tag = .freestanding,
-        .abi = .eabi,
+    const mem_128k_64k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 128 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 64 * KiB, .kind = .ram },
     };
-
-    const qingkev2_cpu: microzig.Cpu = .{
-        .name = "qingkev2-rv32ec",
-        .root_source_file = b.path("src/cpus/qingkev2-rv32ec.zig"),
+    const mem_256k_64k: []const microzig.MemoryRegion = &.{
+        .{ .offset = 0x08000000, .length = 256 * KiB, .kind = .flash },
+        .{ .offset = 0x20000000, .length = 64 * KiB, .kind = .ram },
     };
 
-    const qingkev3_target: std.Target.Query = .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        // generic_rv32 has feature I.
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .c, .a, .m, .xwchc }),
-        .os_tag = .freestanding,
-        .abi = .eabi,
-    };
-
-    const qingkev3_cpu: microzig.Cpu = .{
-        .name = "qingkev3-rv32imac",
-        .root_source_file = b.path("src/cpus/qingkev3-rv32imac.zig"),
-    };
-
-    const qingkev4b_target: std.Target.Query = .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        // generic_rv32 has feature I.
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .c, .a, .m, .xwchc }),
-        .os_tag = .freestanding,
-        .abi = .eabi,
-    };
-
-    const qingkev4f_target: std.Target.Query = .{
-        .cpu_arch = .riscv32,
-        .cpu_model = .{ .explicit = &std.Target.riscv.cpu.generic_rv32 },
-        // generic_rv32 has feature I.
-        .cpu_features_add = std.Target.riscv.featureSet(&.{ .c, .a, .m, .f, .xwchc }),
-        .os_tag = .freestanding,
-        .abi = .eabi,
-    };
-
-    const qingkev4_cpu: microzig.Cpu = .{
-        .name = "qingkev4-rv32imac",
-        .root_source_file = b.path("src/cpus/qingkev4-rv32imac.zig"),
-    };
-
-    const chip_ch32v003x4: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev2a_target,
-        .cpu = qingkev2_cpu,
-        .chip = .{
-            .name = "CH32V00xxx", // <name/> from SVD
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v003.svd"),
-            },
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 16 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 2 * KiB, .kind = .ram },
-            },
+    const chip_ch32v003_base: BaseChip = .{
+        .name = "CH32V00xxx", // <name/> from SVD
+        .cpu_features = std.Target.riscv.featureSet(&.{ .@"32bit", .e, .c, .xwchc }),
+        .cpu = .{
+            .name = "qingkev2-rv32ec",
+            .root_source_file = b.path("src/cpus/qingkev2-rv32ec.zig"),
         },
-        .hal = hal_ch32v003,
+        .hal = .{
+            .root_source_file = b.path("src/hals/ch32v003.zig"),
+        },
+        .svd = b.path("src/chips/ch32v003.svd"),
     };
 
-    const chip_ch32v103x8: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev3_target,
-        .cpu = qingkev3_cpu,
-        .chip = .{
-            .name = "CH32V103xx", // <name/> from SVD
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 64 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 20 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v103.svd"),
-            },
+    const chip_ch32v103_base: BaseChip = .{
+        .name = "CH32V103xx", // <name/> from SVD
+        .cpu_features = std.Target.riscv.featureSet(&.{ .@"32bit", .i, .m, .a, .c, .xwchc }),
+        .cpu = .{
+            .name = "qingkev3-rv32imac",
+            .root_source_file = b.path("src/cpus/qingkev3-rv32imac.zig"),
         },
-        .hal = hal_ch32v103,
+        .hal = .{
+            .root_source_file = b.path("src/hals/ch32v103.zig"),
+        },
+        .svd = b.path("src/chips/ch32v103.svd"),
     };
 
-    const chip_ch32v103x6: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev3_target,
-        .cpu = qingkev3_cpu,
-        .chip = .{
-            .name = "CH32V103xx", // <name/> from SVD
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 32 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 10 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v103.svd"),
-            },
+    const chip_ch32v20x_base: BaseChip = .{
+        .name = "CH32V20xxx", // <name/> from SVD
+        .cpu_features = std.Target.riscv.featureSet(&.{ .@"32bit", .i, .m, .a, .c, .xwchc }),
+        .cpu = .{
+            .name = "qingkev4-rv32imac",
+            .root_source_file = b.path("src/cpus/qingkev4-rv32imac.zig"),
         },
-        .hal = hal_ch32v103,
+        .hal = .{
+            .root_source_file = b.path("src/hals/ch32v20x.zig"),
+        },
+        .svd = b.path("src/chips/ch32v20x.svd"),
     };
 
-    const chip_ch32v203x8: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev4b_target,
-        .cpu = qingkev4_cpu,
-        .chip = .{
-            .name = "CH32V20xxx", // <name/> from SVD
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 64 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 20 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v20x.svd"),
-            },
+    const chip_ch32v30x_base: BaseChip = .{
+        .name = "CH32V30xxx", // <name/> from SVD
+        .cpu_features = std.Target.riscv.featureSet(&.{ .@"32bit", .i, .m, .a, .f, .c, .xwchc }),
+        .cpu = .{
+            .name = "qingkev4-rv32imafc",
+            .root_source_file = b.path("src/cpus/qingkev4-rv32imac.zig"),
+            // TODO
+            //.root_source_file = b.path("src/cpus/qingkev4-rv32imafc.zig"),
         },
-        .hal = hal_ch32v203,
+        .hal = .{
+            .root_source_file = b.path("src/hals/ch32v30x.zig"),
+        },
+        .svd = b.path("src/chips/ch32v30x.svd"),
     };
 
-    const chip_ch32v203x6: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev4b_target,
-        .cpu = qingkev4_cpu,
-        .chip = .{
-            .name = "CH32V20xxx", // <name/> from SVD
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 32 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 10 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v20x.svd"),
-            },
-        },
-        .hal = hal_ch32v203,
-    };
-
-    const chip_ch32v305xb: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev4f_target,
-        .cpu = qingkev4_cpu,
-        .chip = .{
-            .name = "CH32V30xxx", // <name/> from SVD
-            .memory_regions = &.{
-                .{ .offset = 0x08000000, .length = 128 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 32 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v30x.svd"),
-            },
-        },
-        .hal = hal_ch32v307,
-    };
-
-    const chip_ch32v307xc: microzig.Target = .{
-        .dep = dep,
-        .preferred_binary_format = .bin,
-        .zig_target = qingkev4f_target,
-        .cpu = qingkev4_cpu,
-        .chip = .{
-            .name = "CH32V30xxx", // <name/> from SVD
-            .memory_regions = &.{
-                // From datasheet:
-                // The products with 256K FLASH+64K SRAM support user select word to be configured as one of several
-                // combinations of (192K FLASH+128K SRAM), (224K FLASH+96K SRAM), (256K FLASH+64K SRAM),
-                // (288K FLASH+32K SRAM). On this basis, the 256K FLASH+64K SRAM product with the sixth inverted
-                // batch number not equal to 0 has also added a configuration combination: (128K FLASH+192K SRAM).
-                // FLASH flash represents the zero-waiting running area R0WAIT, and the product of 256K FLASH+64K
-                // SRAM supports the non-zero waiting area of (480K-R0WAIT) bytes.
-                .{ .offset = 0x08000000, .length = 256 * KiB, .kind = .flash },
-                .{ .offset = 0x20000000, .length = 64 * KiB, .kind = .ram },
-            },
-            .register_definition = .{
-                .svd = b.path("src/chips/ch32v30x.svd"),
-            },
-        },
-        .hal = hal_ch32v307,
-    };
+    const chip_ch32v003x4 = chip_ch32v003_base.create(dep, mem_16k_2k);
+    const chip_ch32v103x6 = chip_ch32v103_base.create(dep, mem_32k_10k);
+    const chip_ch32v103x8 = chip_ch32v103_base.create(dep, mem_64k_20k);
+    const chip_ch32v203x6 = chip_ch32v20x_base.create(dep, mem_32k_10k);
+    const chip_ch32v203x8 = chip_ch32v20x_base.create(dep, mem_64k_20k);
+    const chip_ch32v203xb = chip_ch32v20x_base.create(dep, mem_128k_64k);
+    const chip_ch32v208xb = chip_ch32v20x_base.create(dep, mem_128k_64k);
+    const chip_ch32v303xb = chip_ch32v30x_base.create(dep, mem_128k_32k);
+    const chip_ch32v303xc = chip_ch32v30x_base.create(dep, mem_256k_64k);
+    const chip_ch32v305xb = chip_ch32v30x_base.create(dep, mem_128k_32k);
+    const chip_ch32v307xc = chip_ch32v30x_base.create(dep, mem_256k_64k);
 
     const board_ch32v003f4p6_r0_1v1 = chip_ch32v003x4.derive(.{
         .board = .{
@@ -275,13 +209,17 @@ pub fn init(dep: *std.Build.Dependency) Self {
 
     return .{
         .chips = .{
-            .ch32v003x4 = chip_ch32v003x4.derive(.{}),
-            .ch32v103x6 = chip_ch32v103x6.derive(.{}),
-            .ch32v103x8 = chip_ch32v103x8.derive(.{}),
-            .ch32v203x6 = chip_ch32v203x6.derive(.{}),
-            .ch32v203x8 = chip_ch32v203x8.derive(.{}),
-            .ch32v305xb = chip_ch32v305xb.derive(.{}),
-            .ch32v307xc = chip_ch32v307xc.derive(.{}),
+            .ch32v003x4 = chip_ch32v003x4,
+            .ch32v103x6 = chip_ch32v103x6,
+            .ch32v103x8 = chip_ch32v103x8,
+            .ch32v203x6 = chip_ch32v203x6,
+            .ch32v203x8 = chip_ch32v203x8,
+            .ch32v203xb = chip_ch32v203xb,
+            .ch32v208xb = chip_ch32v208xb,
+            .ch32v303xb = chip_ch32v303xb,
+            .ch32v303xc = chip_ch32v303xc,
+            .ch32v305xb = chip_ch32v305xb,
+            .ch32v307xc = chip_ch32v307xc,
         },
 
         .boards = .{
