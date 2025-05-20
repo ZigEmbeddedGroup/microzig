d37bd358b2e815a3cf8d48a97bdb42bcd7c867a4
Grazfather <grazfather@gmail.com>
2025-05-20 15:05:17 -0400

    why can't I access these types?

--- examples/nordic/nrf5x/src/blinky.zig
+++ examples/nordic/nrf5x/src/blinky.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const microzig = @import("microzig");
-const peripherals = microzig.chip.peripherals;
+const gpio = microzig.hal.gpio;
+// const peripherals = microzig.chip.peripherals;
 
 fn delay(cycles: u32) void {
     var i: u32 = 0;
@@ -11,15 +12,21 @@ fn delay(cycles: u32) void {
 
 pub fn main() !void {
     // Set P0.24 to output (Blue LED)
-    peripherals.P0.DIRSET.modify(.{ .PIN24 = .Output });
+    // peripherals.P0.DIRSET.modify(.{ .PIN24 = .Output });
+    const led = gpio.num(0, 24);
+    led.set_direction(.out);
+    led.set_drive_strength(.HOH1); // DELETEME
+    led.set_pull(.up); // DELETEME
 
     while (true) {
         // Turn LED on (active low)
-        peripherals.P0.OUTCLR.modify(.{ .PIN24 = .High });
+        // peripherals.P0.OUTCLR.modify(.{ .PIN24 = .High });
+        led.put(1);
         delay(3000000);
 
         // Turn LED off
-        peripherals.P0.OUTSET.modify(.{ .PIN24 = .High });
+        // peripherals.P0.OUTSET.modify(.{ .PIN24 = .High });
+        led.put(0);
         delay(3000000);
     }
 }
