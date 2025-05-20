7c667cb8e8ef68904c17f14563571279b02d52df
curuvar <58759586+curuvar@users.noreply.github.com>
2025-05-20 08:26:24 -0400

    Fixes

--- core/src/core.zig
+++ core/src/core.zig
@@ -1,7 +1,7 @@
 pub const experimental = @import("core/experimental.zig");
 pub const heap = @import("core/heap.zig");
 pub const Allocator = @import("allocator.zig");
-/// USB data types and helper functions
+pub const interrupt = @import("core/interrupt.zig");
 pub const usb = @import("core/usb.zig");
 
 test "core tests" {
