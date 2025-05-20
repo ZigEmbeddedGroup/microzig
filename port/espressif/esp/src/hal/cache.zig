80da32305b8082e9243b3115c59d91ca1b43f696
tact1m4n3 <tudor@dicu.org>
2025-05-20 08:44:46 +0300

    tiny change

--- port/espressif/esp/src/hal/cache.zig
+++ port/espressif/esp/src/hal/cache.zig
@@ -4,12 +4,11 @@ const hal = microzig.hal;
 const compatibility = hal.compatibility;
 const rom = hal.rom;
 
-const CACHE_LL_L1_ICACHE_AUTOLOAD = 1 << 2;
-
-// TODO: to be verified
 pub fn init() void {
     switch (compatibility.chip) {
         .esp32_c3 => {
+            const CACHE_LL_L1_ICACHE_AUTOLOAD = 1 << 2;
+
             rom.functions.Cache_Enable_ICache(if (EXTMEM.ICACHE_AUTOLOAD_CTRL.read().ICACHE_AUTOLOAD_ENA != 0)
                 CACHE_LL_L1_ICACHE_AUTOLOAD
             else
