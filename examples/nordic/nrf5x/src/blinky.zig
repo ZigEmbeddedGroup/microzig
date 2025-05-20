871bf4722e9e4fc3b83288e3bf3d9ca6c3ad7134
Grazfather <grazfather@gmail.com>
2025-05-20 11:56:35 -0400

    nrf5x: Add working blinky example

--- examples/nordic/nrf5x/src/blinky.zig
+++ examples/nordic/nrf5x/src/blinky.zig
@@ -1,6 +1,25 @@
 const std = @import("std");
 const microzig = @import("microzig");
+const peripherals = microzig.chip.peripherals;
+
+fn delay(cycles: u32) void {
+    var i: u32 = 0;
+    while (i < cycles) : (i += 1) {
+        asm volatile ("nop");
+    }
+}
 
 pub fn main() !void {
-    // TODO: Implement the blinky
+    // Set P0.24 to output (Blue LED)
+    peripherals.P0.DIRSET.modify(.{ .PIN24 = .Output });
+
+    while (true) {
+        // Turn LED on (active low)
+        peripherals.P0.OUTCLR.modify(.{ .PIN24 = .High });
+        delay(3000000);
+
+        // Turn LED off
+        peripherals.P0.OUTSET.modify(.{ .PIN24 = .High });
+        delay(3000000);
+    }
 }
