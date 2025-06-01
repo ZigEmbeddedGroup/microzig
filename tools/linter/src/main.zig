85806527d730abc2cecb08f2c7162f0bd9b4c160
Matt Knight <mattnite@proton.me>
2025-06-01 00:13:37 -0700

    Clean up linter package

--- tools/linter/src/main.zig
+++ tools/linter/src/main.zig
@@ -78,38 +78,9 @@ fn find_first_token_tag(ast: std.zig.Ast, tag: Token.Tag, start_idx: TokenIndex)
     } else unreachable;
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
-    };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
-}
-
 const std = @import("std");
 const assert = std.debug.assert;
 const Allocator = std.mem.Allocator;
-
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
-const lib = @import("linter_lib");
-
-const Type = struct {
-    pub fn whatIsThis() void {}
-};
 
 fn is_snake_case(str: []const u8) bool {
     for (str) |c| {
