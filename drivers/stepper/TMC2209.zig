0f5a1fa9aced54811a2b15a1263e7bf47eac5424
Craig Swank <craigswank@gmail.com>
2025-06-16 17:44:02 -0600

    limit the move() frequency

--- drivers/stepper/TMC2209.zig
+++ drivers/stepper/TMC2209.zig
@@ -12,18 +12,26 @@ pub const TMC2209_Config = struct {
     uart: mdf.base.Datagram_Device,
     clock: mdf.base.Clock_Device,
     address: u8,
+    pulse_frequency: f32 = 0.715, // frequency of the built in pulse generator
+    steps: u32 = 200, // number of steps per revolution of the motor
 };
 
 pub const TMC2209 = struct {
     const Self = @This();
 
-    const control_register = packed struct {};
+    const write_request = packed struct {
+        sync: u8 = 0x5,
+        addr: u8,
+        register: u8,
+        data: u32,
+        crc: u8 = 0,
+    };
 
-    ramp: [20]f32 = undefined,
+    ramp: [40]f32 = undefined,
     microsteps: u32 = 1,
-    steps: u32 = 200, // number of steps per revolution of the motor
     hz: f32 = 0,
-    pulse_frequency: f32 = 0.715,
+    pulse_frequency: f32,
+    steps: u32,
     uart: mdf.base.Datagram_Device,
     clock_device: mdf.base.Clock_Device,
     address: u8,
@@ -33,24 +41,18 @@ pub const TMC2209 = struct {
             .uart = cfg.uart,
             .clock_device = cfg.clock,
             .address = cfg.address,
+            .steps = cfg.steps,
+            .pulse_frequency = cfg.pulse_frequency,
         };
-    }
-
-    fn write(self: *Self, register: u8, data: u32) !void {
-        const req = packed struct { sync: u8 = 0x5, addr: u8, register: u8, data: u32, crc: u8 = 0 }{
-            .addr = self.address,
-            .register = register | 0x80,
-            .data = std.mem.nativeToBig(u32, data),
-        };
-
-        var buf = std.mem.toBytes(req);
-        buf[7] = self.crc(buf[0..7]);
-        try self.uart.write(&buf);
     }
 
     // velocity = hz / 0.715 * steps * microsteps
     // 0.715 is the built in pulse generator
     pub fn move(self: *Self, hz: f32) !void {
+        if (hz > 20 or hz < -20) {
+            return error.InvalidMoveFrequency;
+        }
+
         var steps: f32 = 0;
         for (self.calculate_ramp(hz)[0..]) |val| {
             steps = @floatFromInt(self.steps * self.microsteps);
@@ -76,6 +78,18 @@ pub const TMC2209 = struct {
         try self.write(cf.register, @bitCast(cf.val));
     }
 
+    fn write(self: *Self, register: u8, data: u32) !void {
+        const req = write_request{
+            .addr = self.address,
+            .register = register | 0x80,
+            .data = std.mem.nativeToBig(u32, data),
+        };
+
+        var buf = std.mem.toBytes(req);
+        buf[7] = self.crc(buf[0..7]);
+        try self.uart.write(&buf);
+    }
+
     fn calculate_ramp(self: *Self, hz: f32) []f32 {
         var val: f32 = 0;
         var i: usize = 0;
@@ -86,14 +100,13 @@ pub const TMC2209 = struct {
                 val += 1;
                 i += 1;
             }
-        } else if (hz < 0 and hz < self.hz) {
+        } else {
             val = self.hz - 1;
             while (val > hz) {
                 self.ramp[i] = val;
                 val -= 1;
-                i += 1;
             }
-        } else {}
+        }
 
         self.ramp[i] = hz;
         return self.ramp[0 .. i + 1];
@@ -364,16 +377,20 @@ test "move" {
 
     var stepper = try TMC2209.init(.{ .address = 0, .uart = td.datagram_device(), .clock = tc.clock_device() });
 
+    // move frequency is -20 to 20
+    try std.testing.expectError(error.InvalidMoveFrequency, stepper.move(20.1));
+    try std.testing.expectError(error.InvalidMoveFrequency, stepper.move(-20.1));
+
     try stepper.set_microsteps(16);
     try stepper.move(5);
 
     const sent = [_][]const u8{
         &.{ 0x05, 0x00, 0xec, 0x14, 0x00, 0x00, 0x05, 0x43 }, //set microsteps
-        &.{ 0x5, 0x0, 0xa2, 0x0, 0x0, 0x11, 0x7b, 0x04 },
-        &.{ 0x5, 0x0, 0xa2, 0x0, 0x0, 0x22, 0xf7, 0x82 },
-        &.{ 0x5, 0x0, 0xa2, 0x0, 0x0, 0x34, 0x72, 0xb1 },
-        &.{ 0x5, 0x0, 0xa2, 0x0, 0x0, 0x45, 0xee, 0x7e },
-        &.{ 0x5, 0x0, 0xa2, 0x0, 0x0, 0x57, 0x69, 0x24 },
+        &.{ 0x05, 0x00, 0xa2, 0x00, 0x00, 0x11, 0x7b, 0x04 },
+        &.{ 0x05, 0x00, 0xa2, 0x00, 0x00, 0x22, 0xf7, 0x82 },
+        &.{ 0x05, 0x00, 0xa2, 0x00, 0x00, 0x34, 0x72, 0xb1 },
+        &.{ 0x05, 0x00, 0xa2, 0x00, 0x00, 0x45, 0xee, 0x7e },
+        &.{ 0x05, 0x00, 0xa2, 0x00, 0x00, 0x57, 0x69, 0x24 },
     };
     try td.expect_sent(&sent);
 }
