ad2d38e5f7a63305ac5df8bbea9f868d77d48516
Craig Swank <craigswank@Craigs-MacBook-Pro-2.local>
2025-06-26 08:55:18 -0400

    wip

---
+++ examples/raspberrypi/rp2xxx/src/tmc2209.zig
@@ -0,0 +1,67 @@
+const std = @import("std");
+const microzig = @import("microzig");
+const stepper = microzig.drivers.stepper;
+const base = microzig.drivers.base;
+const rpxxxx = microzig.hal;
+const gpio = rpxxxx.gpio;
+const UART_Device = rpxxxx.drivers.UART_Device;
+const ClockDevice = rpxxxx.drivers.ClockDevice;
+const TMC2209 = stepper.TMC2209;
+
+const time = rpxxxx.time;
+
+const uart0 = rpxxxx.uart.instance.num(0);
+const uart1 = rpxxxx.uart.instance.num(1);
+const baud_rate = 115200;
+const uart_tx_pin = gpio.num(0);
+
+const pin_config = rpxxxx.pins.GlobalConfiguration{
+    .GPIO0 = .{ .name = "gpio0", .function = .UART0_TX },
+    .GPIO4 = .{ .name = "gpio4", .function = .UART1_TX },
+    .GPIO5 = .{ .name = "gpio5", .function = .UART1_RX },
+};
+
+pub const microzig_options = microzig.Options{
+    .log_level = .debug,
+    .logFn = rpxxxx.uart.log,
+};
+
+pub fn main() !void {
+    try init();
+
+    var cd = ClockDevice{};
+    var ud = UART_Device.init(uart1, null);
+
+    var driver = try TMC2209.init(.{
+        .uart = ud.stream_device(),
+        .clock = cd.clock_device(),
+        .address = 0,
+    });
+
+    try driver.set_microsteps(128);
+
+    var i: f32 = 1;
+    while (true) {
+        try driver.move(5 * i);
+        i *= -1;
+        time.sleep_ms(5000);
+    }
+}
+
+fn init() !void {
+    uart_tx_pin.set_function(.uart);
+    uart0.apply(.{
+        .baud_rate = baud_rate,
+        .clock_config = rpxxxx.clock_config,
+    });
+
+    uart1.apply(.{
+        .baud_rate = baud_rate,
+        .clock_config = rpxxxx.clock_config,
+    });
+
+    rpxxxx.uart.init_logger(uart0);
+    pin_config.apply();
+
+    std.log.info("Hello from mlx90640", .{});
+}
