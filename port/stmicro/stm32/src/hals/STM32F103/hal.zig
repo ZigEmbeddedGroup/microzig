f510f816fde1a3bd1161906d35130ff4fc82d911
Guilherme S. Schultz <65524156+RecursiveError@users.noreply.github.com>
2025-06-29 13:33:52 -0300

    Add clock management and configuration for STM32F103 (#609)

--- port/stmicro/stm32/src/hals/STM32F103/hal.zig
+++ port/stmicro/stm32/src/hals/STM32F103/hal.zig
@@ -1,3 +1,4 @@
+pub const rcc = @import("rcc.zig");
 pub const pins = @import("pins.zig");
 pub const gpio = @import("gpio.zig");
 pub const uart = @import("uart.zig");
@@ -7,4 +8,8 @@ pub const drivers = @import("drivers.zig");
 pub const timer = @import("timer.zig");
 pub const usb = @import("usb.zig");
 pub const adc = @import("adc.zig");
-pub fn init() void {}
+
+pub var RESET: rcc.ResetReason = .POR_or_PDR;
+pub fn init() void {
+    RESET = rcc.get_reset_reason();
+}
