5917223c66f799088cffb53488d8c990d65563f1
tact1m4n3 <tudor@dicu.org>
2025-07-04 11:47:29 +0300

    panic if main returns error

--- core/src/start.zig
+++ core/src/start.zig
@@ -65,24 +65,37 @@ export fn microzig_main() noreturn {
 
     if (@typeInfo(return_type) == .error_union) {
         main() catch |err| {
-            const msg_base = "main() returned error.{s}";
-            const err_name = if (!builtin.strip_debug_info) @errorName(err) else "";
+            // Although here we could use @errorReturnTrace similar to
+            // `std.start` and just dump the trace (without panic), the user
+            // might not use logging and have the panic handler just blink an
+            // led.
 
-            std.log.err(msg_base, .{err_name});
-            if (@errorReturnTrace()) |trace| {
-                _ = microzig.utilities.dump_stack_trace(trace);
+            const msg_base = "main() returned error.";
+
+            if (!builtin.strip_debug_info) {
+                const max_error_size = comptime blk: {
+                    var max_error_size: usize = 0;
+                    const err_type = @typeInfo(return_type).error_union.error_set;
+                    if (@typeInfo(err_type).error_set) |err_set| {
+                        for (err_set) |current_err| {
+                            max_error_size = @max(max_error_size, current_err.name.len);
+                        }
+                    }
+                    break :blk max_error_size;
+                };
+
+                var buf: [msg_base.len + max_error_size]u8 = undefined;
+                const msg = std.fmt.bufPrint(&buf, "{s}{s}", .{ msg_base, @errorName(err) }) catch @panic(msg_base);
+                @panic(msg);
+            } else {
+                @panic(msg_base);
             }
-
-            // Maybe someone doesn't use logging so let's notify that we
-            // have an error (in case they specify a custom panic handler
-            // like blink an led).
-            @panic("main() returned error.");
         };
     } else {
         main();
     }
 
-    // main returned, just hang around here a bit
+    // Main returned, just hang around here a bit.
     microzig.hang();
 }
 
