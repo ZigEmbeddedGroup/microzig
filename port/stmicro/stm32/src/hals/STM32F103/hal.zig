799592e1cd3da9748a004f9167f9b9559a4b9148
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-08-07 20:49:52 -0300

    Add EXTI example and HAL support for STM32F1xx

--- port/stmicro/stm32/src/hals/STM32F103/hal.zig
+++ port/stmicro/stm32/src/hals/STM32F103/hal.zig
@@ -1,6 +1,7 @@
 pub const rcc = @import("rcc.zig");
 pub const pins = @import("pins.zig");
 pub const gpio = @import("gpio.zig");
+pub const exti = @import("exti.zig");
 pub const uart = @import("uart.zig");
 pub const i2c = @import("i2c.zig");
 pub const spi = @import("spi.zig");
