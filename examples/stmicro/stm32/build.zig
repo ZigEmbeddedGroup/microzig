34ee7e7fa52e7d92f9b176f71f8307c487e0717a
RecursiveError <guilhermesschultz.contato@gmail.com>
2025-08-07 20:49:52 -0300

    Add EXTI example and HAL support for STM32F1xx

--- examples/stmicro/stm32/build.zig
+++ examples/stmicro/stm32/build.zig
@@ -41,6 +41,7 @@ pub fn build(b: *std.Build) void {
         .{ .target = stm32.chips.STM32F103CB, .name = "STM32F1xx_timer", .file = "src/stm32f1xx/timer.zig" },
         .{ .target = stm32.chips.STM32F103CB, .name = "STM32F1xx_timer_capture", .file = "src/stm32f1xx/timer_capture.zig" },
         .{ .target = stm32.chips.STM32F103CB, .name = "STM32F1xx_rtc", .file = "src/stm32f1xx/rtc.zig" },
+        .{ .target = stm32.chips.STM32F103CB, .name = "STM32F1xx_EXTI", .file = "src/stm32f1xx/EXTI.zig" },
     };
 
     for (available_examples) |example| {
