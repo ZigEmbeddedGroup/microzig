e780dea5dd5111c0dd60b98dfb3e78e4548e866f
Güney Saramalı <guneysaramali@live.co.uk>
2025-08-14 22:31:35 +0300

    Cleaning the code a bit

--- examples/raspberrypi/rp2xxx/src/ssd1306_oled.zig
+++ examples/raspberrypi/rp2xxx/src/ssd1306_oled.zig
@@ -29,6 +29,7 @@ const pin_config = rp2xxx.pins.GlobalConfiguration{
 const i2c0 = i2c.instance.num(0);
 const lcd_address = rp2xxx.i2c.Address.new(0x3C);
 const empty_row: []const u8 = "                ";
+const four_rows = empty_row ++ empty_row ++ empty_row ++ empty_row;
 
 pub fn main() void {
     var backinf_buffer: [200 * 1024]u8 = undefined;
@@ -39,7 +40,7 @@ pub fn main() void {
     const I2C_DEVICE = rp2xxx.drivers.I2C_Device.init(i2c0, lcd_address, null);
     const lcd = microzig.drivers.display.ssd1306.init(.i2c, I2C_DEVICE, null) catch unreachable;
     lcd.clear_screen(false) catch unreachable;
-    const print_val = empty_row ++ empty_row ++ empty_row ++ empty_row ++ "    WELCOME!";
+    const print_val = four_rows ++ "    WELCOME!";
     var buff: [print_val.len * 8]u8 = undefined;
     lcd.write_gdram(font8x8.Fonts.draw(&buff, print_val)) catch unreachable;
     time.sleep_ms(2000);
@@ -64,12 +65,10 @@ fn center(buf: []u8, value: anytype) []u8 {
     var temp_buf: [2]u8 = undefined;
     const str = std.fmt.bufPrint(&temp_buf, "{}", .{value}) catch unreachable;
 
-    const space = " ";
     const to_be_added = @divTrunc(16 - str.len, 2);
-    const four_rows = empty_row ++ empty_row ++ empty_row ++ empty_row;
     @memcpy(buf[0..64], four_rows);
     for (0..to_be_added) |i| {
-        @memcpy(buf[(64 + i)..(64 + i + 1)], space);
+        @memcpy(buf[(64 + i)..(64 + i + 1)], " ");
     }
     @memcpy(buf[(64 + to_be_added)..(64 + to_be_added + str.len)], str);
     for ((64 + str.len + to_be_added)..buf.len) |i| {
