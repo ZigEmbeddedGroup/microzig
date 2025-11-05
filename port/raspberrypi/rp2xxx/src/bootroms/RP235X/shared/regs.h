27e2e1f40661f1240a178f52cef9dd85e0e0ee2a
IvanCh3ung <56063598+IvanCh3ung@users.noreply.github.com>
2025-11-04 16:55:09 -0800

    RP235X BootROM skeleton

---
+++ port/raspberrypi/rp2xxx/src/bootroms/RP235X/shared/regs.h
@@ -0,0 +1,11 @@
+#ifndef RP2040_STAGE2_REGS_H
+#define RP2040_STAGE2_REGS_H
+
+#define _u(x) x ## u
+
+#include "ssi.h"
+#include "pads_qspi.h"
+#include "addressmap.h"
+#include "m0plus.h"
+
+#endif // RP2040_STAGE2_REGS_H
