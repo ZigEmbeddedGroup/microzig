e8d0bf86b8372ea99212f3afedb732d4db534868
Güney Saramalı <guneysaramali@live.co.uk>
2025-11-22 18:45:23 +0200

    PR fixes

--- examples/raspberrypi/rp2xxx/src/ssd1306_oled.zig
+++ examples/raspberrypi/rp2xxx/src/ssd1306_oled.zig
@@ -13,11 +13,11 @@ const four_rows = empty_row ** 4;
 pub fn main() void {
     // Safe buffer size for rp2xxx to allocate, value can change for other chips
     const buffer_size = 200 * 1024; // 200 KB
-    var backinf_buffer: [RP_buffer_size]u8 = undefined;
+    var backinf_buffer: [buffer_size]u8 = undefined;
     var fba = std.heap.FixedBufferAllocator.init(&backinf_buffer);
 
-    const sda_pin = gpio.num(4);
-    const scl_pin = gpio.num(5);
+    const sda_pin = gpio.num(8);
+    const scl_pin = gpio.num(9);
     inline for (&.{ scl_pin, sda_pin }) |pin| {
         pin.set_slew_rate(.slow);
         pin.set_schmitt_trigger_enabled(true);
@@ -26,8 +26,8 @@ pub fn main() void {
 
     rp2xxx.i2c.I2C.apply(i2c0, .{ .baud_rate = 400_000, .clock_config = rp2xxx.clock_config });
 
-    const I2C_DEVICE = rp2xxx.drivers.I2C_Datagram_Device.init(i2c0, @enumFromInt(0x3C), null);
-    const lcd = microzig.drivers.display.ssd1306.init(.i2c, I2C_DEVICE, null) catch unreachable;
+    const i2c_dd = rp2xxx.drivers.I2C_Datagram_Device.init(i2c0, @enumFromInt(0x3C), null);
+    const lcd = microzig.drivers.display.ssd1306.init(.i2c, i2c_dd, null) catch unreachable;
 
     const print_val = four_rows ++ "    WELCOME";
     var buff: [print_val.len * 8]u8 = undefined;
@@ -37,45 +37,42 @@ pub fn main() void {
 
     time.sleep_ms(2000);
 
-    var counter: u8 = 0;
-    while (true) : (time.sleep_ms(1000)) {
-        var allocator = fba.allocator();
+    for (0..60) |i| {
+        var aa = std.heap.ArenaAllocator.init(fba.allocator());
+        defer aa.deinit();
         var temp_buf: [7]u8 = undefined;
-        const str = std.fmt.bufPrint(&temp_buf, "Try #{}", .{counter}) catch unreachable;
+        const str = std.fmt.bufPrint(&temp_buf, "Try #{}", .{i}) catch unreachable;
         var counter_buf: [80]u8 = undefined;
-        const text_centered = centerToScreen(&counter_buf, str);
+        const text_centered = center_to_screen(&counter_buf, str);
 
-        const text = font8x8.Fonts.drawAlloc(allocator, text_centered) catch continue;
-        defer allocator.free(text);
+        const text = font8x8.Fonts.drawAlloc(aa.allocator(), text_centered) catch continue;
 
         lcd.clear_screen(false) catch continue;
         lcd.write_gdram(text) catch continue;
 
-        counter += 1;
-        if (counter > 59)
-            break;
+        time.sleep_ms(1000);
     }
 }
 
-fn centerToScreen(buf: []u8, str: []u8) []u8 {
-    const ldc_row_len = empty_row.len; // 16
-    const four_rows_len = four_rows.len; // 64
-    const to_be_added = @divTrunc(ldc_row_len - str.len, 2);
+fn center_to_screen(buf: []u8, str: []u8) []u8 {
+    const ldc_row_len = empty_row.len;
+    const four_rows_len = four_rows.len;
+    const padding = @divTrunc(ldc_row_len - str.len, 2);
 
+    // Copy the initial four rows (Could just memset)
     @memcpy(buf[0..four_rows_len], four_rows);
 
-    for (0..to_be_added) |i| {
-        @memcpy(buf[(four_rows_len + i)..(four_rows_len + i + 1)], " ");
-    }
+    // Add left padding
+    const left_pad_start = four_rows_len;
+    const left_pad_end = left_pad_start + padding;
+    @memset(buf[left_pad_start..left_pad_end], ' ');
 
-    @memcpy(
-        buf[(four_rows_len + to_be_added)..(four_rows_len + to_be_added + str.len)],
-        str,
-    );
+    // Copy the centered string
+    const str_start = left_pad_end;
+    const str_end = str_start + str.len;
+    @memcpy(buf[str_start..str_end], str);
 
-    for ((four_rows_len + to_be_added + str.len)..buf.len) |i| {
-        @memcpy(buf[i..(i + 1)], " ");
-    }
-
+    // Add right padding
+    @memset(buf[str_end..buf.len], ' ');
     return buf;
 }
