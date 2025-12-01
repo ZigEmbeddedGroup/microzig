a0cc566e996e0d43fb74d84865785da5065ae047
Grazfather <grazfather@gmail.com>
2025-11-30 19:58:01 -0500

    ch32v: Add USART hal

--- examples/wch/ch32v/build.zig
+++ examples/wch/ch32v/build.zig
@@ -32,6 +32,7 @@ pub fn build(b: *std.Build) void {
         .{ .target = mb.ports.ch32v.chips.ch32v203x6, .name = "blinky_systick_ch32v203", .file = "src/blinky_systick.zig" },
         .{ .target = mb.ports.ch32v.boards.ch32v203.suzuduino_uno_v1b, .name = "suzuduino_blinky", .file = "src/board_blinky.zig" },
         .{ .target = mb.ports.ch32v.boards.ch32v203.lana_tny, .name = "lana_tny_ws2812", .file = "src/ws2812.zig" },
+        .{ .target = mb.ports.ch32v.boards.ch32v203.lana_tny, .name = "lana_tny_uart_log", .file = "src/uart_log.zig" },
 
         // CH32V30x
         .{ .target = mb.ports.ch32v.chips.ch32v303xb, .name = "empty_ch32v303", .file = "src/empty.zig" },
--- port/wch/ch32v/src/hals/ch32v103.zig
+++ port/wch/ch32v/src/hals/ch32v103.zig
@@ -3,6 +3,7 @@ pub const pins = @import("pins.zig");
 pub const gpio = @import("gpio.zig");
 pub const clocks = @import("clocks.zig");
 pub const time = @import("time.zig");
+pub const usart = @import("usart.zig");
 
 /// Initialize HAL subsystems used by default
 /// CH32V103: set clock to 48 MHz via HSI PLL; SysTick driver differs on 103, so time is not enabled here.
