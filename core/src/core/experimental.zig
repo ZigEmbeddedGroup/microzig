70531cd0ca3efc9bc1f8f6a99f2da42f0af1d173
Matthew Knight <mattnite@proton.me>
2025-12-28 20:28:06 -0800

    Remove unused experimental code
    They're already implemented elsewhere

--- core/src/core/experimental.zig
+++ core/src/core/experimental.zig
@@ -3,13 +3,9 @@
 //!
 //! They are bound to have breaking changes in the future, or even disappear,
 //! so use at your own risk.
-pub const clock = @import("experimental/clock.zig");
 pub const debug = @import("experimental/debug.zig");
 pub const gpio = @import("experimental/gpio.zig");
-pub const i2c = @import("experimental/i2c.zig");
 pub const Pin = @import("experimental/pin.zig").Pin;
-pub const spi = @import("experimental/spi.zig");
-pub const uart = @import("experimental/uart.zig");
 
 ///semihosting requires a valid host session, either via a simulator or via a debug probe, otherwise the code will always result in a halt.
 ///This implementation only supports Arm-M.
