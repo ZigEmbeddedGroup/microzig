f3600321fce1f2f102626692a52a474b9f3f755a
Matt Knight <mattnite@proton.me>
2026-01-01 23:27:08 -0800

    Get AVR examples compiling
    Note that this does not actually fix AVR compilation issues, but it's a
    start. I think by bundling compiler_rt before, we were causing all
    compiler_rt functions to be evaluated and thus the compiler errors. Now
    with the basic blinky and size optimizations, we don't seem to trip
    those evaluations.
    
    I have not tested this blinky program on hardware and I plan on doing
    that soon.
    
    For debug builds we run into a ton of size issues with fmt. I think this
    is from some of the "batteries included" things from MicroZig.
    
    Tagging #620

--- port/microchip/atmega/src/hals/ATmega328P.zig
+++ port/microchip/atmega/src/hals/ATmega328P.zig
@@ -1,194 +1,74 @@
 const std = @import("std");
 const microzig = @import("microzig");
 const peripherals = microzig.chip.peripherals;
-const USART0 = peripherals.USART0;
-
-pub const cpu = microzig.cpu;
-const Port = enum(u8) {
-    B = 1,
-    C = 2,
-    D = 3,
-};
-
-pub const clock = struct {
-    pub const Domain = enum {
-        cpu,
-    };
-};
-
-pub fn parse_pin(comptime spec: []const u8) type {
-    const invalid_format_msg = "The given pin '" ++ spec ++ "' has an invalid format. Pins must follow the format \"P{Port}{Pin}\" scheme.";
-
-    if (spec.len != 3)
-        @compileError(invalid_format_msg);
-    if (spec[0] != 'P')
-        @compileError(invalid_format_msg);
-
-    return struct {
-        pub const port: Port = std.meta.stringToEnum(Port, spec[1..2]) orelse @compileError(invalid_format_msg);
-        pub const pin: u3 = std.fmt.parseInt(u3, spec[2..3], 10) catch @compileError(invalid_format_msg);
-    };
-}
+const cpu = microzig.cpu;
 
 pub const gpio = struct {
-    pub const State = enum(u1) {
-        low = 0,
-        high = 1,
+    pub const Port = enum(u2) {
+        b = 1,
+        c = 2,
+        d = 3,
+
+        pub const Regs = extern struct {
+            /// Port Input Pins
+            PIN: u8,
+            /// Port Data Direction Register
+            DDR: u8,
+            /// Port Data Register
+            PORT: u8,
+        };
+
+        // These addresses are IO addresses. These are used instead of the data
+        // addresses so that we can use SBI and CBI instructiosn
+        pub inline fn get_regs(port: Port) *volatile Regs {
+            return switch (port) {
+                .b => @ptrFromInt(0x3),
+                .c => @ptrFromInt(0x6),
+                .d => @ptrFromInt(0x9),
+            };
+        }
     };
 
-    fn regs(comptime desc: type) type {
-        return struct {
-            // io address
-            const pin_addr: u5 = 3 * @intFromEnum(desc.port) + 0x00;
-            const dir_addr: u5 = 3 * @intFromEnum(desc.port) + 0x01;
-            const port_addr: u5 = 3 * @intFromEnum(desc.port) + 0x02;
-
-            // ram mapping
-            const pin = @as(*volatile u8, @ptrFromInt(0x20 + @as(usize, pin_addr)));
-            const dir = @as(*volatile u8, @ptrFromInt(0x20 + @as(usize, dir_addr)));
-            const port = @as(*volatile u8, @ptrFromInt(0x20 + @as(usize, port_addr)));
+    pub fn pin(port: Port, num: u3) Pin {
+        return Pin{
+            .port = port,
+            .num = num,
         };
     }
 
-    pub fn set_output(comptime pin: type) void {
-        cpu.sbi(regs(pin).dir_addr, pin.pin);
-    }
+    pub const Direction = enum {
+        input,
+        output,
+    };
 
-    pub fn set_input(comptime pin: type) void {
-        cpu.cbi(regs(pin).dir_addr, pin.pin);
-    }
+    pub const Pin = packed struct(u5) {
+        port: Port,
+        num: u3,
 
-    pub fn read(comptime pin: type) State {
-        return if ((regs(pin).pin.* & (1 << pin.pin)) != 0)
-            .high
-        else
-            .low;
-    }
-
-    pub fn write(comptime pin: type, state: State) void {
-        switch (state) {
-            .high => cpu.sbi(regs(pin).port_addr, pin.pin),
-            .low => cpu.cbi(regs(pin).port_addr, pin.pin),
+        pub inline fn set_direction(p: Pin, dir: Direction) void {
+            const dir_addr: *volatile u8 = &p.port.get_regs().DDR;
+            switch (dir) {
+                .input => cpu.cbi(@intFromPtr(dir_addr), p.num),
+                .output => cpu.sbi(@intFromPtr(dir_addr), p.num),
+            }
         }
-    }
 
-    pub fn toggle(comptime pin: type) void {
-        cpu.sbi(regs(pin).pin_addr, pin.pin);
-    }
-};
+        pub inline fn read(p: Pin) u1 {
+            const pin_addr: *volatile u8 = &p.port.get_regs().PIN;
+            return @truncate(pin_addr.* >> p.num & 0x01);
+        }
 
-pub const uart = struct {
-    pub const DataBits = enum {
-        five,
-        six,
-        seven,
-        eight,
-        nine,
-    };
+        pub inline fn put(p: Pin, value: u1) void {
+            const port_addr: *volatile u8 = &p.port.get_regs().PORT;
+            switch (value) {
+                1 => cpu.sbi(@intFromPtr(port_addr), p.num),
+                0 => cpu.cbi(@intFromPtr(port_addr), p.num),
+            }
+        }
 
-    pub const StopBits = enum {
-        one,
-        two,
-    };
-
-    pub const Parity = enum {
-        odd,
-        even,
+        pub inline fn toggle(p: Pin) void {
+            const pin_addr: *volatile u8 = &p.port.get_regs().PIN;
+            cpu.sbi(@intFromPtr(pin_addr), p.num);
+        }
     };
 };
-
-pub fn Uart(comptime index: usize, comptime pins: microzig.uart.Pins) type {
-    if (index != 0) @compileError("Atmega328p only has a single uart!");
-    if (pins.tx != null or pins.rx != null)
-        @compileError("Atmega328p has fixed pins for uart!");
-
-    return struct {
-        const Self = @This();
-
-        fn compute_divider(baud_rate: u32) !u12 {
-            const pclk = microzig.clock.get().cpu;
-            const divider = ((pclk + (8 * baud_rate)) / (16 * baud_rate)) - 1;
-
-            return std.math.cast(u12, divider) orelse return error.UnsupportedBaudRate;
-        }
-
-        fn compute_baudrate(divider: u12) u32 {
-            return microzig.clock.get().cpu / (16 * @as(u32, divider) + 1);
-        }
-
-        pub fn init(config: microzig.uart.Config) !Self {
-            const ucsz: u3 = switch (config.data_bits) {
-                .five => 0b000,
-                .six => 0b001,
-                .seven => 0b010,
-                .eight => 0b011,
-                .nine => return error.UnsupportedWordSize, // 0b111
-            };
-
-            const upm: u2 = if (config.parity) |parity| switch (parity) {
-                .even => @as(u2, 0b10), // even
-                .odd => @as(u2, 0b11), // odd
-            } else 0b00; // parity disabled
-
-            const usbs: u1 = switch (config.stop_bits) {
-                .one => 0b0,
-                .two => 0b1,
-            };
-
-            const umsel: u2 = 0b00; // Asynchronous USART
-
-            // baud is computed like this:
-            //             f(osc)
-            // BAUD = ----------------
-            //        16 * (UBRRn + 1)
-
-            const ubrr_val = try compute_divider(config.baud_rate);
-
-            USART0.UCSR0A.modify(.{
-                .MPCM0 = 0,
-                .U2X0 = 0,
-            });
-            USART0.UCSR0B.write(.{
-                .TXB80 = 0, // we don't care about these btw
-                .RXB80 = 0, // we don't care about these btw
-                .UCSZ02 = @as(u1, @truncate((ucsz & 0x04) >> 2)),
-                .TXEN0 = 1,
-                .RXEN0 = 1,
-                .UDRIE0 = 0, // no interrupts
-                .TXCIE0 = 0, // no interrupts
-                .RXCIE0 = 0, // no interrupts
-            });
-            USART0.UCSR0C.write(.{
-                .UCPOL0 = 0, // async mode
-                .UCSZ0 = @as(u2, @truncate((ucsz & 0x03) >> 0)),
-                .USBS0 = usbs,
-                .UPM0 = upm,
-                .UMSEL0 = umsel,
-            });
-
-            USART0.UBRR0.modify(ubrr_val);
-
-            return Self{};
-        }
-
-        pub fn can_write(self: Self) bool {
-            _ = self;
-            return (USART0.UCSR0A.read().UDRE0 == 1);
-        }
-
-        pub fn tx(self: Self, ch: u8) void {
-            while (!self.can_write()) {} // Wait for Previous transmission
-            USART0.UDR0.* = ch; // Load the data to be transmitted
-        }
-
-        pub fn can_read(self: Self) bool {
-            _ = self;
-            return (USART0.UCSR0A.read().RXC0 == 1);
-        }
-
-        pub fn rx(self: Self) u8 {
-            while (!self.can_read()) {} // Wait till the data is received
-            return USART0.UDR0.*; // Read received data
-        }
-    };
-}
