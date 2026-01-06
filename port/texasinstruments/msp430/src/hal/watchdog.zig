e33a8630fcccb041ebf45c3f9b254bc5d6b91ce0
Matt Knight <mattnite@proton.me>
2026-01-06 09:33:26 -0800

    Fix watchdog disable

--- port/texasinstruments/msp430/src/hal/watchdog.zig
+++ port/texasinstruments/msp430/src/hal/watchdog.zig
@@ -4,5 +4,7 @@ const WDTCTL = &microzig.chip.peripherals.Watchdog_Timer.WDTCTL;
 pub fn disable() void {
     WDTCTL.modify(.{
         .WDTHOLD = 1,
+        // This needs to be patched
+        .padding = 0x5A,
     });
 }
