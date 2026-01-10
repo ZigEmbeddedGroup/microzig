ef98a5603af788aba584d3b7208481a4494b098a
henryrgithub <hroeramsey@gmail.com>
2026-01-09 20:16:57 -0800

    Uncommented examples. Removed commented lines from Nucleo

--- port/stmicro/stm32/src/boards/STM32F303NUCLEO.zig
+++ port/stmicro/stm32/src/boards/STM32F303NUCLEO.zig
@@ -3,52 +3,9 @@ const std = @import("std");
 pub const microzig = @import("microzig");
 
 pub const hal = microzig.hal;
-pub const rcc = hal.rcc;
-
-//pub const uart_logger = hal.uart.UARTLogger(.USART1);
 
 pub const leds_config = (hal.pins.GlobalConfiguration{
     .GPIOA = .{
         .PIN5 = .{ .name = "LD2", .mode = .{ .output = .{ .resistor = .Floating, .o_type = .PushPull } } },
     },
 });
-
-//pub fn init() void {
-//    rcc.enable_hse(8_000_000);
-//    rcc.enable_pll(.HSE, .Div1, .Mul5) catch {
-//        @panic("PLL faile to enable");
-//    };
-//    rcc.select_pll_for_sysclk() catch {
-//        @panic("Faile to select sysclk");
-//    };
-//}
-//
-//// Init should come first or the baud_rate would be too fast for the default HSI.
-//pub fn init_log() void {
-//    _ = (hal.pins.GlobalConfiguration{
-//        .GPIOC = .{
-//            .PIN4 = .{ .mode = .{ .alternate_function = .{ .afr = .AF7 } } },
-//            .PIN5 = .{ .mode = .{ .alternate_function = .{ .afr = .AF7 } } },
-//        },
-//    }).apply();
-//    uart_logger.init(.{
-//        .baud_rate = 115200,
-//        .dma = hal.dma.DMA1_Channel4.get_channel(),
-//    });
-//}
-//
-//pub fn i2c1() hal.i2c.I2C_Device {
-//    _ = (hal.pins.GlobalConfiguration{
-//        .GPIOB = .{
-//            // I2C
-//            .PIN6 = .{ .mode = .{ .alternate_function = .{
-//                .afr = .AF4,
-//            } } },
-//            .PIN7 = .{ .mode = .{ .alternate_function = .{
-//                .afr = .AF4,
-//            } } },
-//        },
-//    }).apply();
-//
-//    return hal.i2c.I2C_Device.init(.I2C1);
-//}
