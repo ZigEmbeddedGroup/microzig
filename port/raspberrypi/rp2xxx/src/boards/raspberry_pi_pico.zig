4c76889772834b5e8db458588e8169e4073758de
Grazfather <grazfather@gmail.com>
2026-01-10 08:32:18 -0500

    rp2xxx: Define LED pin in default config

--- port/raspberrypi/rp2xxx/src/boards/raspberry_pi_pico.zig
+++ port/raspberrypi/rp2xxx/src/boards/raspberry_pi_pico.zig
@@ -1,7 +1,18 @@
 pub const xosc_freq = 12_000_000;
 
+const microzig = @import("microzig");
+const hal = microzig.hal;
+const pins = hal.pins;
+
 pub const bootrom = @import("shared/rp2040_bootrom.zig");
 
 comptime {
     _ = bootrom;
 }
+
+pub const pin_config = pins.GlobalConfiguration{
+    .GPIO25 = .{
+        .name = "led",
+        .function = .SIO,
+    },
+};
