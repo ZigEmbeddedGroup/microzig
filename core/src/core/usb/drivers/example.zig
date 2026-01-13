cd4e348342816629f8619d611ab6896b776c25c1
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2026-01-13 15:44:24 +0100

    change the example usb driver to a simple echo

--- core/src/core/usb/drivers/example.zig
+++ core/src/core/usb/drivers/example.zig
@@ -1,14 +1,14 @@
 const std = @import("std");
 const usb = @import("../../usb.zig");
-const descriptor = usb.descriptor;
+const log = std.log.scoped(.usb_echo);
 
-pub const ExampleDriver = struct {
+/// This is an example driver that sends any data received on ep2 to ep1.
+pub const EchoExampleDriver = struct {
     /// The descriptors need to have the same memory layout as the sent data.
     pub const Descriptor = extern struct {
-        example_interface: descriptor.Interface,
-        ep_in1: descriptor.Endpoint,
-        ep_in2: descriptor.Endpoint,
-        ep_out: descriptor.Endpoint,
+        example_interface: usb.descriptor.Interface,
+        ep_in: usb.descriptor.Endpoint,
+        ep_out: usb.descriptor.Endpoint,
 
         /// This function is used during descriptor creation. If multiple instances
         /// of a driver are used, a descriptor will be created for each.
@@ -22,26 +22,20 @@ pub const ExampleDriver = struct {
                 .example_interface = .{
                     .interface_number = first_interface,
                     .alternate_setting = 0,
-                    .num_endpoints = 3,
-                    .interface_class = 0,
-                    .interface_subclass = 0,
-                    .interface_protocol = 0,
+                    .num_endpoints = 2,
+                    .interface_class = 0xFF,
+                    .interface_subclass = 0xFF,
+                    .interface_protocol = 0xFF,
                     .interface_s = first_string,
                 },
-                .ep_in1 = .{
+                .ep_in = .{
                     .endpoint = .in(@enumFromInt(first_endpoint_in)),
-                    .attributes = .interrupt,
-                    .max_packet_size = .from(64),
-                    .interval = 16,
-                },
-                .ep_in2 = .{
-                    .endpoint = .in(@enumFromInt(first_endpoint_in + 1)),
                     .attributes = .bulk,
                     .max_packet_size = .from(64),
                     .interval = 0,
                 },
                 .ep_out = .{
-                    .endpoint = .out(@enumFromInt(first_endpoint_out)),
+                    .endpoint = .out(@enumFromInt(first_endpoint_out + 1)),
                     .attributes = .bulk,
                     .max_packet_size = .from(64),
                     .interval = 0,
@@ -50,19 +44,27 @@ pub const ExampleDriver = struct {
         }
     };
 
-    /// This is a mapping from endpoint descriptor field names
-    /// to handler function names.
+    /// This is a mapping from endpoint descriptor field names to handler
+    /// function names. Counterintuitively, usb devices send data on 'in'
+    /// endpoints and receive on 'out' endpoints.
     pub const handlers = .{
-        .ep_in1 = "handler1",
-        .ep_in2 = "handler2",
-        .ep_out = "handler3",
+        .ep_in = "on_tx_ready",
+        .ep_out = "on_rx",
     };
+
+    device: *usb.DeviceInterface,
+    ep_tx: usb.types.Endpoint.Num,
 
     /// This function is called when the host chooses a configuration
     /// that contains this driver.
     pub fn init(desc: *const Descriptor, device: *usb.DeviceInterface) @This() {
-        defer device.ep_listen(desc.ep_out.endpoint.num, 64);
-        return .{};
+        const self: @This() = .{
+            .device = device,
+            .ep_tx = desc.ep_in.endpoint.num,
+        };
+        // Listen for first packet
+        device.ep_listen(desc.ep_out.endpoint.num, 64);
+        return self;
     }
 
     /// Used for configuration through endpoint 0.
@@ -79,18 +81,29 @@ pub const ExampleDriver = struct {
     /// Each endpoint (as defined in the descriptor) has its own handler.
     /// Endpoint number is passed as an argument so that it does not need
     /// to be stored in the driver.
-    pub fn handler1(self: *@This(), ep_num: usb.types.Endpoint.Num) void {
-        _ = self;
-        _ = ep_num;
+    pub fn on_tx_ready(self: *@This(), ep_tx: usb.types.Endpoint.Num) void {
+        log.info("tx ready", .{});
+        // Mark transmission as available
+        @atomicStore(usb.types.Endpoint.Num, &self.ep_tx, ep_tx, .seq_cst);
     }
 
-    pub fn handler2(self: *@This(), ep_num: usb.types.Endpoint.Num) void {
-        _ = self;
-        _ = ep_num;
-    }
-
-    pub fn handler3(self: *@This(), ep_num: usb.types.Endpoint.Num) void {
-        _ = self;
-        _ = ep_num;
+    pub fn on_rx(self: *@This(), ep_rx: usb.types.Endpoint.Num) void {
+        var buf: [64]u8 = undefined;
+        // Read incoming packet into a local buffer
+        const len_rx = self.device.ep_readv(ep_rx, &.{&buf});
+        log.info("Received: {s}", .{buf[0..len_rx]});
+        // Check if we can transmit
+        const ep_tx = @atomicLoad(usb.types.Endpoint.Num, &self.ep_tx, .seq_cst);
+        if (ep_tx != .ep0) {
+            // Mark transmission as not available
+            @atomicStore(usb.types.Endpoint.Num, &self.ep_tx, .ep0, .seq_cst);
+            // Send received packet
+            log.info("Sending {} bytes", .{len_rx});
+            const len_tx = self.device.ep_writev(ep_tx, &.{buf[0..len_rx]});
+            if (len_tx != len_rx)
+                log.err("Only sent {} bytes", .{len_tx});
+        }
+        // Listen for next packet
+        self.device.ep_listen(ep_rx, 64);
     }
 };
