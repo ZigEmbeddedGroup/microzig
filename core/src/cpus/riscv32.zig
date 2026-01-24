141207c51d1af40b992d4737bc0d0e7a781e5505
Grazfather <grazfather@gmail.com>
2026-01-24 13:02:41 -0500

    riscv32: Use single asm blocks to configure PMP and to switch m2u mode

--- core/src/cpus/riscv32.zig
+++ core/src/cpus/riscv32.zig
@@ -15,19 +15,24 @@ pub fn wfe() void {
 }
 
 pub fn pmp_open_all_space() void {
-    // Config entry0 addr to all 1s to make the range cover all space
-    asm volatile ("li x6, 0xffffffff" ::: .{ .x6 = true });
-    asm volatile ("csrw pmpaddr0, x6");
-    // Config entry0 cfg to make it NAPOT address mode, and R/W/X okay
-    asm volatile ("li x6, 0x7f" ::: .{ .x6 = true });
-    asm volatile ("csrw pmpcfg0, x6");
+    asm volatile (
+        \\
+        // Config entry0 addr to all 1s to make the range cover all space
+        \\li x6, 0xffffffff
+        \\csrw pmpaddr0, x6
+        // Config entry0 cfg to make it NAPOT address mode, and R/W/X okay
+        \\li x6, 0x7f
+        \\csrw pmpcfg0, x6
+        ::: .{ .x6 = true });
 }
 
 pub fn switch_m2u_mode() void {
-    asm volatile ("la x6, 1f    " ::: .{ .x6 = true });
-    asm volatile ("csrw mepc, x6");
-    asm volatile ("mret");
-    asm volatile ("1:");
+    asm volatile (
+        \\la x6, 1f
+        \\csrw mepc, x6
+        \\mret
+        \\1:
+        ::: .{ .x6 = true });
 }
 
 pub const startup_logic = struct {
