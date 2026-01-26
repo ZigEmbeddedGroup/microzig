e4de2690d8432db30f6297de79c2202acb8478b0
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2026-01-26 11:09:22 +0000

    usb: Allow partial packet writes, add error checking and documentation (#848)
    Changes:
    - `usb_start_tx` -> `ep_writev`, now returns number of bytes written
    - `usb_start_rx` -> `ep_readv` and `ep_listen`: This makes it easier to
    make interrupt-safe drivers
    - More error checking when constructing descriptors
    - First attempt at documenting the usb driver comptime interface
    - The drivers now assume ACKs are handled automatically. This is the
    case for the rp2xxx usb controller but I have no idea if other
    controllers also handle it that way.
    - New driver for rp2xxxx family that supports rebooting and logs through
    picotool
    - HID driver implementation

--- examples/raspberrypi/rp2xxx/src/usb_hid.zig
+++ examples/raspberrypi/rp2xxx/src/usb_hid.zig
@@ -4,55 +4,126 @@ const microzig = @import("microzig");
 const rp2xxx = microzig.hal;
 const time = rp2xxx.time;
 const gpio = rp2xxx.gpio;
-
 const usb = microzig.core.usb;
+const USB_Device = rp2xxx.usb.Polled(.{});
 
-const led = gpio.num(25);
-const uart = rp2xxx.uart.instance.num(0);
-const uart_tx_pin = gpio.num(0);
+pub const Modifiers = packed struct(u8) {
+    lctrl: bool,
+    lshift: bool,
+    lalt: bool,
+    lgui: bool,
+    rctrl: bool,
+    rshift: bool,
+    ralt: bool,
+    rgui: bool,
 
-const HidDriver = usb.drivers.hid.HidClassDriver(
-    .{ .max_packet_size = 64, .boot_protocol = true, .endpoint_interval = 0 },
-    usb.descriptor.hid.ReportDescriptorKeyboard,
-);
+    pub const none: @This() = @bitCast(@as(u8, 0));
+};
 
-const usb_config_descriptor = microzig.core.usb.descriptor.Configuration.create();
+pub const Code = enum(u8) {
+    // Codes taken from https://gist.github.com/mildsunrise/4e231346e2078f440969cdefb6d4caa3
+    // zig fmt: off
+    reserved = 0x00, error_roll_over, post_fail, error_undefined,
+    a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z,
+    top_1, top_2, top_3, top_4, top_5, top_6, top_7, top_8, top_9, top_0,
+    enter, escape, delete, tab, space,
+    @"-", @"=", @"[", @"]", @"\\", @"non_us_#", @";", @"'", @"`", @",", @".", @"/",
+    caps_lock,
+    f1, f2, f3, f4, f5, f6, f7, f8, f9, f10, f11, f12,
+    print_screen, scroll_lock, pause, insert, home, page_up, delete_forward, end, page_down,
+    right_arrow, left_arrow, down_arrow, up_arrow, num_lock,
+    kpad_div, kpad_mul, kpad_sub, kpad_add, kpad_enter,
+    kpad_1, kpad_2, kpad_3, kpad_4, kpad_5, kpad_6, kpad_7, kpad_8, kpad_9, kpad_0,
+    kpad_delete, @"non_us_\\", application, power, @"kpad_=",
+    f13, f14, f15, f16, f17, f18, f19, f20, f21, f22, f23, f24,
+    lctrl = 224, lshift, lalt, lgui, rctrl, rshift, ralt, rgui,
+    // zig fmt: on
+    _,
+};
 
-var usb_dev: rp2xxx.usb.Polled(
-    usb.Config{
-        .device_descriptor = .{
-            .bcd_usb = .from(0x0200),
-            .device_triple = .{
-                .class = .Unspecified,
-                .subclass = 0,
-                .protocol = 0,
-            },
-            .max_packet_size0 = 64,
-            .vendor = .from(0x2E8A),
-            .product = .from(0x000A),
-            .bcd_device = .from(0x0100),
-            .manufacturer_s = 1,
-            .product_s = 2,
-            .serial_s = 3,
-            .num_configurations = 1,
-        },
-        .string_descriptors = &.{
-            .from_lang(.English),
-            .from_str("Raspberry Pi"),
-            .from_str("Pico Test Device"),
-            .from_str("someserial"),
-            .from_str("Boot Keyboard"),
-        },
-        .configurations = &.{.{
-            .num = 1,
-            .configuration_s = 0,
-            .attributes = .{ .self_powered = true },
-            .max_current_ma = 100,
-            .Drivers = struct { hid: HidDriver },
-        }},
+pub const KeyboardInReport = extern struct {
+    modifiers: Modifiers,
+    reserved: u8 = 0,
+    keys: [6]Code,
+
+    comptime {
+        std.debug.assert(@sizeOf(@This()) == 8);
+    }
+
+    pub const empty: @This() = .{ .modifiers = .none, .keys = @splat(.reserved) };
+};
+
+pub const KeyboardOutReport = packed struct(u8) {
+    num_lock: bool,
+    caps_lock: bool,
+    scroll_lock: bool,
+    padding: u5 = 0,
+};
+
+const Keyboard = usb.drivers.hid.InterruptDriver(.{
+    .subclass = .Boot,
+    .protocol = .Boot,
+    .report_descriptor = &.{
+        .{ .global_usage_page = .generic_desktop },
+        .local_usage_enum(.{ .generic_desktop = .keyboard }),
+        .{ .main_collection = .Application },
+        // Input: modifier key bitmap
+        .{ .data = .{
+            .usage = .{ .global_page = .keyboard },
+            .usage_range = .{ 0xE0, 0xE7 },
+            .count = 8,
+            .Child = bool,
+            .dir = .In,
+            .type = .dynamic,
+        } },
+        // Reserved 8 bits
+        .{ .data_static = .{ .In, u8 } },
+        // Output: indicator LEDs
+        .{ .data = .{
+            .usage = .{ .global_page = .led },
+            .usage_range = .{ 1, 5 },
+            .count = 5,
+            .Child = bool,
+            .dir = .Out,
+            .type = .dynamic,
+        } },
+        // Padding
+        .{ .data_static = .{ .Out, u3 } },
+        // Input: up to 6 pressed key codes
+        .{ .data = .{
+            .usage = .{ .global_page = .keyboard },
+            .usage_range = .{ 0x00, 0xff },
+            .count = 6,
+            .Child = u8,
+            .dir = .In,
+            .type = .selector,
+        } },
+        // End
+        .main_collection_end,
     },
-    .{},
-) = undefined;
+    .InReport = KeyboardInReport,
+    .OutReport = KeyboardOutReport,
+});
+
+var usb_device: USB_Device = undefined;
+
+var usb_controller: usb.DeviceController(.{
+    .bcd_usb = USB_Device.max_supported_bcd_usb,
+    .device_triple = .unspecified,
+    .vendor = USB_Device.default_vendor_id,
+    .product = USB_Device.default_product_id,
+    .bcd_device = .v1_00,
+    .serial = "someserial",
+    .max_supported_packet_size = USB_Device.max_supported_packet_size,
+    .configurations = &.{.{
+        .attributes = .{ .self_powered = false },
+        .max_current_ma = 50,
+        .Drivers = struct { keyboard: Keyboard, reset: rp2xxx.usb.ResetDriver(null, 0) },
+    }},
+}, .{.{
+    .keyboard = .{ .itf_string = "Boot Keyboard", .poll_interval = 1 },
+    .reset = "",
+}}) = .init;
 
 pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noreturn {
     std.log.err("panic: {s}", .{message});
@@ -62,37 +133,59 @@ pub fn panic(message: []const u8, _: ?*std.builtin.StackTrace, _: ?usize) noretu
 
 pub const microzig_options = microzig.Options{
     .log_level = .debug,
+    .log_scope_levels = &.{
+        .{ .scope = .usb_dev, .level = .warn },
+        .{ .scope = .usb_ctrl, .level = .warn },
+        .{ .scope = .usb_hid_int_driver, .level = .warn },
+    },
     .logFn = rp2xxx.uart.log,
 };
 
+const pin_config: rp2xxx.pins.GlobalConfiguration = .{
+    .GPIO0 = .{ .function = .UART0_TX },
+    .GPIO25 = .{ .name = "led", .direction = .out },
+};
+
 pub fn main() !void {
-    uart_tx_pin.set_function(.uart);
+    const pins = pin_config.apply();
+
+    const uart = rp2xxx.uart.instance.num(0);
     uart.apply(.{
         .clock_config = rp2xxx.clock_config,
     });
     rp2xxx.uart.init_logger(uart);
 
-    led.set_function(.sio);
-    led.set_direction(.out);
-    led.put(1);
+    pins.led.put(1);
 
     // Then initialize the USB device using the configuration defined above
-    usb_dev = .init();
+    usb_device = .init();
 
     var old: u64 = time.get_time_since_boot().to_us();
     var new: u64 = 0;
+    const message: []const Code = &.{ .h, .e, .l, .l, .o, .space, .w, .o, .r, .l, .d, .caps_lock, .enter };
+    var idx: usize = 0;
+
     while (true) {
         // You can now poll for USB events
-        usb_dev.poll();
+        usb_device.poll(&usb_controller);
 
-        if (usb_dev.controller.drivers()) |drivers| {
-            _ = drivers; // TODO
-
+        if (usb_controller.drivers()) |drivers| {
             new = time.get_time_since_boot().to_us();
-            if (new - old > 500000) {
+            const time_since_last = new - old;
+            if (time_since_last < 2_000_000) {
+                idx += @intFromBool(if (idx & 1 == 0 and idx < 2 * message.len)
+                    drivers.keyboard.send_report(
+                        &.{ .modifiers = .none, .keys = .{message[@intCast(idx / 2)]} ++ .{.reserved} ** 5 },
+                    )
+                else
+                    drivers.keyboard.send_report(&.empty));
+            } else {
                 old = new;
-                led.toggle();
+                idx = 0;
             }
+
+            if (drivers.keyboard.receive_report()) |report|
+                pins.led.put(@intFromBool(report.caps_lock));
         }
     }
 }
