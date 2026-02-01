56a18626bd00bb537bd6f9a8e407606246f552bd
Piotr Fila <piotr.fila.stud@pw.edu.pl>
2026-02-01 16:02:00 +0100

    remove unused Mmio.toggle

--- core/src/mmio.zig
+++ core/src/mmio.zig
@@ -83,43 +83,5 @@ pub fn Mmio(comptime PackedT: type, access: MmioAccess(PackedT)) type {
             }
             write(addr, val);
         }
-
-        /// In field `field_name` of struct `val`, toggle (only) all bits that are set in `value`.
-        inline fn toggle_field(val: anytype, comptime field_name: []const u8, value: anytype) void {
-            const FieldType = @TypeOf(@field(val, field_name));
-            switch (@typeInfo(FieldType)) {
-                .int => {
-                    @field(val, field_name) = @field(val, field_name) ^ value;
-                },
-                .@"enum" => |enum_info| {
-                    // same as for the .Int case, but casting to and from the u... tag type U of the enum FieldType
-                    const U = enum_info.tag_type;
-                    @field(val, field_name) =
-                        @as(FieldType, @enumFromInt(@as(U, @intFromEnum(@field(val, field_name))) ^
-                        @as(U, @intFromEnum(@as(FieldType, value)))));
-                },
-                else => |T| {
-                    @compileError("unsupported register field type '" ++ @typeName(T) ++ "'");
-                },
-            }
-        }
-
-        /// In field `field_name` of this register, toggle (only) all bits that are set in `value`.
-        /// A one-field version of toggle(), more helpful if `field_name` is comptime calculated.
-        pub inline fn toggle_one(addr: *volatile Self, comptime field_name: []const u8, value: anytype) void {
-            var val = read(addr);
-            toggle_field(&val, field_name, value);
-            write(addr, val);
-        }
-
-        /// For each `.Field = value` entry of `fields`:
-        /// In field `F` of this register, toggle (only) all bits that are set in `value`.
-        pub inline fn toggle(addr: *volatile Self, fields: anytype) void {
-            var val = read(addr);
-            inline for (@typeInfo(@TypeOf(fields)).@"struct".fields) |field| {
-                toggle_field(&val, field.name, @field(fields, field.name));
-            }
-            write(addr, val);
-        }
     };
 }
