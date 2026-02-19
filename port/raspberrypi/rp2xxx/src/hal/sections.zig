3d6b71e6c420a172696b7ac4bf4cb9d4332222b2
Maciej 'vesim' Kuliński <vesim809@pm.me>
2026-02-19 23:43:03 +0100

    rp2xxx: add comments to scratch sections

--- port/raspberrypi/rp2xxx/src/hal/sections.zig
+++ port/raspberrypi/rp2xxx/src/hal/sections.zig
@@ -1,2 +1,6 @@
+// Section name for placement in the second to last SRAM bank, known as scratch X
+// Scratch banks can be used for functions and critical data used by one core to avoid stalling.
 pub const scratch_x = ".scratch_x";
+
+// Section name for placement in the last SRAM bank, known as scratch Y
 pub const scratch_y = ".scratch_y";
