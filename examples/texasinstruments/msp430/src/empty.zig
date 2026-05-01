12261ce70501111d6340c5d524585071198f037b
Matthew Knight <mattnite@proton.me>
2026-05-01 11:18:57 -0700

    test: intentionally remove export_startup to verify linker error

--- examples/texasinstruments/msp430/src/empty.zig
+++ examples/texasinstruments/msp430/src/empty.zig
@@ -4,9 +4,4 @@ const microzig = @import("microzig");
 
 pub const std_options = microzig.std_options(.{});
 
-comptime {
-    _ = microzig.export_startup();
-}
-
-pub const panic = std.debug.no_panic;
 pub fn main() void {}
