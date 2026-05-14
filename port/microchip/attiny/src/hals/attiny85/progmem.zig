a38e96f79174041ab836260d4520b73fac1cebf9
𝑷𝒉𝒊𝒍𝒐𝒄𝒂𝒍𝒚𝒔𝒕 <regime_skylark.0y@icloud.com>
2026-05-14 13:18:41 -0400

    attiny85: Add PCINT EEPROM and PROGMEM helpers

---
+++ port/microchip/attiny/src/hals/attiny85/progmem.zig
@@ -0,0 +1,21 @@
+pub fn Table(comptime T: type, comptime len: usize, comptime values: [len]T) type {
+    return struct {
+        pub const data linksection(".progmem.data") = values;
+
+        pub inline fn get(index: usize) T {
+            return read(T, &data[index]);
+        }
+    };
+}
+
+pub inline fn read(comptime T: type, ptr: *const T) T {
+    return ptr.*;
+}
+
+pub inline fn readByte(ptr: *const u8) u8 {
+    return read(u8, ptr);
+}
+
+pub inline fn readWord(ptr: *const u16) u16 {
+    return read(u16, ptr);
+}
