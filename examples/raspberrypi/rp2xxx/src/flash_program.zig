beef57d53a841af2f14a74126232df007ed14016
Tudor Andrei Dicu <69104075+tact1m4n3@users.noreply.github.com>
2026-05-19 09:15:38 +0300

    rp2xxx: Add flash support on RP2350 (#934)
    Previously, `hal.flash` didn't work on RP2350 as it tried to use the
    second stage bootloader for XIP setup which obviously wasn't available
    (this is only a RP2040 thing). Conveniently, on RP2350, the bootrom
    places a copy of the XIP setup function in boot ram that we can use
    instead.
    
    Tested on RP2350 (arm, arm_flashless and riscv). I haven't run any tests
    on RP2040.
    
    PS: I have reverted some of my changes from #705. The reason for this is
    that stage2_rom didn't have any guaratee for being half word aligned and
    jumping to it is therefore unsafe. Aligning it is not that hard (but
    also not trivial as `@embedFile` doesn't allow you to specify
    alignment), but I noticed that pico sdk doesn't use the second stage
    bootloader in flashless images so maybe it is best to do as they do it.
    I can re-revert this if it is not wanted.
    
    PS_2: I might have discovered a bug in pico-sdk
    [here](https://github.com/raspberrypi/pico-sdk/blob/a1438dff1d38bd9c65dbd693f0e5db4b9ae91779/src/rp2_common/hardware_flash/flash.c#L59).
    They set the thumb bit regardless of the architecture. It might not be a
    bug if on riscv PICO_NO_FLASH is expected to be set but idk.
    
    Fixes #932

--- examples/raspberrypi/rp2xxx/src/flash_program.zig
+++ examples/raspberrypi/rp2xxx/src/flash_program.zig
@@ -3,9 +3,7 @@ const microzig = @import("microzig");
 
 const rp2xxx = microzig.hal;
 const flash = rp2xxx.flash;
-const time = rp2xxx.time;
 const gpio = rp2xxx.gpio;
-const clocks = rp2xxx.clocks;
 
 const led = gpio.num(25);
 const uart = rp2xxx.uart.instance.num(0);
