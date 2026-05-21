3409baecd05743ef08466cc4a4fe0ef324567b4d
Matthew Knight <mattnite@proton.me>
2026-05-21 12:23:08 -0700

    Clean up board

--- port/microchip/attiny/src/boards/adafruit_trinket.zig
+++ port/microchip/attiny/src/boards/adafruit_trinket.zig
@@ -1,18 +1,4 @@
 const microzig = @import("microzig");
 const gpio = microzig.hal.gpio;
 
-pub const clock_frequencies = .{
-    .cpu = 8_000_000,
-};
-
 pub const led_pin = gpio.pin(.b, 1);
-
-pub const pin_map = .{
-    .P0 = "PB0",
-    .P1 = "PB1",
-    .P2 = "PB2",
-    .P3 = "PB3",
-    .P4 = "PB4",
-    // Built-in LED on P1 (PB1)
-    .LED = "PB1",
-};
