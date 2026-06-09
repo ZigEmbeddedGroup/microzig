6734e307f823094ef9e6ab3835990dd516d2b5ba
Matthew Knight <mattnite@proton.me>
2026-06-08 23:49:59 -0700

    Include vendored header

---
+++ modules/freertos/include/freertos.h
@@ -0,0 +1,11 @@
+#ifndef _MICROZIG_FREERTOS_H_
+#define _MICROZIG_FREERTOS_H_
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "queue.h"
+#include "semphr.h"
+#include "event_groups.h"
+#include "timers.h"
+
+#endif
