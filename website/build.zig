78e2aec89813c0dc16b86951abf97b20af33968e
Matthew Knight <mattnite@proton.me>
2026-07-02 17:01:50 -0700

    Revert zine, update docs

--- website/build.zig
+++ website/build.zig
@@ -2,9 +2,7 @@ const std = @import("std");
 const zine = @import("zine");
 
 pub fn build(b: *std.Build) !void {
-    b.getInstallStep().dependOn(&zine.website(b, .{
-        .output_path = "zig-out",
-    }).step);
+    b.getInstallStep().dependOn(&zine.website(b, .{}).step);
 
     const serve = b.step("serve", "Start the Zine dev server");
     const run_zine = zine.serve(b, .{});
