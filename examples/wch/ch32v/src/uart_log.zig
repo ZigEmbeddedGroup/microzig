5f75b30de31761d853f15dd08094c6f14bf04871
Grazfather <grazfather@gmail.com>
2026-09-09 00:23:57 -0400

    ch32v: Revamp the board-level usart setup design (#1021)

--- examples/wch/ch32v/src/uart_log.zig
+++ examples/wch/ch32v/src/uart_log.zig
@@ -4,7 +4,7 @@ const hal = microzig.hal;
 const board = microzig.board;
 const time = hal.time;
 
-const uart_cfg: hal.usart.UartConfig = if (@hasDecl(board, "uart_config")) board.uart_config else .{};
+const uart_setup = board.uart_setup;
 
 pub const panic = microzig.panic;
 
@@ -20,8 +20,8 @@ comptime {
 pub fn main() !void {
     board.init();
 
-    hal.usart.setup_uart(uart_cfg);
-    hal.usart.init_logger(uart_cfg.instance);
+    uart_setup.apply(.{ .baud_rate = 115200 });
+    hal.usart.init_logger(uart_setup.instance);
 
     var i: u32 = 0;
     while (true) : (i += 1) {
--- port/wch/ch32v/src/boards/nanoCH32V203.zig
+++ port/wch/ch32v/src/boards/nanoCH32V203.zig
@@ -25,7 +25,11 @@ pub fn init() void {
 }
 
 /// Default UART: USART1 on PA9
-pub const uart_config: ch32v.usart.UartConfig = .{};
+pub const uart_setup: ch32v.usart.Setup = .{
+    .instance = .USART1,
+    .tx_pin = ch32v.gpio.Pin.init(0, 9), // PA9
+    .rx_pin = ch32v.gpio.Pin.init(0, 10), // PA10
+};
 
 pub const pin_config = ch32v.pins.GlobalConfiguration{
     .GPIOA = .{
